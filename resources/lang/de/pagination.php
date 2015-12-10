172db630da2b225e14646b36b96c909f51007d81
John <pvpbosses4@gmail.com>
2015-12-10 18:50:30 -0500

    Translated pageination.php

--- resources/lang/de/pagination.php
+++ resources/lang/de/pagination.php
@@ -10,17 +10,20 @@ return [
     | you want to customize your views to better match your application.
     |
     */
-    'previous' => '&laquo; Previous',
-    'next'     => 'Next &raquo;',
+    'previous' => '&laquo; Zurück',
+    'next'     => 'Nächster &raquo;',
     'sidebar'  => [
-        'account_controls' => 'Account Controls',
-        'account_settings' => 'Account Settings',
-        'account_security' => 'Account Security',
-        'server_controls' => 'Server Controls',
-        'servers' => 'Your Servers',
-        'overview' => 'Server Overview',
-        'files' => 'File Manager',
-        'subusers' => 'Manage Sub-Users',
-        'manage' => 'Manage Server',
+        'account_controls' => 'Konto Kontrollen',
+        'account_settings' => 'Konto Einstellungen',
+        'account_security' => 'Konto Sicherheit',
+        'server_controls' => 'Server Kontrollen',
+        'servers' => 'Ihr Server',
+        'overview' => 'Server-Übersicht',
+        'files' => 'Dateimanager',
+        'subusers' => 'Verwalten Sub-Benutzer',
+        'manage' => 'Verwalten Server',
     ],
 ];
+/*
+German Translation by Jboy
+*/
