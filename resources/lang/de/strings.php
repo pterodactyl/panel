5572cf5fd1212f8590e7607bb9c3ef1cee925333
John <pvpbosses4@gmail.com>
2015-12-10 18:16:25 -0500

    TO TRANSLATE

---
+++ resources/lang/de/strings.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Standalone Pterodactyl Language Strings
+    |--------------------------------------------------------------------------
+    */
+    'login' => 'Login',
+    'password' => 'Password',
+    'email' => 'Email',
+    'whoops' => 'Whoops',
+    'success' => 'Success',
+    'location' => 'Location',
+    'node' => 'Node',
+    'connection' => 'Connection',
+    'language' => 'Language',
+    'close' => 'Close',
+    'start' => 'Start',
+    'stop' => 'Stop',
+    'restart' => 'Restart',
+    'save' => 'Save',
+    'enabled' => 'Enabled',
+    'disabled' => 'Disabled',
+    'submit' => 'Submit',
+    'current_password' => 'Current Password',
+    'again' => 'Again',
+];
