b7e2429be6ceddac2373774a6883715f22f3ef07
John <pvpbosses4@gmail.com>
2015-12-10 17:45:08 -0500

    Added Auth Translations

--- resources/lang/de/auth.php
+++ resources/lang/de/auth.php
@@ -1,1 +1,25 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed' => 'Falsches Passwort!', /* "Incorrect Password!" */
+    'throttle' => 'Zu viele Login-Versuche! Versuchen Sie, in :seconds Sekunde.', /* "Too many login attempts! Try in (number) Seconds" */
+    'errorencountered' => 'Ein Fehler ist aufgetreten!', /* An error occurred! */
+    'resetpassword' => 'Passwort zurücksetzen.', /* Reset Password */
+    'confirmpassword' => 'Bestätige das Passwort.', /* Confirm your password */
+    'sendlink' => 'Senden Sie Passwort-Reset Link.', /* Send password reset link */
+    'emailsent' => 'E-Mail gesendet.', /* Email Sent */
+    'remeberme' => 'Login merken.', /* Remember my Login*/
+];
 
+/*
+German Translation by Jboy
+*/
