6289e7ae8d5207ce6b8c3406d422b8958645c5ae
Dane Everitt <dane@daneeveritt.com>
2015-12-13 22:26:10 -0500

    Fixes display issue with socket errors

--- resources/lang/en/server.php
+++ resources/lang/en/server.php
@@ -9,7 +9,7 @@ return [
     */
 
     'ajax' => [
-        'socket_error' => 'We were unable to connect to the main Socket.IO server, there may be network issues currently. <br /><br />If this is your first time seeing this message it may be because you need to accept this server\'s SSL certificate. Please click this notification and accept the certificate.',
+        'socket_error' => 'We were unable to connect to the main Socket.IO server, there may be network issues currently. <br /><br />If this is your first time seeing this message it may be because you need to accept this servers SSL certificate. Please click this notification and accept the certificate.',
         'socket_status' => 'This server\'s power status has changed to',
         'socket_status_crashed' => 'This server has been detected as CRASHED.',
     ],
