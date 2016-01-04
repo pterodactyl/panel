a3af08e40233b072fda422ae3b49a00d2f344236
Dane Everitt <dane@daneeveritt.com>
2016-01-04 17:52:28 -0500

    Fix 404 page display

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -10,7 +10,9 @@
 @endsection
 
 @section('content')
-    <h1 style="text-align:center;">404 - File Not Found</h1>
-    <p style="text-align:center;"><img src="{{ asset('images/404.jpg') }}" /></p>
-    <p style="text-align:center;"><a href="{{ URL::previous() }}">Take me back</a> or <a href="/">go home</a>.</p>
+<div class="col-md-8">
+    <h1 class="text-center">404 - File Not Found</h1>
+    <p class="text-center"><img src="{{ asset('images/404.jpg') }}" /></p>
+    <p class="text-center"><a href="{{ URL::previous() }}">Take me back</a> or <a href="/">go home</a>.</p>
+</div>
 @endsection
