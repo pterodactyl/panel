77e3744b404d6d1a3bf542afb5d934f509fab6a9
Dane Everitt <dane@daneeveritt.com>
2016-01-15 19:26:50 -0500

    Change authentication method for API.

--- app/Http/Routes/APIRoutes.php
+++ app/Http/Routes/APIRoutes.php
@@ -10,25 +10,7 @@ class APIRoutes
 
     public function map(Router $router) {
 
-        app('Dingo\Api\Auth\Auth')->extend('jwt', function ($app) {
-            return new \Dingo\Api\Auth\Provider\JWT($app['Tymon\JWTAuth\JWTAuth']);
-        });
-
         $api = app('Dingo\Api\Routing\Router');
-
-        $api->version('v1', function ($api) {
-            $api->post('auth/login', [
-                'as' => 'api.auth.login',
-                'uses' => 'Pterodactyl\Http\Controllers\API\AuthController@postLogin'
-            ]);
-
-            $api->post('auth/validate', [
-                'middleware' => 'api.auth',
-                'as' => 'api.auth.validate',
-                'uses' => 'Pterodactyl\Http\Controllers\API\AuthController@postValidate'
-            ]);
-        });
-
         $api->version('v1', ['middleware' => 'api.auth'], function ($api) {
 
             /**
--- config/api.php
+++ config/api.php
@@ -155,7 +155,7 @@ return [
     */
 
     'auth' => [
-        'jwt' => 'Dingo\Api\Auth\Provider\JWT'
+        'custom' => 'Pterodactyl\Http\Middleware\APISecretToken'
     ],
 
     /*
