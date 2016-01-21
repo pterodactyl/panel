0e156223667782e56a22c66cb7d02a3e306023b8
Ian Szot <ian_marek@hotmail.com>
2016-01-21 20:59:30 -0200

    Initial translation to portuguese
    Translated to pt_BR

---
+++ resources/lang/pt/auth.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Pterodactyl - Panel
+ * Copyright (c) 2015 - 2016 Dane Everitt <dane@daneeveritt.com>
+ * Some Modifications (c) 2015 Dylan Seidt <dylan.seidt@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed' => 'Estas credenciais não estão de acordo com os nossos registros.',
+    'throttle' => 'Muitas tentativas de login. Por favor tente novamente em :seconds segundos.',
+    'errorencountered' => 'Ocorreu um erro durante a tentativa de processar esse pedido.',
+    'resetpassword' => 'Trocar senha',
+    'confirmpassword' => 'Confirmar senha',
+    'sendlink' => 'Enviar link de troca de senha',
+    'emailsent' => 'O seu email para trocar de senha está a caminho.',
+    'remeberme' => 'Lembre-me',
+    'totp_failed' => 'O token TOTP dado é inválido. Por favor tenha certeza que o token gerado pelo seu dispositivo era correto.'
+];
