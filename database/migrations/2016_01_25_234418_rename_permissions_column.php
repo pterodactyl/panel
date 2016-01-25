c069b8d18a0f8a49cb1c5a1ef7237f573527fb42
Dane Everitt <dane@daneeveritt.com>
2016-01-25 18:45:34 -0500

    fix column name in permissions; closes #41

---
+++ database/migrations/2016_01_25_234418_rename_permissions_column.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenamePermissionsColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->renameColumn('permissions', 'permission');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            //
+        });
+    }
+}
