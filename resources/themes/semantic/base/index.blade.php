8f2fe2d61249543ddf16ef9222462310f3ac4d76
DDynamic <dylan.seidt@gmail.com>
2016-02-10 15:21:17 -0600

    Add AJAX to Server List

--- resources/themes/semantic/base/index.blade.php
+++ resources/themes/semantic/base/index.blade.php
@@ -9,7 +9,7 @@
         <input type="text" placeholder="Search for a server name, IP, or node." id="search">
         <i class="search icon"></i>
     </div>
-    <table class="ui celled padded table">
+    <table class="ui celled padded single line table">
         <thead>
             <tr>
                 @if (Auth::user()->root_admin == 1)<th></th>@endif
@@ -19,12 +19,11 @@
                 <th>{{ trans('strings.players') }}</th>
                 <th>{{ trans('strings.memory') }}</th>
                 <th>{{ trans('strings.cpu') }}</th>
-                <th>{{ trans('strings.status') }}</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($servers as $server)
-                <tr>
+                <tr data-server="{{ $server->uuidShort }}">
                     <td>
                         @if ($server->owner === Auth::user()->id)
                             <i class="ui blue empty circular label"></i>
@@ -34,11 +33,10 @@
                     </td>
                     <td><a href="/server/{{ $server->uuidShort }}">{{ $server->name }}</a></td>
                     <td>{{ $server->nodeName }} ({{ $server->a_locationShort }})</td>
-                    <td><code>{{ $server->ip }}:{{ $server->port }}</code></td>
-                    <td>--</td>
-                    <td><span data-action="memory">--</span> / {{ $server->memory }} MB</td>
-                    <td><span data-action="cpu" data-cpumax="{{ $server->cpu }}">--</span> %</td>
-                    <td>--</td>
+                    <td><span data-action="status"><a class="ui grey circular label">Unknown</a></span><code> {{ $server->ip }}:{{ $server->port }}</code></td>
+                    <td data-action="players">--</td>
+                    <td><span data-action="memory">--</span></td>
+                    <td><span data-action="cpu" data-cpumax="{{ $server->cpu }}">--</span>%</td>
                 </tr>
             @endforeach
         </tbody>
@@ -62,6 +60,50 @@
             setTimeout(function() {
               $('#search').parent().removeClass('loading')
             }, 1000);
+        });
+        var Status = {
+            0: 'Off',
+            1: 'On',
+            2: 'Starting',
+            3: 'Stopping'
+        };
+        $('[data-server]').each(function(index, data) {
+            var element = $(this);
+            $.ajax({
+                type: 'GET',
+                url: '/server/' + $(this).attr('data-server') + '/ajax/status',
+                headers: {
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                }
+            }).done(function (data) {
+                element.find('[data-action="players"]').html(data.query.players.length);
+                element.find('[data-action="memory"]').html(parseInt(data.proc.memory.total / (1024 * 1024)) + ' MB');
+
+                if(data.proc.memory.amax * 0.9 < data.proc.memory.total) {
+                    element.find('[data-action="memory"]').css('color', '#DB2828');
+                } else if(data.proc.memory.amax * 0.7 < data.proc.memory.total) {
+                    element.find('[data-action="memory"]').css('color', '#F2711C');
+                }
+
+                element.find('[data-action="cpu"]').html(data.proc.cpu.total);
+
+                switch(data.status) {
+                    case 0:
+                        element.find('[data-action="status"]').html('<a class="ui red circular label">Offline</a>');
+                        break;
+                    case 1:
+                        element.find('[data-action="status"]').html('<a class="ui green circular label">Online</a>');
+                        break;
+                    case 2:
+                        element.find('[data-action="status"]').html('<a class="ui teal circular label">Starting</a>');
+                        break;
+                    case 3:
+                        element.find('[data-action="status"]').html('<a class="ui orange circular label">Stopping</a>');
+                        break;
+                }
+            }).fail(function (jqXHR) {
+                console.error(jqXHR);
+            });
         });
     </script>
 @endsection
