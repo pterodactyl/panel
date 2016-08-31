8e4a6aac3c6044dd7b2c33c178d735b3deece225
Dane Everitt <dane@daneeveritt.com>
2016-08-30 22:21:53 -0400

    update migration to default alias to current ip

--- database/migrations/2016_08_30_212718_add_ip_alias.php
+++ database/migrations/2016_08_30_212718_add_ip_alias.php
@@ -15,6 +15,17 @@ class AddIpAlias extends Migration
         Schema::table('allocations', function (Blueprint $table) {
             $table->text('ip_alias')->nullable()->after('ip');
         });
+
+        $allocations = DB::select('SELECT id, ip FROM allocations');
+        foreach($allocations as $allocation) {
+            DB::update(
+                'UPDATE allocations SET ip_alias = :ip WHERE id = :id',
+                [
+                    'ip' => $allocation->ip,
+                    'id' => $allocation->id
+                ]
+            );
+        }
     }
 
     /**
