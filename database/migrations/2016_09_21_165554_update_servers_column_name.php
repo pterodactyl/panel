f29111309d4ae3d20c2ead4ffb75299d7cdf8c9a
Dane Everitt <dane@daneeveritt.com>
2016-09-21 16:57:39 -0400

    Somehow this migration slipped through and was never added

---
+++ database/migrations/2016_09_21_165554_update_servers_column_name.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateServersColumnName extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('databases', function (Blueprint $table) {
+            $table->renameColumn('server', 'server_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('databases', function (Blueprint $table) {
+            $table->renameColumn('server_id', 'server');
+        });
+    }
+}
