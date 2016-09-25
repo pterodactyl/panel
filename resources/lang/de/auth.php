54785e7f85f09f83ba018720529c9742d7c83607
Jakob Schrettenbrunner <dev@schrej.net>
2016-09-26 00:47:39 +0100

    add german translation

---
+++ resources/lang/de/auth.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Pterodactyl - Panel
+ * Copyright (c) 2015 - 2016 Dane Everitt <dane@daneeveritt.com>
+ * Some Modifications (c) 2015 Dylan Seidt <dylan.seidt@gmail.com>
+ * Translated by Jakob Schrettenbrunner <dev@schrej.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Die Zugangsdaten sind ungültig.',
+    'throttle' => 'Zu viele Loginversuche. Bitte in :seconds Sekunden erneut versuchen.',
+    'errorencountered' => 'Während der Anfrage ist ein Fehler aufgetreten.',
+    'resetpassword' => 'Passwort zurücksetzen',
+    'confirmpassword' => 'Passwort bestätigen',
+    'sendlink' => 'Passwort Rücksetzungslink anfordern.',
+    'emailsent' => 'Die E-Mail mit dem Rücksetzungslink ist unterwegs.',
+    'remeberme' => 'Angemeldet bleiben',
+    'totp_failed' => 'Das TOTP Token ist ungültig. Bitte stelle sicher dass das generierte Token gültig ist.'
+
+];
---
+++ resources/lang/de/strings.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Pterodactyl - Panel
+ * Copyright (c) 2015 - 2016 Dane Everitt <dane@daneeveritt.com>
+ * Some Modifications (c) 2015 Dylan Seidt <dylan.seidt@gmail.com>
+ * Translated by Jakob Schrettenbrunner <dev@schrej.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Standalone Pterodactyl Language Strings
+    |--------------------------------------------------------------------------
+    */
+
+    'login' => 'Login',
+    'password' => 'Passwort',
+    'email' => 'Email',
+    'whoops' => 'Uuups',
+    'success' => 'Erfolgreich',
+    'location' => 'Ort',
+    'node' => 'Node',
+    'connection' => 'Verbindung',
+    'language' => 'Sprache',
+    'close' => 'Schließen',
+    'start' => 'Start',
+    'stop' => 'Stop',
+    'restart' => 'Neustart',
+    'save' => 'Speichern',
+    'enabled' => 'Aktiviert',
+    'disabled' => 'Deaktiviert',
+    'submit' => 'Absenden',
+    'current_password' => 'Aktuelles Passwort',
+    'again' => 'wiederholen',
+    'registered' => 'Registriert',
+    'root_administrator' => 'Root Administrator',
+    'yes' => 'Ja',
+    'no' => 'Nein',
+    'memory' => 'Speicher',
+    'cpu' => 'CPU',
+    'status' => 'Status',
+    'players' => 'Spieler',
+
+];
