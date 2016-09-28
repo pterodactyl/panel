ca84022e84a75d49a1ee1c2db5cd95a26aa3a563
Dane Everitt <dane@daneeveritt.com>
2016-09-27 21:02:30 -0400

    bump version to 0.5.0
    Any changes to v0.4.x release will require a release branch with
    modifications.

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ return [
 
     'env' => env('APP_ENV', 'production'),
 
-    'version' => env('APP_VERSION', 'v0.4.1-beta'),
+    'version' => env('APP_VERSION', 'v0.5.0-beta'),
 
     /*
     |--------------------------------------------------------------------------
