b4bcc7247fa8bbd306f7f7a7787ba0fc95e7eb04
Dane Everitt <dane@daneeveritt.com>
2016-11-28 13:55:02 -0500

    Add some things to node config block that will be useful to some

--- resources/views/admin/nodes/view.blade.php
+++ resources/views/admin/nodes/view.blade.php
@@ -276,6 +276,7 @@
                     <div class="col-md-12">
                         <pre><code>{
     "web": {
+        "host": "0.0.0.0",
         "listen": {{ $node->daemonListen }},
         "ssl": {
             "enabled": {{ $node->scheme === 'https' ? 'true' : 'false' }},
@@ -292,6 +293,10 @@
         "port": {{ $node->daemonSFTP }},
         "container": "ptdl-sftp"
     },
+    "query": {
+        "kill_on_fail": false,
+        "fail_limit": 5
+    },
     "logger": {
         "path": "logs/",
         "src": false,
@@ -300,6 +305,7 @@
         "count": 3
     },
     "remote": {
+        "base": "{{ config('app.url') }}",
         "download": "{{ route('remote.download') }}",
         "installed": "{{ route('remote.install') }}"
     },
