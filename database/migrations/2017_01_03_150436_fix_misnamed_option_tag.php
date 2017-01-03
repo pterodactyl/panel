759ebd3ad044187cf186dcc30ce39d710d428833
Dane Everitt <dane@daneeveritt.com>
2017-01-03 15:06:40 -0500

    Fixes misnamed tag for Sponge servers. closes #237

---
+++ database/migrations/2017_01_03_150436_fix_misnamed_option_tag.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixMisnamedOptionTag extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::transaction(function () {
+            DB::table('service_options')->where([
+                ['name', 'Sponge (SpongeVanilla)'],
+                ['tag', 'spigot'],
+                ['docker_image', 'quay.io/pterodactyl/minecraft:sponge'],
+            ])->update([
+                'tag' => 'sponge',
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('service_options')->where([
+            ['name', 'Sponge (SpongeVanilla)'],
+            ['tag', 'sponge'],
+            ['docker_image', 'quay.io/pterodactyl/minecraft:sponge'],
+        ])->update([
+            'tag' => 'spigot',
+        ]);
+    }
+}
