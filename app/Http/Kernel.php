8ab4faad8a47d0e4d1db9ff6b07e6d6bf8703490
Jakob Schrettenbrunner <dev@schrej.net>
2017-02-01 20:31:24 +0100

    remove TRUSTED_PROXIES from .env.example make style ci happy

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        
+
         \Pterodactyl\Http\Middleware\LanguageMiddleware::class,
         \Fideloper\Proxy\TrustProxies::class,
     ];
--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -30,13 +30,13 @@ return [
      * Or, to trust all proxies that connect
      * directly to your server, uncomment this:
      */
-     # 'proxies' => '*',
+    // 'proxies' => '*',
 
     /*
      * Or, to trust ALL proxies, including those that
      * are in a chain of fowarding, uncomment this:
-    */
-    # 'proxies' => '**',
+     */
+    // 'proxies' => '**',
 
     /*
      * Default Header Names
@@ -55,5 +55,5 @@ return [
         \Illuminate\Http\Request::HEADER_CLIENT_HOST  => 'X_FORWARDED_HOST',
         \Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
         \Illuminate\Http\Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
-    ]
+    ],
 ];
