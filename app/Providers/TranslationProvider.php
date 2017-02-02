8fc30fbe3a7ba7cba081f2af82bbe8b81f089469
Jakob Schrettenbrunner <dev@schrej.net>
2017-02-02 15:05:33 +0100

    add phraseapp in context editor

---
+++ app/Providers/TranslationProvider.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Pterodactyl - Panel
+ * Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace Pterodactyl\Providers;
+
+use Pterodactyl\Extensions\Translator as PterodactylTranslator;
+use Illuminate\Translation\Translator;
+use Illuminate\Translation\TranslationServiceProvider;
+
+class TranslationProvider extends TranslationServiceProvider {
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerLoader();
+
+        $this->app->singleton('translator', function ($app) {
+            $loader = $app['translation.loader'];
+
+            // When registering the translator component, we'll need to set the default
+            // locale as well as the fallback locale. So, we'll grab the application
+            // configuration so we can easily get both of these values from there.
+            $locale = $app['config']['app.locale'];
+
+            if ($app['config']['app.phrase_in_context']) {
+                $trans = new PterodactylTranslator($loader, $locale);
+            } else {
+                $trans = new Translator($loader, $locale);
+            }
+
+            $trans->setFallback($app['config']['app.fallback_locale']);
+
+            return $trans;
+        });
+    }
+
+
+}
--- resources/themes/pterodactyl/layouts/master.blade.php
+++ resources/themes/pterodactyl/layouts/master.blade.php
@@ -281,6 +281,19 @@
             {!! Theme::js('vendor/adminlte/app.min.js') !!}
             {!! Theme::js('js/vendor/socketio/socket.io.min.js') !!}
             {!! Theme::js('vendor/bootstrap-notify/bootstrap-notify.min.js') !!}
+
+            @if(config('app.phrase_in_context'))
+                <script>
+                    window.PHRASEAPP_CONFIG = {
+                      projectId: '94f8b39450cd749ae9c3cc0ab8cdb61d'
+                    };
+                    (function() {
+                        var phraseapp = document.createElement('script'); phraseapp.type = 'text/javascript'; phraseapp.async = true;
+                        phraseapp.src = ['https://', 'phraseapp.com/assets/in-context-editor/2.0/app.js?', new Date().getTime()].join('');
+                        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(phraseapp, s);
+                    })();
+                </script>
+            @endif
         @show
     </body>
 </html>
