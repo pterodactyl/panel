47303095897a87b9b260084c8baeb4885a693f3c
Dane Everitt <dane@daneeveritt.com>
2017-03-15 19:47:44 -0400

    Add IDE helper for all functions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -49,6 +49,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 }
