b000b4da43b97bc344527c4abb906343bef5894f
Dane Everitt <dane@daneeveritt.com>
2017-04-15 23:17:45 -0400

    Fixes task migration issue, closes #378

--- database/migrations/2017_04_15_125021_UpgradeTaskSystem.php
+++ database/migrations/2017_04_15_125021_UpgradeTaskSystem.php
@@ -1,5 +1,6 @@
 <?php
 
+use Pterodactyl\Models\Task;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -17,10 +18,21 @@ class UpgradeTaskSystem extends Migration
             $table->dropForeign(['server']);
 
             $table->renameColumn('server', 'server_id');
-            $table->unsignedInteger('user_id')->after('id');
+            $table->unsignedInteger('user_id')->nullable()->after('id');
 
             $table->foreign('server_id')->references('id')->on('servers');
             $table->foreign('user_id')->references('id')->on('users');
+        });
+
+        DB::transaction(function () {
+            foreach(Task::all() as $task) {
+                $task->user_id = $task->server->owner_id;
+                $task->save();
+            }
+        });
+
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->unsignedInteger('user_id')->nullable(false)->change();
         });
     }
 
