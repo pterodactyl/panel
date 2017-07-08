8953f83f87c6d0d038e1e5506779156f011f7b70
Dane Everitt <dane@daneeveritt.com>
2017-07-08 15:51:13 -0500

    Add migrations to handle cascade deletions for servers and users

---
+++ database/migrations/2017_07_08_154416_SetAllocationToReferenceNullOnServerDelete.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SetAllocationToReferenceNullOnServerDelete extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('allocations', function (Blueprint $table) {
+            $table->dropForeign(['server_id']);
+
+            $table->foreign('server_id')->references('id')->on('servers')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('allocations', function (Blueprint $table) {
+            $table->dropForeign(['server_id']);
+
+            $table->foreign('server_id')->references('id')->on('servers');
+        });
+    }
+}
