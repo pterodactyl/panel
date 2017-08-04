c71032a70785aca3a593db477fda575779effed7
Joost Kwakkel <joost@kaashosting.nl>
2017-08-05 00:04:15 +0200

    Only left-clicking now activates the selection of an item

--- public/themes/pterodactyl/js/frontend/files/src/index.js
+++ public/themes/pterodactyl/js/frontend/files/src/index.js
@@ -113,13 +113,15 @@ class FileManager {
 
     selectRow() {
       $('#file_listing tr').on('mousedown', event => {
-          if($(event.target).is('th') || $(event.target).is('input[data-action="selectAll"]')) {
-              new ActionsClass().highlightAll(event);
-          } else if($(event.target).is('td') || $(event.target).is('input[data-action="addSelection"]')) {
-              new ActionsClass().toggleHighlight(event);
-          }
+          if (event.which === 1) {
+              if ($(event.target).is('th') || $(event.target).is('input[data-action="selectAll"]')) {
+                  new ActionsClass().highlightAll(event);
+              } else if ($(event.target).is('td') || $(event.target).is('input[data-action="addSelection"]')) {
+                  new ActionsClass().toggleHighlight(event);
+              }
 
-          new ActionsClass().toggleMassActions();
+              new ActionsClass().toggleMassActions();
+          }
       });
     }
 
