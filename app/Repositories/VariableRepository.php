3a921133fe67fca51231e1f49def77da099e0ec3
kasper Franz <kasper@franz.guru>
2017-08-26 10:17:57 +0200

    StyleCi

--- app/Repositories/VariableRepository.php
+++ app/Repositories/VariableRepository.php
@@ -48,7 +48,7 @@ class VariableRepository
         $option = ServiceOption::select('id')->findOrFail($option);
 
         // If there is not a rules present let's populate it with the default/placeholder value.
-        if(!array_key_exists('rules',$data) || empty($data['rules'])){
+        if (! array_key_exists('rules', $data) || empty($data['rules'])) {
             $data['rules'] = 'required|string|max:20';
         }
 
