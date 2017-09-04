855b7fa1e4655b69d7f4c3af80015c23f4a0e41f
Jakob Schrettenbrunner <dev@schrej.net>
2017-09-05 01:46:55 +0200

    fix menu collapse with adminlte 2.4

--- resources/themes/pterodactyl/layouts/master.blade.php
+++ resources/themes/pterodactyl/layouts/master.blade.php
@@ -60,7 +60,7 @@
                     <span>{{ Settings::get('company', 'Pterodactyl') }}</span>
                 </a>
                 <nav class="navbar navbar-static-top">
-                    <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+                    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
