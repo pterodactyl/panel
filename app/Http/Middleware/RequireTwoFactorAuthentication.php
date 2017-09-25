9aa4544bb1cf5390f7c02cbab5007a75bba9e883
Lance Pioch <git@lance.sh>
2017-09-24 22:24:06 -0400

    Update code for review, add license, remove subuser specific logic, improve the rest a little

--- app/Http/Middleware/RequireTwoFactorAuthentication.php
+++ app/Http/Middleware/RequireTwoFactorAuthentication.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * Pterodactyl - Panel
+ * Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace Pterodactyl\Http\Middleware;
 
@@ -8,10 +30,9 @@ use Prologue\Alerts\AlertsMessageBag;
 
 class RequireTwoFactorAuthentication
 {
-    const NOBODY = 0;
-    const ADMINISTRATORS = 1;
-    const USERS = 2;
-    const EVERYBODY = 3;
+    const LEVEL_NONE = 0;
+    const LEVEL_ADMIN = 1;
+    const LEVEL_ALL = 2;
 
     /**
      * @var \Prologue\Alerts\AlertsMessageBag
@@ -22,6 +43,17 @@ class RequireTwoFactorAuthentication
      * @var \Krucas\Settings\Settings
      */
     protected $settings;
+
+        // All TOTP related routes
+    protected $ignoreRoutes = [
+            'account.security',
+            'account.security.revoke',
+            'account.security.totp',
+            'account.security.totp.set',
+            'account.security.totp.disable',
+            'auth.totp',
+            'auth.logout',
+    ];
 
     /**
      * RequireTwoFactorAuthentication constructor.
@@ -45,46 +77,27 @@ class RequireTwoFactorAuthentication
     public function handle($request, Closure $next)
     {
         // Ignore non-users
-        if (! auth()->check()) {
+        if (! $request->user()) {
             return $next($request);
         }
 
-        // All TOTP related routes
-        $validRoutes = [
-            'account.security',
-            'account.security.revoke',
-            'account.security.totp',
-            'account.security.totp.set',
-            'account.security.totp.disable',
-            'auth.totp',
-            'auth.logout',
-        ];
-
-        // Only allow 2FA pages
-        if (in_array($request->route()->getName(), $validRoutes)) {
+        // Skip the 2FA pages
+        if (in_array($request->route()->getName(), $this->ignoreRoutes)) {
             return $next($request);
         }
 
         // Get the setting
-        $tfa = (int) $this->settings->get('2fa', 0);
-
-        switch ($tfa) {
-            case self::NOBODY:
+        switch ((int) $this->settings->get('2fa', 0)) {
+            case self::LEVEL_NONE:
                 return $next($request);
 
-            case self::ADMINISTRATORS:
+            case self::LEVEL_ADMIN:
                 if (! $request->user()->root_admin) {
                     return $next($request);
                 }
                 break;
 
-            case self::USERS:
-                if ($request->user()->accessLevel === 'subuser') {
-                    return $next($request);
-                }
-                break;
-
-            case self::EVERYBODY:
+            case self::LEVEL_ALL:
                 if ($request->user()->use_totp) {
                     return $next($request);
                 }
