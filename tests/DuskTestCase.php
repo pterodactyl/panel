4321f653640bd5bfb28c229ddbb59ef3b9f57734
Anand Capur <git@anand.io>
2017-10-07 19:40:44 -0700

    fix some stuff and also new databasetruncations trait

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -5,6 +5,7 @@ namespace Tests;
 use Laravel\Dusk\TestCase as BaseTestCase;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Tests\Traits\DatabaseTruncations;
 
 abstract class DuskTestCase extends BaseTestCase
 {
@@ -31,5 +32,17 @@ abstract class DuskTestCase extends BaseTestCase
         return RemoteWebDriver::create(
             'http://localhost:9515', DesiredCapabilities::chrome()
         );
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->createApplication();
+
+        $uses = array_flip(class_uses_recursive(static::class));
+
+        if (isset($uses[DatabaseTruncations::class])) {
+            $this->runDatabaseTruncations();
+        }
     }
 }
