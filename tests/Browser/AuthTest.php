5851a51dda6d6a060b01ece4cc6880eb5091e43d
Anand Capur <git@anand.io>
2017-10-07 19:58:44 -0700

    some tests

---
+++ tests/Browser/AuthTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Browser;
+
+use Pterodactyl\Models\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Tests\Traits\DatabaseTruncations;
+
+class AuthTest extends DuskTestCase
+{
+    use DatabaseTruncations;
+
+    public function testLoginFailsWithInvalidCredentials()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/auth/login')
+                ->type('user', 'fake')
+                ->type('password', 'fake')
+                ->press('Sign In')
+                ->assertSee('There was an error while attempting to login');
+        });
+    }
+
+    public function testLoginSucceedsWithValidCredentialsAndCanLogout()
+    {
+        $user = factory(User::class)->create();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/')
+                ->assertPathIs('/auth/login')
+                ->type('user', $user->email)
+                ->type('password', 'password')
+                ->press('Sign In')
+                ->assertPathIs('/')
+                ->assertSee($user->name_first)
+                ->click('#logoutButton')
+                ->assertPathIs('/auth/login')
+                ->assertSee('Authentication is required to continue.')
+                ->visit('/account')
+                ->assertPathIs('/auth/login');
+
+        });
+    }
+}
---
+++ tests/Browser/MyAccountTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser;
+
+use Pterodactyl\Models\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Tests\Browser\Pages\User\MyAccount;
+use Tests\Traits\DatabaseTruncations;
+
+class MyAccountTest extends DuskTestCase
+{
+    use DatabaseTruncations;
+
+    public function testUpdateIdentity()
+    {
+        $user = factory(User::class)->create();
+        $newUser = factory(User::class)->make();
+
+        $this->browse(function (Browser $browser) use ($user, $newUser) {
+            $browser->loginAs($user)
+                ->visit(new MyAccount())
+                ->updateIdentity($newUser->name_first, $newUser->name_last, $newUser->username)
+                ->assertSee(__('base.account.details_updated'))
+                ->assertInputValue('name_first', $newUser->name_first)
+                ->assertInputValue('name_last', $newUser->name_last)
+                ->assertInputValue('username', $newUser->username);
+        });
+    }
+}
