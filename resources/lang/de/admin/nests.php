4ef426ed8f35bb2f9d21031a90faadb1ddf2f332
Michael R.|SchlaumeierTV <michael@rittmeister.in>
2017-11-04 16:02:46 +0100

    Create nests.php

---
+++ resources/lang/de/admin/nests.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Pterodactyl - Panel
+ * Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com>.
+ * Translated by github.com/DRSchlaubi/
+ *
+ * This software is licensed under the terms of the MIT license.
+ * https://opensource.org/licenses/MIT
+ */
+return [
+    'notices' => [
+        'created' => 'Das neue Nest :name wurde erstellt',
+        'deleted' => 'Nest erfolgreich gelöscht.',
+        'updated' => 'Nest erfolgreich bearbeitet.',
+    ],
+    'eggs' => [
+        'notices' => [
+            'imported' => 'Eg erfolgreich importiert.',
+            'updated_via_import' => 'Dieses Egg wurde aktualisiert',
+            'deleted' => 'Dieses Egg wurde gelöscht',
+            'updated' => 'Egg wurde erfolgreich bearbeitet.',
+            'script_updated' => 'Egg install script wurde aktualisiert.',
+            'egg_created' => 'A new egg was laid successfully. You will need to restart any running daemons to apply this new egg.',
+        ],
+    ],
+    'variables' => [
+        'notices' => [
+            'variable_deleted' => 'Die Variable ":variable" wurde gelöscht',
+            'variable_updated' => 'Die Variable ":variable" wurde aktualisiert du musst die Rebuild Funktion jedes Servers benutzen.',
+            'variable_created' => 'Eine neue Variable wurde erstellt und dem Egg zugewiesen.',
+        ],
+    ],
+];
