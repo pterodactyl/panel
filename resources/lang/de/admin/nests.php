e3596eae38ec9f21efeeffd3ced2e586596ea955
Michael R.|SchlaumeierTV <michael@rittmeister.in>
2017-11-04 16:39:57 +0100

    Update nests.php

--- resources/lang/de/admin/nests.php
+++ resources/lang/de/admin/nests.php
@@ -2,7 +2,7 @@
 /**
  * Pterodactyl - Panel
  * Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com>.
- * Translated by Michael Rittmeister <mail@schlaubi.net>
+ * Translated by Michael Rittmeister <mail@schlaubi.net>.
  *
  * This software is licensed under the terms of the MIT license.
  * https://opensource.org/licenses/MIT
