467b84d33592e1c8e934a606e9eb8062eab23da4
Michael R.|SchlaumeierTV <michael@rittmeister.in>
2017-11-04 16:35:40 +0100

    Update messages.php

--- resources/lang/de/command/messages.php
+++ resources/lang/de/command/messages.php
@@ -2,7 +2,7 @@
 /**
  * Pterodactyl - Panel
  * Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com>.
- * Translated by gituhb.com/DRSchlaubi
+ * Translated by https://gituhb.com/DRSchlaubi
  *
  * This software is licensed under the terms of the MIT license.
  * https://opensource.org/licenses/MIT
