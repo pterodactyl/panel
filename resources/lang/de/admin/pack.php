09e83d7538dde59e90e1723b9ab9179571ac65dd
Michael R.|SchlaumeierTV <michael@rittmeister.in>
2017-11-04 16:34:33 +0100

    Update pack.php

--- resources/lang/de/admin/pack.php
+++ resources/lang/de/admin/pack.php
@@ -2,7 +2,7 @@
 /**
  * Pterodactyl - Panel
  * Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com>.
- * Translated by github.com/DRSchlaubi/
+ * Translated by https://github.com/DRSchlaubi/
  *
  * This software is licensed under the terms of the MIT license.
  * https://opensource.org/licenses/MIT
