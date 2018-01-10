a4bad8d44326d2047ab6b49fb4141446f2a955cc
Diamax10 <georget.florian.1@gmail.com>
2018-01-10 20:12:08 +0100

    Create auth.php in French
    Translation French

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'not_authorized' => 'Vous n\'avez pas la permission de faire cela.',
+    'auth_error' => 'Une erreur s\'est produite lors de la tentative de connexion.',
+    'authentication_required' => 'Vous devez vous identifier pour continuer',
+    'remember_me' => 'Se souvenir de moi',
+    'sign_in' => 'Se connecter',
+    'forgot_password' => 'J\'ai oublié mon mot de passe !',
+    'request_reset_text' => 'Vous avez oublié votre mot de passe ? Ce n\'est pas la fin du monde, entrez juste votre email ci-dessous.',
+    'reset_password_text' => 'Réinitialiser votre mot de passe.',
+    'reset_password' => 'Réinitialiser le mot de passe',
+    'email_sent' => 'Un email vient de vous être envoyé avec les instructions pour réinitialiser votre mot de passe.',
+    'failed' => 'Les informations rentrées ne correspondent pas ou votre clé 2FA est invalide.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
+    'password_requirements' => 'Les mots de passe doivent contenir au minimum une majuscule, une minuscule, un chiffre et doivent comporter au moins 8 caractères.',
+    'request_reset' => 'Trouver le compte',
+    '2fa_required' => '2-Factor Authentication',
+    '2fa_failed' => 'La clé 2FA est invalide.',
+    'totp_failed' => 'Une erreur s'est produite lors de la tentative de validation de TOTP.',
+    '2fa_must_be_enabled' => 'L'administrateur a demandé que l'authentification à 2 facteurs (2FA) soit activée pour votre compte afin d'utiliser le panel.',
+];
