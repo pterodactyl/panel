98ae0d9876f79a135b93d3ae086fc3b935c97492
Anand Capur <git@anand.io>
2018-02-09 16:55:12 -0800

    some minor fixes, also BYE PHP 7.0/7.1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Cache;
 use Pterodactyl\Models\User;
 use Pterodactyl\Models\Server;
 use Pterodactyl\Models\Subuser;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Pterodactyl\Observers\UserObserver;
@@ -28,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
 
         View::share('appVersion', $this->versionData()['version'] ?? 'undefined');
         View::share('appIsGit', $this->versionData()['is_git'] ?? false);
+        
+        Paginator::useBootstrapThree();
     }
 
     /**
--- config/logging.php
+++ config/logging.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'daily'),
+    'default' => env('LOG_CHANNEL', 'stack'),
 
     /*
     |--------------------------------------------------------------------------
@@ -32,7 +32,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily'],
         ],
 
         'single' => [
