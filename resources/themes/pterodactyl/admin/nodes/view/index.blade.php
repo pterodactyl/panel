68960feeead8a2f884ded46f229d1674ff567e84
stanjg <stan@gabes.nl>
2018-03-15 13:51:04 +0100

    Added description field to nodes

--- resources/themes/pterodactyl/admin/nodes/view/index.blade.php
+++ resources/themes/pterodactyl/admin/nodes/view/index.blade.php
@@ -19,131 +19,143 @@
 @endsection
 
 @section('content')
-<div class="row">
-    <div class="col-xs-12">
-        <div class="nav-tabs-custom nav-tabs-floating">
-            <ul class="nav nav-tabs">
-                <li class="active"><a href="{{ route('admin.nodes.view', $node->id) }}">About</a></li>
-                <li><a href="{{ route('admin.nodes.view.settings', $node->id) }}">Settings</a></li>
-                <li><a href="{{ route('admin.nodes.view.configuration', $node->id) }}">Configuration</a></li>
-                <li><a href="{{ route('admin.nodes.view.allocation', $node->id) }}">Allocation</a></li>
-                <li><a href="{{ route('admin.nodes.view.servers', $node->id) }}">Servers</a></li>
-            </ul>
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-sm-8">
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="box box-primary">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Information</h3>
-                    </div>
-                    <div class="box-body table-responsive no-padding">
-                        <table class="table table-hover">
-                            <tr>
-                                <td>Daemon Version</td>
-                                <td><code data-attr="info-version"><i class="fa fa-refresh fa-fw fa-spin"></i></code> (Latest: <code>{{ $version->getDaemon() }}</code>)</td>
-                            </tr>
-                            <tr>
-                                <td>System Information</td>
-                                <td data-attr="info-system"><i class="fa fa-refresh fa-fw fa-spin"></i></td>
-                            </tr>
-                            <tr>
-                                <td>Total CPU Cores</td>
-                                <td data-attr="info-cpus"><i class="fa fa-refresh fa-fw fa-spin"></i></td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-12">
-                <div class="box box-danger">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Delete Node</h3>
-                    </div>
-                    <div class="box-body">
-                        <p class="no-margin">Deleting a node is a irreversable action and will immediately remove this node from the panel. There must be no servers associated with this node in order to continue.</p>
-                    </div>
-                    <div class="box-footer">
-                        <form action="{{ route('admin.nodes.view.delete', $node->id) }}" method="POST">
-                            {!! csrf_field() !!}
-                            {!! method_field('DELETE') !!}
-                            <button type="submit" class="btn btn-danger btn-sm pull-right" {{ ($node->servers_count < 1) ?: 'disabled' }}>Yes, Delete This Node</button>
-                        </form>
-                    </div>
-                </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="nav-tabs-custom nav-tabs-floating">
+                <ul class="nav nav-tabs">
+                    <li class="active"><a href="{{ route('admin.nodes.view', $node->id) }}">About</a></li>
+                    <li><a href="{{ route('admin.nodes.view.settings', $node->id) }}">Settings</a></li>
+                    <li><a href="{{ route('admin.nodes.view.configuration', $node->id) }}">Configuration</a></li>
+                    <li><a href="{{ route('admin.nodes.view.allocation', $node->id) }}">Allocation</a></li>
+                    <li><a href="{{ route('admin.nodes.view.servers', $node->id) }}">Servers</a></li>
+                </ul>
             </div>
         </div>
     </div>
-    <div class="col-sm-4">
-        <div class="box box-primary">
-            <div class="box-header with-border">
-                <h3 class="box-title">At-a-Glance</h3>
+    <div class="row">
+        <div class="col-sm-8">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="box box-primary">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Information</h3>
+                        </div>
+                        <div class="box-body table-responsive no-padding">
+                            <table class="table table-hover">
+                                <tr>
+                                    <td>Daemon Version</td>
+                                    <td><code data-attr="info-version"><i class="fa fa-refresh fa-fw fa-spin"></i></code> (Latest: <code>{{ $version->getDaemon() }}</code>)</td>
+                                </tr>
+                                <tr>
+                                    <td>System Information</td>
+                                    <td data-attr="info-system"><i class="fa fa-refresh fa-fw fa-spin"></i></td>
+                                </tr>
+                                <tr>
+                                    <td>Total CPU Cores</td>
+                                    <td data-attr="info-cpus"><i class="fa fa-refresh fa-fw fa-spin"></i></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                @if ($node->description)
+                    <div class="col-xs-12">
+                        <div class="box box-default">
+                            <div class="box-header with-border">
+                                Description
+                            </div>
+                            <div class="box-body table-responsive">
+                                <pre>{{ $node->description }}</pre>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+                <div class="col-xs-12">
+                    <div class="box box-danger">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Delete Node</h3>
+                        </div>
+                        <div class="box-body">
+                            <p class="no-margin">Deleting a node is a irreversable action and will immediately remove this node from the panel. There must be no servers associated with this node in order to continue.</p>
+                        </div>
+                        <div class="box-footer">
+                            <form action="{{ route('admin.nodes.view.delete', $node->id) }}" method="POST">
+                                {!! csrf_field() !!}
+                                {!! method_field('DELETE') !!}
+                                <button type="submit" class="btn btn-danger btn-sm pull-right" {{ ($node->servers_count < 1) ?: 'disabled' }}>Yes, Delete This Node</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="box-body">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="info-box bg-{{ $stats['disk']['css'] }}">
-                            <span class="info-box-icon"><i class="ion ion-ios-folder-outline"></i></span>
-                            <div class="info-box-content" style="padding: 15px 10px 0;">
-                                <span class="info-box-text">Disk Space Allocated</span>
-                                <span class="info-box-number">{{ $stats['disk']['value'] }} / {{ $stats['disk']['max'] }} Mb</span>
-                                <div class="progress">
-                                    <div class="progress-bar" style="width: {{ $stats['disk']['percent'] }}%"></div>
+        </div>
+        <div class="col-sm-4">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">At-a-Glance</h3>
+                </div>
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="info-box bg-{{ $stats['disk']['css'] }}">
+                                <span class="info-box-icon"><i class="ion ion-ios-folder-outline"></i></span>
+                                <div class="info-box-content" style="padding: 15px 10px 0;">
+                                    <span class="info-box-text">Disk Space Allocated</span>
+                                    <span class="info-box-number">{{ $stats['disk']['value'] }} / {{ $stats['disk']['max'] }} Mb</span>
+                                    <div class="progress">
+                                        <div class="progress-bar" style="width: {{ $stats['disk']['percent'] }}%"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-12">
+                            <div class="info-box bg-{{ $stats['memory']['css'] }}">
+                                <span class="info-box-icon"><i class="ion ion-ios-barcode-outline"></i></span>
+                                <div class="info-box-content" style="padding: 15px 10px 0;">
+                                    <span class="info-box-text">Memory Allocated</span>
+                                    <span class="info-box-number">{{ $stats['memory']['value'] }} / {{ $stats['memory']['max'] }} Mb</span>
+                                    <div class="progress">
+                                        <div class="progress-bar" style="width: {{ $stats['memory']['percent'] }}%"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-12">
+                            <div class="info-box bg-gray">
+                                <span class="info-box-icon"><i class="ion ion-social-buffer-outline"></i></span>
+                                <div class="info-box-content" style="padding: 23px 10px 0;">
+                                    <span class="info-box-text">Total Servers</span>
+                                    <span class="info-box-number">{{ $node->servers_count }}</span>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-sm-12">
-                        <div class="info-box bg-{{ $stats['memory']['css'] }}">
-                            <span class="info-box-icon"><i class="ion ion-ios-barcode-outline"></i></span>
-                            <div class="info-box-content" style="padding: 15px 10px 0;">
-                                <span class="info-box-text">Memory Allocated</span>
-                                <span class="info-box-number">{{ $stats['memory']['value'] }} / {{ $stats['memory']['max'] }} Mb</span>
-                                <div class="progress">
-                                    <div class="progress-bar" style="width: {{ $stats['memory']['percent'] }}%"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-12">
-                        <div class="info-box bg-gray">
-                            <span class="info-box-icon"><i class="ion ion-social-buffer-outline"></i></span>
-                            <div class="info-box-content" style="padding: 23px 10px 0;">
-                                <span class="info-box-text">Total Servers</span>
-                                <span class="info-box-number">{{ $node->servers_count }}</span>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @section('footer-scripts')
     @parent
     <script>
-    (function getInformation() {
-        $.ajax({
-            method: 'GET',
-            url: '{{ $node->scheme }}://{{ $node->fqdn }}:{{ $node->daemonListen }}/v1',
-            timeout: 5000,
-            headers: {
-                'X-Access-Token': '{{ $node->daemonSecret }}'
-            },
-        }).done(function (data) {
-            $('[data-attr="info-version"]').html(data.version);
-            $('[data-attr="info-system"]').html(data.system.type + '(' + data.system.arch + ') <code>' + data.system.release + '</code>');
-            $('[data-attr="info-cpus"]').html(data.system.cpus);
-        }).fail(function (jqXHR) {
+        (function getInformation() {
+            $.ajax({
+                method: 'GET',
+                url: '{{ $node->scheme }}://{{ $node->fqdn }}:{{ $node->daemonListen }}/v1',
+                timeout: 5000,
+                headers: {
+                    'X-Access-Token': '{{ $node->daemonSecret }}'
+                },
+            }).done(function (data) {
+                $('[data-attr="info-version"]').html(data.version);
+                $('[data-attr="info-system"]').html(data.system.type + '(' + data.system.arch + ') <code>' + data.system.release + '</code>');
+                $('[data-attr="info-cpus"]').html(data.system.cpus);
+            }).fail(function (jqXHR) {
 
-        }).always(function() {
-            setTimeout(getInformation, 10000);
-        });
-    })();
+            }).always(function() {
+                setTimeout(getInformation, 10000);
+            });
+        })();
     </script>
 @endsection
