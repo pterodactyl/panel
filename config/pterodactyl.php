ca794152c46618c7ffe98936514737b32f0d0491
stanjg <stan@gabes.nl>
2018-05-04 14:00:03 +0200

    Renamed the .env entry

--- config/pterodactyl.php
+++ config/pterodactyl.php
@@ -161,7 +161,7 @@ return [
     */
     'lang' => [
         'in_context' => env('PHRASE_IN_CONTEXT', false),
-        'global' => env('LANG_MUST_BE_GLOBAL', false),
+        'global' => env('APP_LOCALE_IS_GLOBAL', false),
     ],
 
     /*
