7e510c3a97b5aa132700f6872fb8fb601bd4c020
BlobCodes <thestrangegamershd@gmail.com>
2018-05-24 09:51:29 +0200

    Update messages.php

--- resources/lang/de/command/messages.php
+++ resources/lang/de/command/messages.php
@@ -5,14 +5,12 @@ return [
         'no_location_found' => 'Shortcode wurde nicht gefunden.',
         'ask_short' => 'Location Short Code',
         'ask_long' => 'Location Beschreibung',
-
         'created' => 'Neue  Location (:name) mit der ID :id erstellt.',
         'deleted' => 'Location gelöscht.',
     ],
     'user' => [
         'search_users' => 'Gib einen Benutzernamen, eine UUID oder eine Email an',
         'select_search_user' => 'ID des Benutzers (Gib \'0\' ein, um erneut zu suchen)',
-
         'deleted' => 'Benutzer erfolgreich gelöscht.',
         'confirm_delete' => 'Bist du dir wirklich sicher?',
         'no_users_found' => 'Es wurden keine Benutzer gefunden.',
@@ -23,18 +21,15 @@ return [
         'ask_name_first' => 'Vorname',
         'ask_name_last' => 'Nachname',
         'ask_password' => 'Passwort',
-
         'ask_password_tip' => 'Wenn du das wirklich tun willst, drücke Strg+C und benutze das `--no-password` Flag.',
         'ask_password_help' => 'Das Passwort muss Zahlen, Groß- und Kleinbuchstaben enthalten und mindestens 8 Zeichen lang sein.',
         '2fa_help_text' => [
             'Dieser Befehl deaktiviert die 2-Faktor-Authentifizierung für das Konto eines Benutzers, wenn sie aktiviert ist. Dies sollte nur dann als Befehl zur Wiederherstellung eines Kontos verwendet werden, wenn der Benutzer von seinem Konto ausgeschlossen ist.',
             'Wenn das nicht das ist, was du tun wolltest, drücke Strg+C um den Prozess zu beenden.',
-
         ],
         '2fa_disabled' => '2-Faktor-Authentifizierung wurde für :email deaktiviert.',
     ],
     'schedule' => [
-
         'output_line' => 'Geplanter Job für die erste Aufgabe in `:schedule` (:hash).',
     ],
     'maintenance' => [
@@ -42,16 +37,13 @@ return [
     ],
     'server' => [
         'rebuild_failed' => 'Neubauanforderung für ":name" (#:id) auf Node ":node" ist gescheitert mit Fehler: :message',
-
     ],
     'environment' => [
         'mail' => [
             'ask_smtp_host' => 'SMTP Host (z.B. smtp.google.com)',
             'ask_smtp_port' => 'SMTP Port',
             'ask_smtp_username' => 'SMTP Benutzername',
-
             'ask_smtp_password' => 'SMTP Passwort',
-
             'ask_mailgun_domain' => 'Mailgun Domain',
             'ask_mailgun_secret' => 'Mailgun Secret',
             'ask_mandrill_secret' => 'Mandrill Secret',
@@ -62,7 +54,6 @@ return [
             'ask_encryption' => 'Zu verwendende Verschlüsselungsmethode',
         ],
         'database' => [
-
             'host_warning' => 'Es wird dringend empfohlen, "localhost" nicht als Datenbank-Host zu verwenden, da wir häufige Socket-Verbindungsprobleme gesehen haben. Wenn du eine lokale Verbindung verwenden willst, solltest du "127.0.0.1" verwenden.',
             'host' => 'Datenbank Host',
             'port' => 'Datenbank Port',
@@ -74,8 +65,6 @@ return [
             'connection_error' => 'Die Verbindung zum MySQL-Server kann mit den angegebenen Anmeldeinformationen nicht hergestellt werden. Der zurückgegebene Fehler war ":error".',
             'creds_not_saved' => 'Deine Verbindungsdaten wurden NICHT gespeichert. Du musst gültige Verbindungsinformationen angeben, bevor du fortfahren kannst.',
             'try_again' => 'Zurückgehen und es nochmal versuchen?',
-
-
         ],
         'app' => [
             'app_url_help' => 'Die Anwendungs URL MUSS mit https:// oder http:// beginnen, je nach dem ob du SSL verwendest oder nicht. Solltest du das Schema nicht beinhalten, werden deine E-Mails und anderer Inhalt an die falsche Stelle verlinken.',
