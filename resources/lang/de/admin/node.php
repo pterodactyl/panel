5f057d3aff062c02cfaf2040bcd2fdde279df3bb
fabm3n <11352551+fabm3n@users.noreply.github.com>
2018-05-24 20:41:05 +0200

    Some adjustments in the German language files

--- resources/lang/de/admin/node.php
+++ resources/lang/de/admin/node.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * Pterodactyl - Panel
+ * Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com>.
+ *
+ * This software is licensed under the terms of the MIT license.
+ * https://opensource.org/licenses/MIT
+ */
+ 
 return [
     'validation' => [
         'fqdn_not_resolvable' => 'Diese Domain scheint nicht auf eine IP weiterzuleiten.',
--- resources/lang/de/admin/pack.php
+++ resources/lang/de/admin/pack.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * Pterodactyl - Panel
+ * Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com>.
+ *
+ * This software is licensed under the terms of the MIT license.
+ * https://opensource.org/licenses/MIT
+ */
+ 
 return [
     'notices' => [
         'pack_updated' => 'Pack erfolgreich aktualisiert',
