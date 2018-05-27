d2c770d21823c0f71ac14f30e9edd86eb5a15d16
Jakob Schrettenbrunner <dev@schrej.net>
2018-05-28 00:35:54 +0200

    add new favicon

--- resources/themes/pterodactyl/templates/wrapper.blade.php
+++ resources/themes/pterodactyl/templates/wrapper.blade.php
@@ -7,6 +7,15 @@
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
             <meta name="csrf-token" content="{{ csrf_token() }}">
+
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+            <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
+            <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
+            <link rel="manifest" href="/favicons/manifest.json">
+            <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#bc6e3c">
+            <link rel="shortcut icon" href="/favicons/favicon.ico">
+            <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+            <meta name="theme-color" content="#0e4688">
         @show
 
         @section('assets')
@@ -15,12 +24,14 @@
 
         @include('layouts.scripts')
     </head>
-    <body class="{{ $css['body'] ?? 'bg-grey-lighter' }}">
-        @yield('above-container')
-        <div class="container" id="pterodactyl">
-            @yield('container')
+    <body class="{{ $css['body'] ?? 'bg-white' }}">
+        <div id="pterodactyl" class="flex flex-col min-h-screen">
+            @yield('above-container')
+            <div class="container">
+                @yield('container')
+            </div>
+            @yield('below-container')
         </div>
-        @yield('below-container')
         @section('scripts')
             {!! $asset->js('assets/scripts/bundle.js') !!}
         @show
