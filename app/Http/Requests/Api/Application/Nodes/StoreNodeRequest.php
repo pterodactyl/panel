569a836e546a6f3c24bc6b50e464e06b3af5b98d
Dane Everitt <dane@daneeveritt.com>
2018-06-02 14:33:33 -0700

    Update StoreNodeRequest.php

--- app/Http/Requests/Api/Application/Nodes/StoreNodeRequest.php
+++ app/Http/Requests/Api/Application/Nodes/StoreNodeRequest.php
@@ -75,7 +75,7 @@ class StoreNodeRequest extends ApplicationApiRequest
         $response['daemonListen'] = $response['daemon_listen'];
         $response['daemonSFTP'] = $response['daemon_sftp'];
         $response['daemonBase'] = $response['daemon_base'] ?? (new Node)->getAttribute('daemonBase');
-        
+
         unset($response['daemon_base'], $response['daemon_listen'], $response['daemon_sftp']);
 
         return $response;
