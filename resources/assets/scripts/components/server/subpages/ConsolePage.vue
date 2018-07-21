c2ebf1cbcd732a939658a0f119b93fcc4ed280c7
Dane Everitt <dane@daneeveritt.com>
2018-07-21 00:04:56 -0700

    Neaten up the console a bit more

--- resources/assets/scripts/components/server/subpages/ConsolePage.vue
+++ resources/assets/scripts/components/server/subpages/ConsolePage.vue
@@ -1,10 +1,19 @@
 <template>
     <div>
         <div class="text-xs font-mono">
-            <div class="rounded-t p-2 bg-black text-grey-lightest overflow-scroll" style="min-height: 16rem;max-height:32rem;">
-                <span class="block" v-for="line in console">{{line}}</span>
+            <div class="rounded-t p-2 bg-black overflow-scroll w-full" style="min-height: 16rem;max-height:32rem;">
+                <div class="mb-2">
+                    <span class="block text-grey-light" v-for="line in console" v-text="line"></span>
+                </div>
             </div>
-            <div class="rounded-b p-2 bg-grey-darkest text-white">$</div>
+            <div class="rounded-b bg-grey-darkest text-white flex">
+                <div class="flex-no-shrink p-2">
+                    <span class="font-bold">$</span>
+                </div>
+                <div class="w-full">
+                    <input type="text" aria-label="Send console command" class="bg-transparent text-white p-2 pl-0 w-full" placeholder="enter command and press enter to send">
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -18,11 +27,5 @@
         computed: {
             ...mapState('server', ['console']),
         },
-
-        data: function () {
-            return {
-                lines: [],
-            };
-        }
     };
 </script>
