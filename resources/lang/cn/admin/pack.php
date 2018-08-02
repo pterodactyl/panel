89dce80cc1dba280bb79cbeae5612d59c523188d
lunarbunnys <1255655119@qq.com>
2018-08-02 11:08:51 +0800

    Add Simplified Chinese translation for the panel

---
+++ resources/lang/cn/admin/pack.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Pterodactyl - Panel
+ * Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com>.
+ *
+ * This software is licensed under the terms of the MIT license.
+ * https://opensource.org/licenses/MIT
+ */
+
+return [
+    'notices' => [
+        'pack_updated' => '整合包已经被更新。',
+        'pack_deleted' => '成功删除整合包： ":name" 。',
+        'pack_created' => '一个整合包已被成功创建，现在可以用它来部署服务器了。',
+    ],
+];
