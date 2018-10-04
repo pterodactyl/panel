268fc257fa252178fbac6ef97019e9a75e005e5e
gabeoliver-bicode <37226472+gabeoliver-bicode@users.noreply.github.com>
2018-10-04 10:29:56 -0400

    creates controller for modmanager

---
+++ app/Http/Controllers/Server/Tools/ModManagerController.php
@@ -0,0 +1,10 @@
+<?php
+namespace Pterodactyl\Http\Controllers\Server\Tools;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Pterodactyl\Http\Controllers\Controller;
+use Pterodactyl\Traits\Controllers\JavascriptInjection;
+use Pterodactyl\Contracts\Repository\ServerRepositoryInterface;
+class ModManagerController extends Controller
+{
+}
