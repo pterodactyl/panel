0c3df9f23adcfd23b6878e44a87e8a80394b7e0b
jer3m01 <jer3m01@jer3m01.com>
2019-02-17 17:40:33 +0100

    Forgot to add files to git

---
+++ app/Console/Commands/Environment/OAuth2SettingsCommand.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Pterodactyl\Console\Commands\Environment;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Config\Repository as ConfigRepository;
+use Pterodactyl\Exceptions\PterodactylException;
+use Pterodactyl\Traits\Commands\EnvironmentWriterTrait;
+
+class OAuth2SettingsCommand extends Command
+{
+    use EnvironmentWriterTrait;
+
+    /**
+     * @var \Illuminate\Contracts\Console\Kernel
+     */
+    protected $command;
+
+    /**
+     * @var \Illuminate\Contracts\Config\Repository
+     */
+    protected $config;
+
+    /**
+     * @var string
+     */
+    protected $description = 'Configure OAuth2 environment settings for the Panel.';
+
+    /**
+     * @var string
+     */
+    protected $signature = 'p:environment:oauth2
+                            {--clientId= : The client ID assigned to you by the provider.}
+                            {--clientSecret= : The client password assigned to you by the provider.}
+                            {--redirectUri= : The URI to which you should be redirected to after authentication.}
+                            {--urlAuthorize= : The URL of the Authorization Endpoint.}
+                            {--urlAccessToken= : The URL of the Token Endpoint.}
+                            {--urlResourceOwnerDetails= : The URL of the User Info Endpoint.}
+                            {--urlRevoke= : The URL to revoke access from your OAuth2 server/provider.}
+                            {--proxy= : The URL and port of the Proxy.}
+                            {--idKey= : The key for getting the user\'s id from the OAuth2 server.}
+                            {--usernameKey= : The key for getting the user\'s username from the OAuth2 server.}
+                            {--emailKey= : The key for getting the user\'s email from the OAuth2 server.}
+                            {--firstNameKey= : The key for getting the user\'s first name from the OAuth2 server.}
+                            {--lastNameKey= : The key for getting the user\'s last name from the OAuth2 server.}
+                            {--createUser= : Whether or not to create a user if he doesnt exist on the system using OAuth2 resources.}
+                            {--updateUser= : Whether or not to update the user\'s details using OAuth2 resources after each login.}';
+
+    /**
+     * @var array
+     */
+    protected $variables = [];
+
+    /**
+     * @var array
+     */
+    protected $unset = [];
+
+    /**
+     * OauthSettingsCommand constructor.
+     *
+     * @param \Illuminate\Contracts\Config\Repository $config
+     */
+    public function __construct(ConfigRepository $config)
+    {
+        parent::__construct();
+
+        $this->config = $config;
+    }
+
+    /**
+     * Handle command execution.
+     *
+     * @throws \Pterodactyl\Exceptions\PterodactylException
+     */
+    public function handle()
+    {
+        $this->output->warning(trans('command/messages.environment.oauth2.oauth2_warning'));
+        $this->variables['OAUTH2_CLIENT_ID'] = $this->option('clientId') ?? $this->ask(
+                trans('command/messages.environment.oauth2.clientId'), $this->config->get('oauth2.options.clientId', 'pterodactyl')
+            );
+
+        $this->variables['OAUTH2_CLIENT_SECRET'] = $this->option('clientSecret') ?? $this->ask(
+                trans('command/messages.environment.oauth2.clientSecret'), env('OAUTH2_CLIENT_SECRET')
+            );
+
+        $this->variables['OAUTH2_URL_AUTHORIZE'] = $this->option('urlAuthorize') ?? $this->ask(
+                trans('command/messages.environment.oauth2.urlAuthorize'), $this->config->get('oauth2.options.urlAuthorize', 'http://example.com/oauth2/authorize')
+            );
+
+        $this->variables['OAUTH2_URL_ACCESS_TOKEN'] = $this->option('urlAccessToken') ?? $this->ask(
+                trans('command/messages.environment.oauth2.urlAccessToken'), $this->config->get('oauth2.options.urlAccessToken', 'http://example.com/oauth2/token')
+            );
+
+        $this->variables['OAUTH2_URL_RESOURCE_OWNER_DETAILS'] = $this->option('urlResourceOwnerDetails') ?? $this->ask(
+                trans('command/messages.environment.oauth2.urlResourceOwnerDetails'), $this->config->get('oauth2.options.urlResourceOwnerDetails', 'http://example.com/oauth2/resource')
+            );
+
+        $this->variables['OAUTH2_URL_REVOKE'] = $this->option('urlRevoke') ?? $this->ask(
+                trans('command/messages.environment.oauth2.urlRevoke'), env('OAUTH2_URL_REVOKE', 'http://example.com/oauth2/revoke')
+            );
+
+        if ($this->option('proxy') ?? $this->confirm(trans('command/messages.environment.oauth2.ask_proxy'), env('OAUTH2_URL_PROXY_URL') != null)) {
+            $this->variables['OAUTH2_URL_PROXY_URL'] = $this->option('proxy') ?? $this->ask(
+                    trans('command/messages.environment.oauth2.proxy'), $this->config->get('oauth2.options.proxy-options.proxy', '192.168.0.1:8888')
+                );
+        } else {
+            array_push($this->unset, 'OAUTH2_URL_PROXY_URL');
+        }
+
+        $this->output->comment(trans('command/messages.environment.oauth2.resource_keys_help'));
+
+        $this->variables['OAUTH2_ID_KEY'] = $this->option('idKey') ?? $this->ask(
+                trans('command/messages.environment.oauth2.id'), env('OAUTH2_ID_KEY', 'id')
+            );
+
+        $this->variables['OAUTH2_USERNAME_KEY'] = $this->option('usernameKey') ?? $this->ask(
+                trans('command/messages.environment.oauth2.username'),env('OAUTH2_USERNAME_KEY', 'username')
+            );
+
+        $this->variables['OAUTH2_EMAIL_KEY'] = $this->option('emailKey') ?? $this->ask(
+                trans('command/messages.environment.oauth2.email'),env('OAUTH2_EMAIL_KEY', 'email')
+            );
+
+        if ($this->option('firstNameKey') ?? $this->confirm(trans('command/messages.environment.oauth2.ask_first_name'), env('OAUTH2_FIRST_NAME_KEY') != null)) {
+            $this->variables['OAUTH2_FIRST_NAME_KEY'] = $this->option('firstNameKey') ?? $this->ask(
+                    trans('command/messages.environment.oauth2.first_name'),env('OAUTH2_FIRST_NAME_KEY', 'first_name')
+                );
+        } else {
+            array_push($this->unset,'OAUTH2_FIRST_NAME_KEY');
+        }
+
+        if ($this->option('lastNameKey') ?? $this->confirm(trans('command/messages.environment.oauth2.ask_last_name'), env('OAUTH2_LAST_NAME_KEY') != null)) {
+            $this->variables['OAUTH2_LAST_NAME_KEY'] = $this->option('lastNameKey') ?? $this->ask(
+                    trans('command/messages.environment.oauth2.last_name'),env('OAUTH2_LAST_NAME_KEY', 'last_name')
+                );
+        } else {
+            array_push($this->unset,'OAUTH2_LAST_NAME_KEY');
+        }
+
+        $this->variables['OAUTH2_CREATE_USER'] = $this->option('createUser') ?? ($this->choice(
+                trans('command/messages.environment.oauth2.create_user'),
+                [trans('command/messages.environment.oauth2.create_user_options.only_allow_login'), trans('command/messages.environment.oauth2.create_user_options.create')],
+                (env('OAUTH2_CREATE_USER', false) ? '1' : '0')
+            ) == trans('command/messages.environment.oauth2.create_user_options.create') ? 'true' : 'false');
+
+        $this->output->warning(trans(('command/messages.environment.oauth2.create_user_warning.'. ($this->variables['OAUTH2_CREATE_USER'] == true ? 'create' : 'only_allow_login'))));
+
+        $this->variables['OAUTH2_UPDATE_USER'] = $this->option('updateUser') ?? ($this->confirm(
+                trans('command/messages.environment.oauth2.update_user'),env('OAUTH2_UPDATE_USER', true)
+            ) ? 'true' : 'false');
+
+
+        if (!empty($this->unset)) {
+            $path = base_path('.env');
+            if (! file_exists($path)) {
+                throw new PterodactylException('Cannot locate .env file, was this software installed correctly?');
+            }
+
+            $saveContents = file_get_contents($path);
+
+            foreach ($this->unset as $key) {
+                $saveContents = preg_replace('/^' . $key . '=(.*)$/m', '', $saveContents);
+            }
+            file_put_contents($path, $saveContents);
+        }
+
+        $this->writeToEnvironment($this->variables);
+
+        $this->output->note(trans('command/messages.environment.oauth2.setup_finished'));
+        $this->output->warning(trans('command/messages.environment.oauth2.redirect_uri_warning'));
+        $this->output->warning(trans('command/messages.environment.oauth2.not_official'));
+    }
+}
---
+++ app/Http/Controllers/Auth/OAuth2Controller.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Pterodactyl\Http\Controllers\Auth;
+
+use DB;
+use Illuminate\Auth\AuthManager;
+use Illuminate\Contracts\Hashing\Hasher;
+use Illuminate\Http\Request;
+use Illuminate\Support\MessageBag;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use OAuth2;
+use Pterodactyl\Contracts\Repository\UserRepositoryInterface;
+use Pterodactyl\Exceptions\Model\DataValidationException;
+use Pterodactyl\Exceptions\Repository\RecordNotFoundException;
+use Pterodactyl\Http\Controllers\Controller;
+use Pterodactyl\Services\Users\UserCreationService;
+use Pterodactyl\Services\Users\UserUpdateService;
+
+class OAuth2Controller extends Controller
+{
+    /**
+     * @var \Pterodactyl\Services\Users\UserCreationService
+     */
+    protected $creationService;
+
+    /**
+     * @var \Illuminate\Contracts\Hashing\Hasher
+     */
+    private $hasher;
+
+    /**
+     * @var \Illuminate\Auth\AuthManager
+     */
+    private $auth;
+
+    /**
+     * @var \Pterodactyl\Contracts\Repository\UserRepositoryInterface
+     */
+    protected $repository;
+
+    /**
+     * @var \Pterodactyl\Services\Users\UserUpdateService
+     */
+    protected $updateService;
+
+    /**
+     * OAuth2Controller constructor.
+     *
+     * @param \Pterodactyl\Services\Users\UserCreationService           $creationService
+     * @param \Illuminate\Contracts\Hashing\Hasher                      $hasher
+     * @param \Illuminate\Auth\AuthManager                              $auth
+     * @param \Pterodactyl\Contracts\Repository\UserRepositoryInterface $repository
+     * @param \Pterodactyl\Services\Users\UserUpdateService             $updateService
+     */
+    public function __construct(UserCreationService $creationService, Hasher $hasher, AuthManager $auth, UserRepositoryInterface $repository, UserUpdateService $updateService)
+    {
+        $this->creationService = $creationService;
+        $this->hasher = $hasher;
+        $this->auth = $auth;
+        $this->repository = $repository;
+        $this->updateService = $updateService;
+    }
+
+    /**
+     * OAuth2 Login
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse|mixed
+     * @throws DataValidationException
+     */
+    public function login(Request $request)
+    {
+        if ($this->auth->guard()->check()) {
+            return redirect()->route('index');
+        }
+        if (!env('OAUTH2_CLIENT_ID')) abort(404);
+        try {
+            $accessToken = OAuth2::getAccessToken();
+        } catch (IdentityProviderException $e) {
+            return abort(400, $e->getMessage());
+        }
+
+        try {
+            if ($accessToken->hasExpired()) OAuth2::refreshAccessToken();
+        } catch (\Exception $exception) {}
+
+        try {
+            if (env('OAUTH2_UPDATE_USER', true))
+                $resources = OAuth2::getResources(null, null, null, true);
+            else $resources = OAuth2::getResources();
+        } catch (\Exception $e) {
+            return abort(400, $e->getMessage());
+        }
+        // Get the user's Id.
+        $oauth2_id = $resources[env('OAUTH2_ID_KEY', 'id')];
+
+        // Login the user if he already exists
+        if (DB::table('users')->where('oauth2_id', '=', $oauth2_id)->value('oauth2_id') == $oauth2_id) {
+            try {
+                $user = $this->repository->find(DB::table('users')->where('oauth2_id', '=', $oauth2_id)->value('id'));
+            } catch (RecordNotFoundException $e) {
+                return abort(500, $e->getMessage());
+            }
+            // Update the user's details
+            if (env('OAUTH2_UPDATE_USER', true) == true) {
+                $email = $resources[env('OAUTH2_EMAIL_KEY', 'email')];
+                $username = $resources[env('OAUTH2_USERNAME_KEY', 'username')];
+                $name_first = $user->getAttributes()['name_first'];
+                if(env('OAUTH2_FIRST_NAME_KEY') != null) $name_first = $resources[env('OAUTH2_FIRST_NAME_KEY')];
+
+                $name_last = $user->getAttributes()['name_last'];
+                if(env('OAUTH2_LAST_NAME_KEY') != null) $name_last = $resources[env('OAUTH2_LAST_NAME_KEY')];
+                try {
+                    $this->updateService->handle($user, compact('email', 'username', 'name_first', 'name_last'));
+                } catch (\Exception $e) {
+                    abort(500, $e->getMessage());
+                }
+            }
+            // Login
+            $this->auth->guard()->login($user);
+            if ($this->auth->guard()->check()) {
+                return redirect()->route('index');
+            }
+        }
+
+        // Create a new user using the OAuth2 provided info if enabled
+        if (env('OAUTH2_CREATE_USER', false) == true) {
+            $email = $resources[env('OAUTH2_EMAIL_KEY', 'email')];
+            $username = $resources[env('OAUTH2_USERNAME_KEY', 'username')];
+
+            $password = $this->hasher->make(str_random(30));
+
+            $name_first = __('base.account.first_name');
+            if(env('OAUTH2_FIRST_NAME_KEY') != null) $name_first = $resources[env('OAUTH2_FIRST_NAME_KEY')];
+
+            $name_last = __('base.account.last_name');
+            if(env('OAUTH2_LAST_NAME_KEY') != null) $name_last = $resources[env('OAUTH2_LAST_NAME_KEY')];
+
+            $root_admin = false;
+
+            $user = $this->creationService->handle(compact('email', 'username', 'password', 'name_first', 'name_last', 'root_admin', 'oauth2_id'));
+
+            // Login
+            $this->auth->guard()->login($user);
+
+            if ($this->auth->guard()->check()) {
+                return redirect()->route('index');
+            }
+            return abort(500, 'Could not create a user for OAuth2 with the oauth2_id: ' . $oauth2_id . '.');
+        }
+
+        session()->forget([config('oauth2.authorization-code-session-key'),
+            config('oauth2.authorization-state-session-key'),
+            config('oauth2.authorization-access-token-session-key'),
+            config('oauth2.authorization-resources-session-key')]);
+
+        $errors = new MessageBag(['user' => [__('auth.failed')]]);
+
+        return redirect()->route('auth.login')
+            ->withErrors($errors);
+    }
+
+    public function callback()
+    {
+        if ($this->auth->guard()->check()) {
+            return redirect()->route('index');
+        }
+        if (!env('OAUTH2_CLIENT_ID')) abort(404);
+        OAuth2::AuthorizationCodeCallback();
+        return redirect()->route('auth.oauth2');
+    }
+}
