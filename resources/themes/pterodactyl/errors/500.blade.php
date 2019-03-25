a3cbf15eb7ca44224042d0a6bf99b0b96397c8fb
GabeSystems <37226472+gabesystems@users.noreply.github.com>
2019-03-25 11:12:45 -0400

    This will fix anyone with issues related to #1521

---
+++ resources/themes/pterodactyl/errors/500.blade.php
@@ -0,0 +1,26 @@
+{{-- Pterodactyl - Panel --}}
+{{-- Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com> --}}
+
+{{-- This software is licensed under the terms of the MIT license. --}}
+{{-- https://opensource.org/licenses/MIT --}}
+@extends('layouts.error')
+
+@section('title')
+    HTTP 500 ERROR
+@endsection
+
+@section('content-header')
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
+        <div class="box box-danger">
+            <div class="box-body text-center">
+                <h1 class="text-red" style="font-size: 160px !important;font-weight: 100 !important;">500</h1>
+                <p class="text-muted">Whoops! Something went wrong.</p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
