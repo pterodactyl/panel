11ff98e032f48ffc3af7ca4160f46daee7e0d5e7
BuildTools <pascal.de.heij12@gmail.com>
2019-04-13 13:48:34 +0200

    Added pack translation

---
+++ resources/lang/nl/admin/pack.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'notices' => [
+        'pack_updated' => 'Pakket is succesvol bijgewerkt.',
+        'pack_deleted' => 'Het pakket ":test" is succesvol verwijderd van het systeem.',
+        'pack_created' => 'Een nieuw pakket is met succes aangemaakt op het systeem en nu is beschikbaar voor implementatie op de servers.',
+    ],
+];
