9946fed8103bce93a6038466f74570c55eedadef
BuildTools <pascal.de.heij12@gmail.com>
2019-04-13 12:10:18 +0200

    Added navigation translation

---
+++ resources/lang/nl/navigation.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'account' => [
+        'header' => 'ACCOUNT BEHEER',
+        'my_account' => 'Mijn Account',
+        'security_controls' => 'Veiligheidscontroles',
+        'api_access' => 'Account API',
+        'my_servers' => 'Mijn Servers',
+    ],
+    'server' => [
+        'header' => 'SERVER BEHEER',
+        'console' => 'Console',
+        'console-pop' => 'Volledig Scherm Console',
+        'file_management' => 'Bestandsbeheer',
+        'file_browser' => 'Bestandsbrowser',
+        'create_file' => 'Maak Bestand',
+        'upload_files' => 'Upload Bestanden',
+        'subusers' => 'Subgebruikers',
+        'schedules' => 'Schema\'s',
+        'configuration' => 'Configuratie',
+        'port_allocations' => 'Toewijzingsinstellingen',
+        'sftp_settings' => 'SFTP Instellingen',
+        'startup_parameters' => 'Opstartparameters',
+        'databases' => 'Databases',
+        'edit_file' => 'Bewerk Bestand',
+        'admin_header' => 'ADMINISTRATIEF',
+        'admin' => 'Server Configuratie',
+        'server_name' => 'Server Naam',
+    ],
+];
