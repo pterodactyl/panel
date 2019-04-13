3d83a8c8f7c324fd1068f808b020f48c8ce7faf2
BuildTools <pascal.de.heij12@gmail.com>
2019-04-13 13:51:48 +0200

    Added user translation

---
+++ resources/lang/nl/admin/user.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'exceptions' => [
+        'user_has_servers' => 'Kan geen gebruiker verwijderen met actieve servers gekoppeld aan het account. Verwijder eerst hun server(s) voordat u verdergaat.',
+    ],
+    'notices' => [
+        'account_created' => 'Account is succesvol aangemaakt.',
+        'account_updated' => 'Account is succesvol bijgewerkt.',
+    ],
+];
