3f8e55606c4ef6fc11ac45217fa671087a5e0f67
BuildTools <pascal.de.heij12@gmail.com>
2019-04-15 16:30:09 +0200

    Updated Dutch base translation

--- resources/lang/nl/base.php
+++ resources/lang/nl/base.php
@@ -1,129 +1,89 @@
 <?php
 
 return [
-    'account' => [
-        'current_password' => 'Huidig Wachtwoord',
-        'delete_user' => 'Gebruiker Verwijderen',
-        'details_updated' => 'Je account details zijn succesvol veranderd',
-        'email_password' => 'E-mail wachtwoord',
-        'exception' => 'Er is een fout opgetreden tijdens het veranderen van je account.',
-        'first_name' => 'Voornaam',
-        'header' => 'Account Beheer',
-        'header_sub' => 'Beheer uw account details',
-        'invalid_pass' => 'Het opgegeven wachtwoord is ongeldig voor dit account.',
-        'invalid_password' => 'Het opgegeven wachtwoord voor je account is ongeldig.',
-        'last_name' => 'Naam',
-        'new_email' => 'Nieuw e-mail adres',
-        'new_password' => 'Nieuw Wachtwoord',
-        'new_password_again' => 'Herhaal nieuw wachtwoord',
-        'totp_apps' => 'U moet een TOTP ondersteunde applicatie hebben (bijv. Google Authenticator, DUO Mobile, Auth, Enpass) om van deze optie gebruik te kunnen maken.',
-        'totp_checkpoint_help' => 'Bevestig a.u.b. uw TOTP instellingen door de QR code rechts te scannen met de authenticator applicatie op uw smartphone. Vul vervolgens de 6-delige code, die de applicatie genereerde, in het onderstaand veld in. Druk op enter wanneer u klaar bent.',
-        'totp_disable' => 'Schakel 2-delige authenticatie uit.',
-        'totp_disable_help' => 'Om TOTP uit te schakelen op dit account moet je een geldige TOTP token geven. TOTP bescherming zal uitgeschakeld worden als de token geldig is.',
-        'totp_enable' => 'Schakel Two-Factor Authentication in',
-        'totp_enabled' => 'TOTP is nu ingeschakeld op dit account. Klik op de sluit knop om te beëindigen.',
-        'totp_enabled_error' => 'De opgegeven TOTP token kon niet gevalideerd worden. Probeer het aub nogmaals.',
-        'totp_header' => 'Two-Factor Authenticatie',
-        'totp_qr' => 'TOTP QR Code',
-        'totp_token' => 'TOTP Token',
-        'update_email' => 'E-mail Adres Bijwerken',
-        'update_identitity' => 'Identiteit Bijwerken',
-        'update_pass' => 'Wachtwoord Bijwerken',
-        'update_user' => 'Gebruiker Bijwerken',
-        'username_help' => 'Uw gebruikersnaam moet uniek zijn en mag enkel de volgende characters bevatten: :requirements.',
-    ],
-    'api' => [
-        'index' => [
-            'create_new' => 'Creëer nieuwe API sleutel',
-            'header' => 'API toegang',
-            'header_sub' => 'Beheer jouw API sleutels.',
-            'keypair_created' => 'Een API Key-Pair is gegenereerd. Jouw API geheime token is <code>:token</code>. Noteer deze code, hij wordt later niet nog een keer weergegeven.',
-            'list' => 'API sleutels',
-        ],
-        'new' => [
-            'allowed_ips' => [
-                'title' => 'Toegestane IP\'s',
-            ],
-            'base' => [
-                'title' => 'Basis Informatie',
-            ],
-            'descriptive_memo' => [
-                'description' => 'Voeg een korte beschrijving over waarvoor deze API sleutel gebruikt zal worden toe.',
-                'title' => 'Beschrijvende notitie',
-            ],
-            'form_title' => 'Details',
-            'header' => 'Nieuwe API Sleutel',
-            'header_sub' => 'Maak een nieuwe API toegangs sleutel',
-            'node_management' => [
-                'delete' => [
-                    'title' => 'Verwijder node',
-                ],
-                'list' => [
-                    'title' => 'Toon nodes',
-                ],
-            ],
-            'server_management' => [
-                'create' => [
-                    'title' => 'Maak een server',
-                ],
-                'delete' => [
-                    'description' => 'Geeft toegang tot het verwijderen van een server',
-                ],
-                'list' => [
-                    'title' => 'Toon servers',
-                ],
-                'power' => [
-                    'description' => 'Geeft toegang om de aan/uit status van een server te beheren.',
-                ],
-                'title' => 'Server beheer',
-                'unsuspend' => [
-                    'title' => 'Hef schorsing op',
-                ],
-            ],
-            'service_management' => [
-                'list' => [
-                    'title' => 'Toon diensten',
-                ],
-                'view' => [
-                    'title' => 'Toon dienst',
-                ],
-            ],
-            'user_management' => [
-                'create' => [
-                    'title' => 'Gebruiker Aanmaken',
-                ],
-                'delete' => [
-                    'description' => 'Geeft toegang tot het verwijderen van een gebruiker',
-                ],
-                'list' => [
-                    'title' => 'Toon gebruikers',
-                ],
-                'title' => 'Gebruikersbeheer',
-                'update' => [
-                    'title' => 'Gebruiker bijwerken',
-                ],
-                'view' => [
-                    'title' => 'Toon gebruiker',
-                ],
-            ],
-        ],
-    ],
+    'validation_error' => 'Er is een fout opgetreden tijdens het valideren van de data die u heeft opgegeven.',
     'errors' => [
+        'return' => 'Keer terug naar de vorige pagina',
+        'home' => 'Naar Home',
         '403' => [
             'header' => 'Geen toegang',
+            'desc' => 'U heeft geen rechten voor toegang tot deze bron.',
         ],
         '404' => [
             'header' => 'Bestand niet gevonden.',
+            'desc' => 'We konden de opgevraagde bron niet vinden op de server.',
         ],
-        'return' => 'Keer terug naar de vorige pagina',
+        'installing' => [
+            'header' => 'Server Installeren',
+            'desc' => 'De gevraagde server is nog bezig met het voltooien van het installatieproces. Probeer het over een paar minuten opnieuw, u ontvangt een e-mail zodra dit proces is voltooid.',
+        ],
+        'suspended' => [
+            'header' => 'Server Opgeschort',
+            'desc' => 'Deze server is opgeschort en kan niet worden geopend.',
+        ],
+        'maintenance' => [
+            'header' => 'Node Onder Onderhoud',
+            'title' => 'Tijdelijk Onbeschikbaar',
+            'desc' => 'Deze node is onder onderhoud, daarom kan uw server tijdelijk niet worden geopend.',
+        ],
     ],
-    'form_error' => 'De volgende fouten werden gevonden bij het verwerken van dit verzoek.',
+    'index' => [
+        'header' => 'Uw Servers',
+        'header_sub' => 'Servers waar u toegang tot heeft.',
+        'list' => 'Server Lijst',
+    ],
+    'api' => [
+        'index' => [
+            'list' => 'API sleutels',
+            'header' => 'API toegang',
+            'header_sub' => 'Beheer jouw API sleutels.',
+            'create_new' => 'Creëer nieuwe API sleutel',
+            'keypair_created' => 'Een API sleutel is gegenereerd en wordt hieronder weergegeven.',
+        ],
+        'new' => [
+            'header' => 'Nieuwe API Sleutel',
+            'header_sub' => 'Maak een nieuwe API toegangs sleutel.',
+            'form_title' => 'Details',
+            'descriptive_memo' => [
+                'title' => 'Beschrijving',
+                'description' => 'Voeg een korte beschrijving toe als referentie waarvoor deze API sleutel gebruikt zal worden.',
+            ],
+            'allowed_ips' => [
+                'title' => 'Toegestane IP\'s',
+                'description' => 'Voer een door regels gescheiden lijst in van IP\'s die toegang hebben tot de API met behulp van deze sleutel. CIDR-notatie is toegestaan. Laat dit leeg om alle IP-adressen toe te staan.',
+            ],
+        ],
+    ],
+    'account' => [
+        'details_updated' => 'Je account details zijn succesvol veranderd',
+        'invalid_password' => 'Het opgegeven wachtwoord voor je account is ongeldig.',
+        'header' => 'Account Beheer',
+        'header_sub' => 'Beheer uw account details',
+        'update_pass' => 'Wachtwoord Bijwerken',
+        'update_email' => 'E-mail Adres Bijwerken',
+        'current_password' => 'Huidig Wachtwoord',
+        'new_password' => 'Nieuw Wachtwoord',
+        'new_password_again' => 'Herhaal nieuw wachtwoord',
+        'new_email' => 'Nieuw e-mail adres',
+        'first_name' => 'Voornaam',
+        'last_name' => 'Achternaam',
+        'update_identitity' => 'Identiteit Bijwerken',
+        'username_help' => 'Uw gebruikersnaam moet uniek zijn en mag enkel de volgende characters bevatten: :requirements.',
+        'language' => 'Taal',
+    ],
     'security' => [
-        '2fa_header' => '2-factor authenticatie',
+        'session_mgmt_disabled' => 'Uw host heeft de mogelijkheid om accountsessies via deze interface te beheren niet ingeschakeld.',
+        'header' => 'Accountbeveiliging',
+        'header_sub' => 'Beheer actieve sessies en 2-Factor Authenticatie.',
+        'sessions' => 'Actieve Sessies',
+        '2fa_header' => '2-factor Authenticatie',
+        '2fa_token_help' => 'Voer de 2FA-token in die door uw app is gegenereerd (Google Authenticator, Authy, enz.).',
+        'disable_2fa' => 'Schakel 2-Factor Authenticatie uit',
+        '2fa_enabled' => '2-Factor Authenticatie is ingeschakeld voor dit account en is vereist om in te loggen bij het paneel. Als u 2FA wilt uitschakelen, voert u hieronder een geldig token in en verzendt u het formulier.',
+        '2fa_disabled' => '2-Factor Authenticatie is uitgeschakeld voor uw account! Schakel 2FA in om een extra beveiligingsniveau toe te voegen aan uw account.',
+        'enable_2fa' => 'Zet 2-Factor Authenticatie aan',
         '2fa_qr' => 'Configureer 2FA op uw toestel',
-        'enable_2fa' => 'Zet 2-Factor authenticatie aan',
+        '2fa_checkpoint_help' => 'Gebruik de 2FA-applicatie op uw telefoon om een foto van de QR-code aan de linkerkant te maken, of voer handmatig de code in. Zodra u dit hebt gedaan, genereert u een token en voert u dit hieronder in.',
+        '2fa_disable_error' => 'De opgegeven 2FA-token was niet geldig. 2FA is niet uitgeschakeld voor dit account.',
     ],
-    'server_name' => 'Server naam',
-    'validation_error' => 'Er is een fout opgetreden tijdens het valideren van de data die u heeft opgegeven.',
-    'view_as_admin' => 'U bekijkt de server lijst als een administrator. Hierdoor zijn alle servers die geïnstalleerd zijn op het systeem zichtbaar. Alle servers waarvan u de eigenaar bent zijn gemarkeerd met een blauwe bol links van hun naam.',
 ];
