f9f644f4f3e3377b354b92dbe73d2e6a9b4b38ca
BuildTools <pascal.de.heij12@gmail.com>
2019-04-15 14:08:00 +0200

    Updated Dutch passwords translation

--- resources/lang/nl/passwords.php
+++ resources/lang/nl/passwords.php
@@ -2,6 +2,8 @@
 
 return [
     'password' => 'Wachtwoord',
+    'reset' => 'Uw wachtwoord is gereset!',
     'sent' => 'We hebben u een wachtwoord herstel link gestuurd!',
+    'token' => 'Dit wachtwoord herstel token is ongeldig.',
     'user' => 'We kunnen geen gebruiker vinden met dat e-mail adres.',
 ];
