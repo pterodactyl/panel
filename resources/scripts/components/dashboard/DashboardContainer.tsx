cb9536cb246f5eda5dac71c4a749968201d1cf5c
Dane Everitt <dane@daneeveritt.com>
2019-08-17 16:10:13 -0700

    Cleanup

--- resources/scripts/components/dashboard/DashboardContainer.tsx
+++ resources/scripts/components/dashboard/DashboardContainer.tsx
@@ -1,10 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faServer } from '@fortawesome/free-solid-svg-icons/faServer';
-import { faMicrochip } from '@fortawesome/free-solid-svg-icons/faMicrochip';
-import { faMemory } from '@fortawesome/free-solid-svg-icons/faMemory';
-import { faHdd } from '@fortawesome/free-solid-svg-icons/faHdd';
-import { faEthernet } from '@fortawesome/free-solid-svg-icons/faEthernet';
 import { Server } from '@/api/server/getServer';
 import getServers from '@/api/getServers';
 import ServerRow from '@/components/dashboard/ServerRow';
