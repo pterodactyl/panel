21abeae1f0c74be8ff621e990e443c200059cd06
LukasW1337 <33436545+LukasW1337@users.noreply.github.com>
2019-10-11 17:26:01 +0200

    Fixing missing arrays

--- public/themes/pterodactyl/js/admin/node/view-servers.js
+++ public/themes/pterodactyl/js/admin/node/view-servers.js
@@ -17,6 +17,10 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+var totalDiskArray = [];
+var totalRamArray = [];
+var totalCPUArray = [];
+
 (function initSocket() {
     if (typeof $.notifyDefaults !== 'function') {
         console.error('Notify does not appear to be loaded.');
