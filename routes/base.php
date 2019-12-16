14b1f13fe07381684de86615c08db8e7b64446ba
Dane Everitt <dane@daneeveritt.com>
2019-12-15 18:11:56 -0800

    Obliterate swaths of old template code

--- routes/base.php
+++ routes/base.php
@@ -6,34 +6,5 @@ Route::get('/account', 'IndexController@index')->name('account');
 Route::get('/locales/{locale}/{namespace}.json', 'LocaleController')
     ->where('namespace', '.*');
 
-/*
-|--------------------------------------------------------------------------
-| Account API Controller Routes
-|--------------------------------------------------------------------------
-|
-| Endpoint: /account/api
-|
-*/
-Route::group(['prefix' => 'account/api'], function () {
-    Route::get('/', 'ClientApiController@index')->name('account.api');
-    Route::get('/new', 'ClientApiController@create')->name('account.api.new');
-    Route::post('/new', 'ClientApiController@store');
-    Route::delete('/revoke/{identifier}', 'ClientApiController@delete')->name('account.api.revoke');
-});
-
-/*
-|--------------------------------------------------------------------------
-| Account Security Controller Routes
-|--------------------------------------------------------------------------
-|
-| Endpoint: /account/security
-|
-*/
-Route::group(['prefix' => 'account/two_factor'], function () {
-    Route::get('/', 'SecurityController@index')->name('account.two_factor');
-    Route::post('/totp', 'SecurityController@store')->name('account.two_factor.enable');
-    Route::post('/totp/disable', 'SecurityController@delete')->name('account.two_factor.disable');
-});
-
 Route::get('/{react}', 'IndexController@index')
     ->where('react', '^(?!(\/)?(api|auth|admin|daemon)).+');
