605c154812697f1eede56d3b80e3405eb70d7c21
Dane Everitt <dane@daneeveritt.com>
2020-03-22 15:15:42 -0700

    Remove unused controllers

--- routes/server.php
+++ routes/server.php
@@ -6,13 +6,5 @@
  * This software is licensed under the terms of the MIT license.
  * https://opensource.org/licenses/MIT
  */
-Route::get('/credentials', 'CredentialsController@index')->name('server.credentials');
-
-Route::group(['prefix' => '/files'], function () {
-    Route::get('/{directory?}', 'FileController@index')
-        ->name('server.files')
-        ->where('directory', '.*');
-});
-
 Route::get('/')->name('server.index');
 Route::get('/console')->name('server.console');
