449be620071f1fb30500277def1a3969d32ff766
PJZ9n <pjz9n@yahoo.co.jp>
2020-03-27 04:53:36 +0900

    Create passwords.php

---
+++ resources/lang/ja/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'password' => 'パスワードは6文字以上で、確認が一致している必要があります。',
+    'reset' => 'あなたのパスワードは初期化されました！',
+    'sent' => 'パスワードの初期化URLをメールで送信しました！',
+    //"パスワード初期化トークン" can be used instead of "パスワード初期化URL".
+    'token' => 'このパスワード初期化URLは無効です。',
+    'user' => "そのメールアドレスのユーザーは見つかりません。",
+];
