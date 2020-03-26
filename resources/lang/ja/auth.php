f5c65d3ac8ea053fc0877cf92ac158fb43ff0691
PJZ9n <pjz9n@yahoo.co.jp>
2020-03-27 04:52:15 +0900

    Create auth.php

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'not_authorized' => 'この操作を実行する権限がありません。',
+    'auth_error' => 'ログインの試行中にエラーが発生しました。',
+    'authentication_required' => '続行するには認証が必要です。',
+    //This is difficult to translate grammatically.
+    //There is a more correct expression "私を覚えて", but this can be misleading.
+    'remember_me' => 'パスワードを記憶する',
+    'sign_in' => 'サインイン',
+    'forgot_password' => 'パスワードを忘れた',
+    'request_reset_text' => 'アカウントのパスワードを忘れた場合でも、再設定ができます。あなたのメールアドレスを入力して下さい。',
+    'reset_password_text' => 'あなたのアカウントのパスワードを初期化する。',
+    'reset_password' => 'アカウントのパスワードを初期化',
+    'email_sent' => 'パスワードを初期化するための詳細な手順が記載されたメールが送信されました。',
+    'failed' => '入力された情報が、登録されているものと一致しないか、指定された2FAトークンが無効です。',
+    'throttle' => 'ログイン試行が多すぎます。:seconds秒後に再試行してください。',
+    'password_requirements' => 'パスワードには、大文字、小文字、数字を最低1つ含める必要があり、8文字以上にする必要があります。',
+    'request_reset' => 'アカウントを探す',
+    '2fa_required' => '2FA認証',
+    '2fa_failed' => '指定された2FAトークンが無効です。',
+    'totp_failed' => 'TOTPの検証中にエラーが発生しました。',
+    '2fa_must_be_enabled' => '管理者は、コントロールパネルを使用するために、アカウントで2FA認証を有効にする必要があります。',
+];
