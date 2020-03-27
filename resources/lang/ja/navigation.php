f93b34ab6c8e445379894dcba03058f65df2b637
PJZ9n <pjz9n@yahoo.co.jp>
2020-03-27 14:48:49 +0900

    Create navigation.php

---
+++ resources/lang/ja/navigation.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'home' => 'ホーム',
+    'account' => [
+        'header' => 'アカウントの管理',
+        'my_account' => '自分のアカウント',
+        'security_controls' => 'セキュリティ制御',
+        'api_access' => 'アカウントAPI',
+        'my_servers' => '私のサーバー',
+    ],
+    'server' => [
+        'header' => 'サーバー管理',
+        'console' => 'コンソール',
+        'console-pop' => '全画面コンソール',
+        'file_management' => 'ファイル管理',
+        'file_browser' => 'ファイルブラウザ',
+        'create_file' => 'ファイル作成',
+        'upload_files' => 'ファイルアップロード',
+        'subusers' => 'サブユーザー',
+        'schedules' => 'スケジュール',
+        'configuration' => '設定',
+        'port_allocations' => 'アロケーション設定',
+        'sftp_settings' => 'SFTP設定',
+        'startup_parameters' => 'スタートアップパラメーター',
+        'databases' => 'データベース',
+        'edit_file' => 'ファイル編集',
+        'admin_header' => '管理者',
+        'admin' => 'サーバー設定',
+        'server_name' => 'サーバー名',
+    ],
+];
