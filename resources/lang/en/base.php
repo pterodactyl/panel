ec85b927f7b02fa7e26db2a2b723b895845888ac
synocx <62736272+synocx@users.noreply.github.com>
2020-04-14 09:20:53 +0800

    Add files via upload

--- resources/lang/en/base.php
+++ resources/lang/en/base.php
@@ -12,5 +12,18 @@ return [
             'header' => 'File Not Found',
             'desc' => 'We were unable to locate the requested resource on the server.',
         ],
-    ],
+        'installing' => [
+            'header' => 'Server Installing',
+            'desc' => 'The requested server is still completing the install process. Please check back in a few minutes, you should receive an email as soon as this process is completed.',
+        ],
+        'suspended' => [
+            'header' => 'Server Suspended',
+            'desc' => 'This server has been suspended and cannot be accessed.',
+        ],
+        'maintenance' => [
+            'header' => 'Node Under Maintenance',
+            'title' => 'Temporarily Unavailable',
+            'desc' => 'This node is under maintenance, therefore your server can temporarily not be accessed.',
+      ],
+   ], 
 ];
