356fd0b5d5f65136b119f3460141a8c80b62a188
synocx <62736272+synocx@users.noreply.github.com>
2020-04-14 08:59:30 +0800

    Fix 404 and 403 error message

---
+++ resources/lang/en/base.php
@@ -0,0 +1,16 @@
+<?php
+                                                                     
+return [
+    'errors' => [
+        'return' => 'Return to Previous Page',
+        'home' => 'Go Home',
+        '403' => [
+            'header' => 'Forbidden',
+            'desc' => 'You do not have permission to access this resource on this server.',
+        ],
+        '404' => [
+            'header' => 'File Not Found',
+            'desc' => 'We were unable to locate the requested resource on the server.',
+        ],
+    ],
+];
