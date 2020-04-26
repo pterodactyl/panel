941692a060ff2dc8732a9457a912d5865f952ffa
Charles Morgan <sir3lit@gmail.com>
2020-04-26 11:00:38 -0400

    Update 2020_04_22_055500_add_max_connections_column.php
    Fix migration, not sure how I messed that up....... but 👌

--- database/migrations/2020_04_22_055500_add_max_connections_column.php
+++ database/migrations/2020_04_22_055500_add_max_connections_column.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddMaxConnectionsColumnToDatabasesTable extends Migration
+class AddMaxConnectionsColumn extends Migration
 {
     /**
      * Run the migrations.
