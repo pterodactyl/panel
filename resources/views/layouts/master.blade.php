69eb33fb89fa7ce1dd22a88515aaf44a757a799b
Dane Everitt <dane@daneeveritt.com>
2020-05-07 20:24:28 -0700

    closes #1787

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -221,7 +221,7 @@
                         <div class="col-xs-12">
                             @if (count($errors) > 0)
                                 <div class="alert alert-danger">
-                                    @lang('base.validation_error')<br><br>
+                                    There was an error validating the data provided.<br><br>
                                     <ul>
                                         @foreach ($errors->all() as $error)
                                             <li>{{ $error }}</li>
