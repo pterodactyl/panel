57f7aa1638f939f310c27317c1094d8b0ef5f105
Dane Everitt <dane@daneeveritt.com>
2020-05-17 14:20:43 -0700

    Remove unused routes; closes #2039

--- routes/api-remote.php
+++ routes/api-remote.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/authenticate/{token}', 'ValidateKeyController@index');
-Route::post('/download-file', 'FileDownloadController@index');
-
 // Routes for the Wings daemon.
 Route::post('/sftp/auth', 'SftpAuthenticationController');
 
