08472726a1e419896820b81d2b7b23ad60945c7f
Charles Morgan <sir3lit@gmail.com>
2020-06-05 01:05:32 -0400

    Added 'db:backup' to artisan command line
    Allows users to export the panels database to a sql file using mysqldump, uses the config values in the .env, This is reliant on MySQL / MariaDB being installed on the machine the panel is running on.

---
+++ app/Console/Commands/BackupDatabase.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Pterodactyl\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+use Illuminate\Contracts\Config\Repository as ConfigRepository;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+class BackupDatabase extends Command
+{
+    protected $signature = 'db:backup';
+
+    protected $description = 'Provides a exported sql file of the panels current database.';
+
+    /**
+     *
+     * @param \Illuminate\Contracts\Config\Repository $config
+     */
+    public function __construct(ConfigRepository $config)
+    {
+        parent::__construct();
+        $this->config = $config;
+        $this->process = new Process(sprintf(
+            'mysqldump -h %s -P %s -u%s -p%s %s > %s',
+            $this->config->get('database.connections.mysql.host'),
+            $this->config->get('database.connections.mysql.port'),
+            $this->config->get('database.connections.mysql.username'),
+            $this->config->get('database.connections.mysql.password'),
+            $this->config->get('database.connections.mysql.database'),
+            storage_path('../backup.sql')
+        ));
+    }
+    /**
+     * Run the command
+     * @return mixed
+     */
+    public function handle()
+    {
+        try {
+            $this->process->mustRun();
+            $this->info('The database backup has completed successfully.');
+        } catch (ProcessFailedException $exception) {
+            $this->error('The backup process has failed...');
+            $this->error($exception->getMessage());
+        }
+    }
+}
