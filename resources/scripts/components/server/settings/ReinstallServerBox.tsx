0c1f4e14532bd58e16a7c7cfb551b61b1e96e743
Rihan Arfan <me@file.properties>
2020-06-12 21:48:09 +0000

    Reduce font strength

--- resources/scripts/components/server/settings/ReinstallServerBox.tsx
+++ resources/scripts/components/server/settings/ReinstallServerBox.tsx
@@ -46,7 +46,7 @@ export default () => {
             </ConfirmationModal>
             <p className={'text-sm'}>
                 Reinstalling your server will stop it, and then re-run the installation script that initially
-                set it up. <strong className={'font-bold'}>Some files may be deleted or modified during this process,
+                set it up. <strong className={'font-medium'}>Some files may be deleted or modified during this process,
                 please back up your data before continuing.</strong>
             </p>
             <div className={'mt-6 text-right'}>
