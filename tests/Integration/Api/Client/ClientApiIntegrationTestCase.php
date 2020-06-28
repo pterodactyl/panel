b9a451b528cbe1f9aebda6fcfc2d596ecded582b
Dane Everitt <dane@daneeveritt.com>
2020-06-28 13:50:07 -0700

    Add test coverage for schedules

--- tests/Integration/Api/Client/ClientApiIntegrationTestCase.php
+++ tests/Integration/Api/Client/ClientApiIntegrationTestCase.php
@@ -2,12 +2,18 @@
 
 namespace Pterodactyl\Tests\Integration\Api\Client;
 
+use Carbon\Carbon;
+use ReflectionClass;
+use Carbon\CarbonImmutable;
 use Pterodactyl\Models\Node;
 use Pterodactyl\Models\User;
+use Pterodactyl\Models\Model;
 use Pterodactyl\Models\Server;
 use Pterodactyl\Models\Subuser;
 use Pterodactyl\Models\Location;
+use Illuminate\Support\Collection;
 use Pterodactyl\Tests\Integration\IntegrationTestCase;
+use Pterodactyl\Transformers\Api\Client\BaseClientTransformer;
 
 abstract class ClientApiIntegrationTestCase extends IntegrationTestCase
 {
@@ -22,6 +28,17 @@ abstract class ClientApiIntegrationTestCase extends IntegrationTestCase
         User::query()->forceDelete();
 
         parent::tearDown();
+    }
+
+    /**
+     * Setup tests and ensure all of the times are always the same.
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Carbon::setTestNow(Carbon::now());
+        CarbonImmutable::setTestNow(Carbon::now());
     }
 
     /**
@@ -49,5 +66,26 @@ abstract class ClientApiIntegrationTestCase extends IntegrationTestCase
         ]);
 
         return [$user, $server];
+    }
+
+    /**
+     * Asserts that the data passed through matches the output of the data from the transformer. This
+     * will remove the "relationships" key when performing the comparison.
+     *
+     * @param array $data
+     * @param \Pterodactyl\Models\Model|\Illuminate\Database\Eloquent\Model $model
+     */
+    protected function assertJsonTransformedWith(array $data, $model)
+    {
+        $reflect = new ReflectionClass($model);
+        $transformer = sprintf('\\Pterodactyl\\Transformers\\Api\\Client\\%sTransformer', $reflect->getShortName());
+
+        $transformer = new $transformer;
+        $this->assertInstanceOf(BaseClientTransformer::class, $transformer);
+
+        $this->assertSame(
+            $transformer->transform($model),
+            Collection::make($data)->except(['relationships'])->toArray()
+        );
     }
 }
