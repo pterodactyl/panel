34a46a388fe19cad6f94d32a9a3baca61a0c4f64
Dane Everitt <dane@daneeveritt.com>
2020-07-11 11:19:38 -0700

    Update DropdownMenu.tsx

--- resources/scripts/components/elements/DropdownMenu.tsx
+++ resources/scripts/components/elements/DropdownMenu.tsx
@@ -64,7 +64,7 @@ class DropdownMenu extends React.PureComponent<Props, State> {
 
     windowListener = (e: MouseEvent) => {
         const menu = this.menu.current;
-        
+
         if (e.button === 2 || !this.state.visible || !menu) {
             return;
         }
