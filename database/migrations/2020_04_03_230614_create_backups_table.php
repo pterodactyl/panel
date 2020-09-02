631885d60c1a0f38fee99162ec21cf25324c0a53
Dane Everitt <dane@daneeveritt.com>
2020-09-01 20:24:25 -0700

    Handle a plugin not properly namespacing itself and causing migration errors; ref #2291

--- database/migrations/2020_04_03_230614_create_backups_table.php
+++ database/migrations/2020_04_03_230614_create_backups_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -13,6 +14,21 @@ class CreateBackupsTable extends Migration
      */
     public function up()
     {
+        $db = config('database.default');
+        // There exists a backups plugin for the 0.7 version of the Panel. However, it didn't properly
+        // namespace itself so now we have to deal with these tables being in the way of tables we're trying
+        // to use. For now, just rename them to maintain the data.
+        $results = DB::select('SELECT TABLE_NAME FROM information_schema.tables WHERE table_schema = ? AND table_name LIKE ? AND table_name NOT LIKE \'%_plugin_bak\'', [
+            config("database.connections.{$db}.database"),
+            'backup%'
+        ]);
+
+        // Take any of the results, most likely "backups" and "backup_logs" and rename them to have a
+        // suffix so data isn't completely lost, but they're no longer in the way of this migration...
+        foreach ($results as $result) {
+            Schema::rename($result['TABLE_NAME'], $result['TABLE_NAME'] . '_plugin_bak');
+        }
+
         Schema::create('backups', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedInteger('server_id');
