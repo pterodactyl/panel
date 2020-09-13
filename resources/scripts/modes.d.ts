9d7f4f954e01226de818234047d00a997e5b6cff
Matthew Penner <me@matthewp.io>
2020-09-13 17:17:40 -0600

    Replace Ace editor with CodeMirror

--- resources/scripts/modes.d.ts
+++ resources/scripts/modes.d.ts
@@ -1,3 +1,13 @@
-declare const modes: Record<string, string>;
+export interface Mode {
+    name: string,
+    mime: string,
+    mimes?: string[],
+    mode: string,
+    ext: string[],
+    alias?: string[],
+    file?: RegExp,
+}
+
+declare const modes: Mode[];
 
 export default modes;
