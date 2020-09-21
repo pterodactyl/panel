d613ac22a34292084040196eba97f765d5eeed0a
Charles Morgan <sir3lit@gmail.com>
2020-09-20 22:09:58 -0400

    Fix overflows through out the front end
    Fixed Server row overflow for servers name and description
    Fixed Sub-user overflow for users with oddly long emails....
    Fixed Server new overflow on console view.
    
    See Attached images.

--- resources/scripts/components/server/users/UserRow.tsx
+++ resources/scripts/components/server/users/UserRow.tsx
@@ -31,7 +31,7 @@ export default ({ subuser }: Props) => {
                 <img css={tw`w-full h-full`} src={`${subuser.image}?s=400`}/>
             </div>
             <div css={tw`ml-4 flex-1`}>
-                <p css={tw`text-sm truncate`}>{subuser.email}</p>
+                <p css={tw`text-sm break-all`}>{subuser.email}</p>
             </div>
             <div css={tw`ml-4`}>
                 <p css={tw`font-medium text-center`}>
