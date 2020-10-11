a4d7170facb77b85af34c841b928911973abb840
Dane Everitt <dane@daneeveritt.com>
2020-10-10 18:17:04 -0700

    Don't allow creation of a database with an identical name for the same server; closes #2447

--- app/Http/Requests/Api/Client/Servers/Databases/StoreDatabaseRequest.php
+++ app/Http/Requests/Api/Client/Servers/Databases/StoreDatabaseRequest.php
@@ -2,7 +2,11 @@
 
 namespace Pterodactyl\Http\Requests\Api\Client\Servers\Databases;
 
+use Webmozart\Assert\Assert;
+use Pterodactyl\Models\Server;
+use Illuminate\Validation\Rule;
 use Pterodactyl\Models\Permission;
+use Illuminate\Database\Query\Builder;
 use Pterodactyl\Contracts\Http\ClientPermissionsRequest;
 use Pterodactyl\Http\Requests\Api\Client\ClientApiRequest;
 
@@ -21,9 +25,31 @@ class StoreDatabaseRequest extends ClientApiRequest implements ClientPermissions
      */
     public function rules(): array
     {
+        $server = $this->route()->parameter('server');
+
+        Assert::isInstanceOf($server, Server::class);
+
         return [
-            'database' => 'required|alpha_dash|min:3|max:48',
+            'database' => [
+                'required',
+                'alpha_dash',
+                'min:3',
+                'max:48',
+                // Yes, I am aware that you could have the same database name across two unique hosts. However,
+                // I don't really care about that for this validation. We just want to make sure it is unique to
+                // the server itself. No need for complexity.
+                Rule::unique('databases', 'database')->where(function (Builder $query) use ($server) {
+                    $query->where('server_id', $server->id);
+                }),
+            ],
             'remote' => 'required|string|regex:/^[0-9%.]{1,15}$/',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'database.unique' => 'The database name you have selected is already in use by this server.',
         ];
     }
 }
