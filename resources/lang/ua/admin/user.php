e9db6e56c0ceb132d1a4998679c95cd29d370c62
Yaryna Sirko <yarinkasirko@gmail.com>
2020-10-12 22:51:09 +0300

    added the ua lang

---
+++ resources/lang/ua/admin/user.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Pterodactyl - Panel
+ * Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com>.
+ *
+ * This software is licensed under the terms of the MIT license.
+ * https://opensource.org/licenses/MIT
+ */
+
+return [
+    'exceptions' => [
+        'user_has_servers' => 'Не вдається видалити користувача з активними серверами, приєднаними до його облікового запису. Видаліть їх сервери, перш ніж продовжувати. ',
+    ],
+    'notices' => [
+         'account_created' => 'Обліковий запис створено успішно.',
+         'account_updated' => 'Обліковий запис успішно оновлено.',
+    ],
+];
---
+++ resources/lang/ua/passwords.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+	'password' => 'Паролі повинні містити щонайменше шість символів і відповідати підтвердженню.',
+    'reset' => 'Ваш пароль було скинуто!',
+    'sent' => 'Ми надіслали вам посилання для скидання пароля!',
+    'token' => 'Цей маркер скидання пароля недійсний.',
+    'user' => "Ми не можемо знайти користувача з цією адресою електронної пошти.",
+];
