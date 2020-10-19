19295fb10b0a7597f39515b60196ecad0f33f160
DarthShmev <34663790+DarthShmev@users.noreply.github.com>
2020-10-19 14:50:39 -0400

    Update AccountApiContainer.tsx
    Fix api key row container from resizing when hovering over row.

--- resources/scripts/components/dashboard/AccountApiContainer.tsx
+++ resources/scripts/components/dashboard/AccountApiContainer.tsx
@@ -82,7 +82,7 @@ export default () => {
                                 >
                                     <FontAwesomeIcon icon={faKey} css={tw`text-neutral-300`}/>
                                     <div css={tw`ml-4 flex-1 overflow-hidden`}>
-                                        <p css={tw`text-sm break-words`}>{key.description}</p>
+                                        <p css={tw`text-sm break-all`}>{key.description}</p>
                                         <p css={tw`text-2xs text-neutral-300 uppercase`}>
                                             Last used:&nbsp;
                                             {key.lastUsedAt ? format(key.lastUsedAt, 'MMM do, yyyy HH:mm') : 'Never'}
