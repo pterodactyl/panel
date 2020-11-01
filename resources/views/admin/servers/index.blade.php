63f8f533677f18e7b47d02f02f3e9111ab438e65
Dane Everitt <dane@daneeveritt.com>
2020-11-01 12:28:14 -0800

    Fix server pagination when using search queries; closes #2593

--- resources/views/admin/servers/index.blade.php
+++ resources/views/admin/servers/index.blade.php
@@ -26,7 +26,7 @@
                 <div class="box-tools search01">
                     <form action="{{ route('admin.servers') }}" method="GET">
                         <div class="input-group input-group-sm">
-                            <input type="text" name="filter[*]" class="form-control pull-right" value="{{ request()->input('filter[*]') }}" placeholder="Search Servers">
+                            <input type="text" name="filter[*]" class="form-control pull-right" value="{{ request()->input()['filter']['*'] ?? '' }}" placeholder="Search Servers">
                             <div class="input-group-btn">
                                 <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
                                 <a href="{{ route('admin.servers.new') }}"><button type="button" class="btn btn-sm btn-primary" style="border-radius: 0 3px 3px 0;margin-left:-1px;">Create New</button></a>
@@ -75,7 +75,7 @@
             </div>
             @if($servers->hasPages())
                 <div class="box-footer with-border">
-                    <div class="col-md-12 text-center">{!! $servers->appends(['query' => Request::input('query')])->render() !!}</div>
+                    <div class="col-md-12 text-center">{!! $servers->appends(['filter' => Request::input('filter')])->render() !!}</div>
                 </div>
             @endif
         </div>
