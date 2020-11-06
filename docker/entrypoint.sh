80574e991b1e953c2cfbe43327d65713d8b57395
Daave <daave@widgetbot.io>
2020-11-06 13:23:05 +0000

    Remove quotes and fix indentation

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -20,11 +20,11 @@ else
   touch /app/var/.env
 
   ## manually generate a key because key generate --force fails
-  if [ -z "$APP_KEY" ]; then
-      echo -e "Generating key."
-      APP_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-      echo -e "Generated app key: $APP_KEY"
-      echo -e "APP_KEY=$APP_KEY" > /app/var/.env
+  if [ -z $APP_KEY ]; then
+     echo -e "Generating key."
+     APP_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+     echo -e "Generated app key: $APP_KEY"
+     echo -e "APP_KEY=$APP_KEY" > /app/var/.env
   else
     echo -e "APP_KEY exists in environment, using that."
     echo -e "APP_KEY=$APP_KEY" > /app/var/.env
