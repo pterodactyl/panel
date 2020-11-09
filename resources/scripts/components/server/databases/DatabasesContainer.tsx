4a234af7a30c0d53ec78ee8c8bf15e2d5db43ecc
Charles Morgan <sir3lit@gmail.com>
2020-11-08 21:09:22 -0500

    Minor changes
    Changes CopyOnClick to allow any.
    Allows database information to be copied on click.
    Changes layouts on database/backups to match the network tab.
    Changes text to lighten it one level from 400 to 300 for easier visibility.
    Moves database api endpoints to their own folder for some organization.

--- resources/scripts/components/server/databases/DatabasesContainer.tsx
+++ resources/scripts/components/server/databases/DatabasesContainer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import getServerDatabases from '@/api/server/getServerDatabases';
+import getServerDatabases from '@/api/server/databases/getServerDatabases';
 import { ServerContext } from '@/state/server';
 import { httpErrorToHuman } from '@/api/http';
 import FlashMessageRender from '@/components/FlashMessageRender';
@@ -62,17 +62,17 @@ export default () => {
                             </p>
                         }
                         <Can action={'database.create'}>
-                            {(databaseLimit > 0 && databases.length > 0) &&
-                            <p css={tw`text-center text-xs text-neutral-400 mt-2`}>
-                                {databases.length} of {databaseLimit} databases have been allocated to this
-                                server.
-                            </p>
-                            }
-                            {databaseLimit > 0 && databaseLimit !== databases.length &&
-                            <div css={tw`mt-6 flex justify-end`}>
-                                <CreateDatabaseButton/>
+                            <div css={tw`mt-6 sm:flex items-center justify-end`}>
+                                {(databaseLimit > 0 && databases.length > 0) &&
+                                <p css={tw`text-sm text-neutral-300 mb-4 sm:mr-6 sm:mb-0`}>
+                                    {databases.length} of {databaseLimit} databases have been allocated to this
+                                    server.
+                                </p>
+                                }
+                                {databaseLimit > 0 && databaseLimit !== databases.length &&
+                                    <CreateDatabaseButton css={tw`w-full sm:w-auto`}/>
+                                }
                             </div>
-                            }
                         </Can>
                     </>
                 </Fade>
