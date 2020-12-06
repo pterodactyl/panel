d22456d9ca08f8b9a6361603a09be4767bc3e0b6
Dane Everitt <dane@daneeveritt.com>
2020-12-06 13:56:14 -0800

    Block API access when 2FA is required on account; closes #2791

---
+++ resources/scripts/api/interceptors.ts
@@ -0,0 +1,16 @@
+import http from '@/api/http';
+import { AxiosError } from 'axios';
+import { History } from 'history';
+
+export const setupInterceptors = (history: History) => {
+    http.interceptors.response.use(resp => resp, (error: AxiosError) => {
+        if (error.response?.status === 400) {
+            if (error.response?.data.errors?.[0].code === 'TwoFactorAuthRequiredException') {
+                if (!window.location.pathname.startsWith('/account')) {
+                    history.replace('/account', { twoFactorRedirect: true });
+                }
+            }
+        }
+        throw error;
+    });
+};
