b2328b50c69d033ee164973eb4cd913d55159fbc
Dane Everitt <dane@daneeveritt.com>
2020-12-27 10:44:56 -0800

    Fix checkbox design in file manager

--- resources/scripts/components/elements/Input.tsx
+++ resources/scripts/components/elements/Input.tsx
@@ -16,7 +16,7 @@ const light = css<Props>`
 `;
 
 const checkboxStyle = css<Props>`
-    ${tw`cursor-pointer appearance-none inline-block align-middle select-none flex-shrink-0 w-4 h-4 text-primary-400 border border-neutral-300 rounded-sm`};
+    ${tw`bg-neutral-500 cursor-pointer appearance-none inline-block align-middle select-none flex-shrink-0 w-4 h-4 text-primary-400 border border-neutral-300 rounded-sm`};
     color-adjust: exact;
     background-origin: border-box;
     transition: all 75ms linear, box-shadow 25ms linear;
