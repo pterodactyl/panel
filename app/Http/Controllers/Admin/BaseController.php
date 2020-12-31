359769244fca3c4ef4c8f3286a6a082cf9ab9602
Matthew Penner <me@matthewp.io>
2020-12-30 20:19:42 -0700

    Remove un-needed code from Admin/BaseController.php

--- app/Http/Controllers/Admin/BaseController.php
+++ app/Http/Controllers/Admin/BaseController.php
@@ -4,25 +4,9 @@ namespace Pterodactyl\Http\Controllers\Admin;
 
 use Illuminate\View\View;
 use Pterodactyl\Http\Controllers\Controller;
-use Pterodactyl\Services\Helpers\SoftwareVersionService;
 
 class BaseController extends Controller
 {
-    /**
-     * @var \Pterodactyl\Services\Helpers\SoftwareVersionService
-     */
-    private $version;
-
-    /**
-     * BaseController constructor.
-     *
-     * @param \Pterodactyl\Services\Helpers\SoftwareVersionService $version
-     */
-    public function __construct(SoftwareVersionService $version)
-    {
-        $this->version = $version;
-    }
-
     /**
      * Return the admin index view.
      *
@@ -30,7 +14,6 @@ class BaseController extends Controller
      */
     public function index(): View
     {
-        // return view('admin.index', ['version' => $this->version]);
         return view('templates/base.core');
     }
 }
