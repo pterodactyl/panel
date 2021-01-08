52ce5e9a57cacd2ff9077ce54b6bb6295a76c5e7
Matthew Penner <me@matthewp.io>
2021-01-08 15:15:02 -0700

    admin(ui): tweaks to OverviewContainer.tsx

--- resources/scripts/components/admin/overview/OverviewContainer.tsx
+++ resources/scripts/components/admin/overview/OverviewContainer.tsx
@@ -47,7 +47,7 @@ export default () => {
                     </div>
                     :
                     <div css={tw`rounded shadow-md bg-neutral-700`}>
-                        <div css={tw`bg-neutral-900 rounded-t border-b border-black p-3`}>
+                        <div css={tw`bg-neutral-900 rounded-t border-b border-black px-4 py-3`}>
                             <p css={tw`text-sm uppercase`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" css={tw`inline-block mr-2`} style={{ height: '1rem' }}>
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"/>
@@ -57,7 +57,7 @@ export default () => {
                             </p>
                         </div>
 
-                        <div css={tw`p-3`}>
+                        <div css={tw`px-4 py-4`}>
                             {versionData?.panel.current === 'canary' ?
                                 <p css={tw`text-neutral-200`}>
                                     I hope you enjoy living on the edge because you are
