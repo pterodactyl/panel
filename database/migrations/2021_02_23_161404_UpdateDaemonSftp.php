32de521cd0de205a43024424147d3fbc1fbecec7
VinGal <45609819+VinGal0@users.noreply.github.com>
2021-02-23 09:18:32 +0000

    Create 2021_02_23_161404_UpdateDaemonSftp.php

---
+++ database/migrations/2021_02_23_161404_UpdateDaemonSftp.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateDaemonSftp extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('nodes', function (Blueprint $table) {
+            $table->unsignedSmallInteger('daemonSFTP')->default(2022)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('nodes', function (Blueprint $table) {
+            $table->smallInteger('daemonSFTP')->default(2022);
+        });
+    }
+}
