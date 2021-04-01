7122da0a1bc34b89d8a82bd8adc7b425e8d6d18a
jer3m01 <jer3m01@jer3m01.com>
2021-04-01 16:19:07 +0200

    chore: rebase on develop

--- resources/scripts/components/dashboard/forms/SetupOAuthModal.tsx
+++ resources/scripts/components/dashboard/forms/SetupOAuthModal.tsx
@@ -1,19 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Modal, { RequiredModalProps } from '@/components/elements/Modal';
-import { Form, Formik, FormikHelpers } from 'formik';
-import { object, string } from 'yup';
-import Field from '@/components/elements/Field';
-import getTwoFactorTokenUrl from '@/api/account/getTwoFactorTokenUrl';
-import enableAccountTwoFactor from '@/api/account/enableAccountTwoFactor';
-import FlashMessageRender from '@/components/FlashMessageRender';
-import {Actions, State, useStoreActions, useStoreState} from 'easy-peasy';
+import { State, useStoreState } from 'easy-peasy';
 import { ApplicationStore } from '@/state';
-import { httpErrorToHuman } from '@/api/http';
-import ContentBox from "@/components/elements/ContentBox";
-
-interface Values {
-    code: string;
-}
+import ContentBox from '@/components/elements/ContentBox';
+import tw from 'twin.macro';
+import Button, { LinkButton } from "@/components/elements/Button";
 
 export default ({ ...props }: RequiredModalProps) => {
     const { drivers } = useStoreState<ApplicationStore, any>(state => state.settings.data!.oauth);
@@ -25,29 +16,37 @@ export default ({ ...props }: RequiredModalProps) => {
         >
             {JSON.parse(drivers).map((driver: string) => (
                 <ContentBox
-                    className={'mt-8 md:mt-0 mx-4 my-4 inline-block'}
+                    key={driver}
+                    css={tw`mt-8 md:mt-0 mx-4 my-4 inline-block`}
                 >
-                    <div className={"w-48"}>
-                        <img src={'/assets/svgs/' + driver + '.svg'} className={'w-16 float-right'} alt={driver}/>
+                    <div css={tw`w-48`}>
+                        <img src={'/assets/svgs/' + driver + '.svg'} css={tw`w-16 float-right`} alt={driver}/>
                         <h3>{driver.charAt(0).toUpperCase() + driver.slice(1)}</h3>
 
-                        { oauth[driver] != undefined &&
+                        { oauth[driver] != null &&
                         <div>
-                            <div className={"mt-8 mb-4"}>
+                            <div css={tw`mt-8 mb-4`}>
                                 Linked with id <code>{oauth[driver]}</code>
                             </div>
-                            <div className={"mb-4"}>
-                                <a className={"btn btn-red btn-secondary btn-sm"} href={'/account/oauth/unlink?driver=' + driver}>
+                            <div css={tw`mb-4`}>
+                                <LinkButton
+                                    color={'red'}
+                                    isSecondary
+                                    href={`/account/oauth/unlink?driver=${driver}`}
+                                >
                                     Unlink {driver}
-                                </a>
+                                </LinkButton>
                             </div>
                         </div>
                         }
-                        { oauth[driver] == undefined &&
-                        <div className={"mt-16 mb-4"}>
-                            <a className={"btn btn-green btn-secondary btn-sm"} href={'/account/oauth/link?driver=' + driver}>
+                        { oauth[driver] == null &&
+                        <div css={tw`mt-16 mb-4`}>
+                            <LinkButton
+                                isSecondary
+                                href={`/account/oauth/link?driver=${driver}`}
+                            >
                                 Link {driver}
-                            </a>
+                            </LinkButton>
                         </div>
                         }
 
