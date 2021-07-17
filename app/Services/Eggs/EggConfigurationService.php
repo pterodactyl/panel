91ea0a4f41b3b9f991a25bfbbdc685ddba0f876a
Charles Morgan <sir3lit@gmail.com>
2021-07-17 13:02:00 -0400

    Update core eggs to new docker yolk images (#3382)

--- app/Services/Eggs/EggConfigurationService.php
+++ app/Services/Eggs/EggConfigurationService.php
@@ -50,7 +50,7 @@ class EggConfigurationService
 
         return [
             'done' => is_string($done) ? [$done] : $done,
-            'user_interaction' => Arr::get($startup, 'userInteraction') ?? Arr::get($startup, 'user_interaction') ?? [],
+            'user_interaction' => [],
             'strip_ansi' => Arr::get($startup, 'strip_ansi') ?? false,
         ];
     }
