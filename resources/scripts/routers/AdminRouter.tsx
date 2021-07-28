f1be653486a307f4cd8e5cc848abe27bf42958e7
Matthew Penner <me@matthewp.io>
2021-07-28 11:56:25 -0600

    ui(admin): remove api key components

--- resources/scripts/routers/AdminRouter.tsx
+++ resources/scripts/routers/AdminRouter.tsx
@@ -8,7 +8,6 @@ import { AdminContext } from '@/state/admin';
 import { NotFound } from '@/components/elements/ScreenBlock';
 import OverviewContainer from '@/components/admin/overview/OverviewContainer';
 import SettingsContainer from '@/components/admin/settings/SettingsContainer';
-import ApiKeysContainer from '@/components/admin/api/ApiKeysContainer';
 import DatabasesContainer from '@/components/admin/databases/DatabasesContainer';
 import NewDatabaseContainer from '@/components/admin/databases/NewDatabaseContainer';
 import DatabaseEditContainer from '@/components/admin/databases/DatabaseEditContainer';
@@ -124,10 +123,6 @@ const AdminRouter = ({ location, match }: RouteComponentProps) => {
                         <svg fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
                         <span>Settings</span>
                     </NavLink>
-                    <NavLink to={`${match.url}/api`}>
-                        <svg fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" /></svg>
-                        <span>API Keys</span>
-                    </NavLink>
 
                     <span>Management</span>
 
@@ -208,7 +203,6 @@ const AdminRouter = ({ location, match }: RouteComponentProps) => {
                     <Switch location={location}>
                         <Route path={`${match.path}`} component={OverviewContainer} exact/>
                         <Route path={`${match.path}/settings`} component={SettingsContainer} exact/>
-                        <Route path={`${match.path}/api`} component={ApiKeysContainer} exact/>
 
                         <Route path={`${match.path}/databases`} component={DatabasesContainer} exact/>
                         <Route path={`${match.path}/databases/new`} component={NewDatabaseContainer} exact/>
