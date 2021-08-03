924f00ac9aa20a0b100996655a5b61b00aa15d55
Charles Morgan <sir3lit@gmail.com>
2021-08-02 23:32:10 -0400

    Java Docker image updater feature (#3430)
    Co-Authored-By: Lance Pioch <me@lance.sh>

--- resources/scripts/components/server/features/eula/EulaModalFeature.tsx
+++ resources/scripts/components/server/features/eula/EulaModalFeature.tsx
@@ -53,37 +53,34 @@ const EulaModalFeature = () => {
             .then(() => setLoading(false));
     };
 
-    useEffect(() => () => {
+    useEffect(() => {
         clearFlashes('feature:eula');
     }, []);
 
     return (
-        !visible ?
-            null
-            :
-            <Modal visible onDismissed={() => setVisible(false)} closeOnBackground={false} showSpinnerOverlay={loading}>
-                <FlashMessageRender key={'feature:eula'} css={tw`mb-4`}/>
-                <h2 css={tw`text-2xl mb-4 text-neutral-100`}>Accept Minecraft&reg; EULA</h2>
-                <p css={tw`text-neutral-200`}>
+        <Modal visible={visible} onDismissed={() => setVisible(false)} closeOnBackground={false} showSpinnerOverlay={loading}>
+            <FlashMessageRender key={'feature:eula'} css={tw`mb-4`}/>
+            <h2 css={tw`text-2xl mb-4 text-neutral-100`}>Accept Minecraft&reg; EULA</h2>
+            <p css={tw`text-neutral-200`}>
                     By pressing {'"I Accept"'} below you are indicating your agreement to the&nbsp;
-                    <a
-                        target={'_blank'}
-                        css={tw`text-primary-300 underline transition-colors duration-150 hover:text-primary-400`}
-                        rel={'noreferrer noopener'}
-                        href="https://account.mojang.com/documents/minecraft_eula"
-                    >
+                <a
+                    target={'_blank'}
+                    css={tw`text-primary-300 underline transition-colors duration-150 hover:text-primary-400`}
+                    rel={'noreferrer noopener'}
+                    href="https://account.mojang.com/documents/minecraft_eula"
+                >
                         Minecraft&reg; EULA
-                    </a>.
-                </p>
-                <div css={tw`mt-8 sm:flex items-center justify-end`}>
-                    <Button isSecondary onClick={() => setVisible(false)} css={tw`w-full sm:w-auto border-transparent`}>
+                </a>.
+            </p>
+            <div css={tw`mt-8 sm:flex items-center justify-end`}>
+                <Button isSecondary onClick={() => setVisible(false)} css={tw`w-full sm:w-auto border-transparent`}>
                         Cancel
-                    </Button>
-                    <Button onClick={onAcceptEULA} css={tw`mt-4 sm:mt-0 sm:ml-4 w-full sm:w-auto`}>
+                </Button>
+                <Button onClick={onAcceptEULA} css={tw`mt-4 sm:mt-0 sm:ml-4 w-full sm:w-auto`}>
                         I Accept
-                    </Button>
-                </div>
-            </Modal>
+                </Button>
+            </div>
+        </Modal>
     );
 };
 
