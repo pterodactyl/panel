4da38891c788d7203b0c88bf70c66cc6830ec2aa
Matthew Penner <me@matthewp.io>
2021-09-13 01:53:10 -0600

    admin: fix nest create not working

--- app/Http/Requests/Api/Application/Nests/StoreNestRequest.php
+++ app/Http/Requests/Api/Application/Nests/StoreNestRequest.php
@@ -2,8 +2,13 @@
 
 namespace Pterodactyl\Http\Requests\Api\Application\Nests;
 
+use Pterodactyl\Models\Nest;
 use Pterodactyl\Http\Requests\Api\Application\ApplicationApiRequest;
 
 class StoreNestRequest extends ApplicationApiRequest
 {
+    public function rules(array $rules = null): array
+    {
+        return $rules ?? Nest::getRules();
+    }
 }
