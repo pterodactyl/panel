1ea8a77f79d3ff1a15781eb0202b511db2b9489d
damagex <davem1991@gmail.com>
2021-09-26 16:57:58 +0200

    Update app.php

--- config/app.php
+++ config/app.php
@@ -60,7 +60,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://control.thegameworld.net/'),
 
     /*
     |--------------------------------------------------------------------------
