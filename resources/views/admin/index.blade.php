7b3ad9f25063c1dcc187bbc42fa183561c0c8aaf
progremmerpro2021 <94714782+progremmerpro2021@users.noreply.github.com>
2021-11-19 19:34:19 +0000

    Update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -33,6 +33,7 @@
             <div class="box-body">
                 @if ($version->isLatestPanel())
                     You are running Pterodactyl Panel version <code>{{ config('app.version') }}</code>. Your panel is up-to-date!
+                    your ptherodctyl verin work
                 @else
                     Your panel is <strong>not up-to-date!</strong> The latest version is <a href="https://github.com/Pterodactyl/Panel/releases/v{{ $version->getPanel() }}" target="_blank"><code>{{ $version->getPanel() }}</code></a> and you are currently running version <code>{{ config('app.version') }}</code>.
                 @endif
