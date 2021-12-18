838bff7a503fd6e159e05da9c4fc392ee30e90cf
JexUK <cmrxn2005@gmail.com>
2021-12-18 20:10:11 +0000

    change FileObjectRow bg

--- resources/scripts/components/server/files/FileObjectRow.tsx
+++ resources/scripts/components/server/files/FileObjectRow.tsx
@@ -15,7 +15,7 @@ import { usePermissions } from '@/plugins/usePermissions';
 import { join } from 'path';
 
 const Row = styled.div`
-    ${tw`flex bg-neutral-700 rounded-sm mb-px text-sm hover:text-neutral-100 cursor-pointer items-center no-underline hover:bg-neutral-600`};
+    ${tw`flex bg-neutral-900 rounded-sm mb-px text-sm hover:text-neutral-100 cursor-pointer items-center no-underline hover:bg-neutral-600`};
 `;
 
 const Clickable: React.FC<{ file: FileObject }> = memo(({ file, children }) => {
