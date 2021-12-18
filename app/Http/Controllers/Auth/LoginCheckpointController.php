ffb4b50f3c679b3892b9bd3c700a33f2005c1b00
JexUK <cmrxn2005@gmail.com>
2021-12-18 23:55:41 +0000

    webauthn: add controllers and transformers

--- app/Http/Controllers/Auth/LoginCheckpointController.php
+++ app/Http/Controllers/Auth/LoginCheckpointController.php
@@ -2,36 +2,38 @@
 
 namespace Pterodactyl\Http\Controllers\Auth;
 
-use Carbon\CarbonInterface;
-use Carbon\CarbonImmutable;
 use Pterodactyl\Models\User;
+use Illuminate\Auth\AuthManager;
 use Illuminate\Http\JsonResponse;
 use PragmaRX\Google2FA\Google2FA;
+use Illuminate\Contracts\Config\Repository;
 use Illuminate\Contracts\Encryption\Encrypter;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Pterodactyl\Http\Requests\Auth\LoginCheckpointRequest;
-use Illuminate\Contracts\Validation\Factory as ValidationFactory;
+use Illuminate\Contracts\Cache\Repository as CacheRepository;
 
 class LoginCheckpointController extends AbstractLoginController
 {
-    private const TOKEN_EXPIRED_MESSAGE = 'The authentication token provided has expired, please refresh the page and try again.';
-
-    private ValidationFactory $validation;
-
+    private CacheRepository $cache;
+    private Encrypter $encrypter;
     private Google2FA $google2FA;
 
-    private Encrypter $encrypter;
 
     /**
      * LoginCheckpointController constructor.
      */
-    public function __construct(Encrypter $encrypter, Google2FA $google2FA, ValidationFactory $validation)
-    {
-        parent::__construct();
+    public function __construct(
+        AuthManager $auth,
+        Repository $config,
+        CacheRepository $cache,
+        Encrypter $encrypter,
+        Google2FA $google2FA
+    ) {
+        parent::__construct($auth, $config);
 
-        $this->google2FA = $google2FA;
+        $this->cache = $cache;
         $this->encrypter = $encrypter;
-        $this->validation = $validation;
+        $this->google2FA = $google2FA;
     }
 
     /**
@@ -44,29 +46,28 @@ class LoginCheckpointController extends AbstractLoginController
      * @throws \PragmaRX\Google2FA\Exceptions\IncompatibleWithGoogleAuthenticatorException
      * @throws \PragmaRX\Google2FA\Exceptions\InvalidCharactersException
      * @throws \PragmaRX\Google2FA\Exceptions\SecretKeyTooShortException
-     * @throws \Exception
      * @throws \Illuminate\Validation\ValidationException
      */
     public function __invoke(LoginCheckpointRequest $request): JsonResponse
     {
         if ($this->hasTooManyLoginAttempts($request)) {
             $this->sendLockoutResponse($request);
+            return;
         }
 
-        $details = $request->session()->get('auth_confirmation_token');
-        if (!$this->hasValidSessionData($details)) {
-            $this->sendFailedLoginResponse($request, null, self::TOKEN_EXPIRED_MESSAGE);
-        }
-
-        if (!hash_equals($request->input('confirmation_token') ?? '', $details['token_value'])) {
-            $this->sendFailedLoginResponse($request);
-        }
-
+        $token = $request->input('confirmation_token');
         try {
             /** @var \Pterodactyl\Models\User $user */
-            $user = User::query()->findOrFail($details['user_id']);
+            $user = User::query()->findOrFail($this->cache->get($token, 0));
         } catch (ModelNotFoundException $exception) {
-            $this->sendFailedLoginResponse($request, null, self::TOKEN_EXPIRED_MESSAGE);
+            $this->incrementLoginAttempts($request);
+
+            $this->sendFailedLoginResponse(
+                $request,
+                null,
+                'The authentication token provided has expired, please refresh the page and try again.'
+            );
+            return;
         }
 
         // Recovery tokens go through a slightly different pathway for usage.
@@ -78,10 +79,13 @@ class LoginCheckpointController extends AbstractLoginController
             $decrypted = $this->encrypter->decrypt($user->totp_secret);
 
             if ($this->google2FA->verifyKey($decrypted, (string) $request->input('authentication_code') ?? '', config('pterodactyl.auth.2fa.window'))) {
+                $this->cache->delete($token);
+
                 return $this->sendLoginResponse($user, $request);
             }
         }
 
+        $this->incrementLoginAttempts($request);
         $this->sendFailedLoginResponse($request, $user, !empty($recoveryToken) ? 'The recovery token provided is not valid.' : null);
     }
 
@@ -104,36 +108,5 @@ class LoginCheckpointController extends AbstractLoginController
         }
 
         return false;
-    }
-
-    /**
-     * Determines if the data provided from the session is valid or not. This
-     * will return false if the data is invalid, or if more time has passed than
-     * was configured when the session was written.
-     *
-     * @param array $data
-     * @return bool
-     */
-    protected function hasValidSessionData(array $data): bool
-    {
-        $validator = $this->validation->make($data, [
-            'user_id' => 'required|integer|min:1',
-            'token_value' => 'required|string',
-            'expires_at' => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            return false;
-        }
-
-        if (!$data['expires_at'] instanceof CarbonInterface) {
-            return false;
-        }
-
-        if ($data['expires_at']->isBefore(CarbonImmutable::now())) {
-            return false;
-        }
-
-        return true;
     }
 }
--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,29 +5,47 @@ namespace Pterodactyl\Http\Controllers\Auth;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-use Pterodactyl\Models\User;
+use Illuminate\Auth\AuthManager;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Contracts\View\View;
+use LaravelWebauthn\Facades\Webauthn;
+use Illuminate\Contracts\Config\Repository;
 use Illuminate\Contracts\View\Factory as ViewFactory;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Contracts\Cache\Repository as CacheRepository;
+use Pterodactyl\Contracts\Repository\UserRepositoryInterface;
+use Pterodactyl\Exceptions\Repository\RecordNotFoundException;
 
 class LoginController extends AbstractLoginController
 {
+    /**
+     * @var string
+     */
+    private const SESSION_PUBLICKEY_REQUEST = 'webauthn.publicKeyRequest';
+
+    private CacheRepository $cache;
+    private UserRepositoryInterface $repository;
     private ViewFactory $view;
 
     /**
      * LoginController constructor.
      */
-    public function __construct(ViewFactory $view)
-    {
-        parent::__construct();
+    public function __construct(
+        AuthManager $auth,
+        Repository $config,
+        CacheRepository $cache,
+        UserRepositoryInterface $repository,
+        ViewFactory $view
+    ) {
+        parent::__construct($auth, $config);
 
+        $this->cache = $cache;
+        $this->repository = $repository;
         $this->view = $view;
     }
 
     /**
      * Handle all incoming requests for the authentication routes and render the
-     * base authentication view component. Vuejs will take over at this point and
+     * base authentication view component.  React will take over at this point and
      * turn the login area into a SPA.
      */
     public function index(): View
@@ -45,42 +63,63 @@ class LoginController extends AbstractLoginController
      */
     public function login(Request $request): JsonResponse
     {
+        $username = $request->input('user');
+        $useColumn = $this->getField($username);
+
         if ($this->hasTooManyLoginAttempts($request)) {
             $this->fireLockoutEvent($request);
             $this->sendLockoutResponse($request);
+            return;
         }
 
         try {
-            $username = $request->input('user');
-
             /** @var \Pterodactyl\Models\User $user */
-            $user = User::query()->where($this->getField($username), $username)->firstOrFail();
-        } catch (ModelNotFoundException $exception) {
+            $user = $this->repository->findFirstWhere([[$useColumn, '=', $username]]);
+        } catch (RecordNotFoundException $exception) {
             $this->sendFailedLoginResponse($request);
+            return;
         }
 
         // Ensure that the account is using a valid username and password before trying to
         // continue. Previously this was handled in the 2FA checkpoint, however that has
         // a flaw in which you can discover if an account exists simply by seeing if you
-        // can proceede to the next step in the login process.
+        // can proceed to the next step in the login process.
         if (!password_verify($request->input('password'), $user->password)) {
             $this->sendFailedLoginResponse($request, $user);
+            return;
         }
 
-        if ($user->use_totp) {
-            $token = Str::random(64);
+        $webauthnKeys = $user->webauthnKeys()->get();
 
-            $request->session()->put('auth_confirmation_token', [
-                'user_id' => $user->id,
-                'token_value' => $token,
-                'expires_at' => CarbonImmutable::now()->addMinutes(5),
-            ]);
+        if (sizeof($webauthnKeys) > 0) {
+            $token = Str::random(64);
+            $this->cache->put($token, $user->id, CarbonImmutable::now()->addMinutes(5));
+
+            $publicKey = Webauthn::getAuthenticateData($user);
+            $request->session()->put(self::SESSION_PUBLICKEY_REQUEST, $publicKey);
+            $request->session()->save();
+
+            $methods = ['webauthn'];
+            if ($user->use_totp) {
+                $methods[] = 'totp';
+            }
 
             return new JsonResponse([
-                'data' => [
-                    'complete' => false,
-                    'confirmation_token' => $token,
+                'complete' => false,
+                'methods' => $methods,
+                'confirmation_token' => $token,
+                'webauthn' => [
+                    'public_key' => $publicKey,
                 ],
+            ]);
+        } else if ($user->use_totp) {
+            $token = Str::random(64);
+            $this->cache->put($token, $user->id, CarbonImmutable::now()->addMinutes(5));
+
+            return new JsonResponse([
+                'complete' => false,
+                'methods' => ['totp'],
+                'confirmation_token' => $token,
             ]);
         }
 
