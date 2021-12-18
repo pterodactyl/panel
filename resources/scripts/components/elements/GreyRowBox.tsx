5629d08e4d624b85166982a710e8791f19454668
JexUK <cmrxn2005@gmail.com>
2021-12-18 20:08:07 +0000

    add GreyRow to dark theme

--- resources/scripts/components/elements/GreyRowBox.tsx
+++ resources/scripts/components/elements/GreyRowBox.tsx
@@ -7,6 +7,6 @@ export default styled.div<{ $hoverable?: boolean }>`
     ${props => props.$hoverable !== false && tw`hover:border-neutral-500`};
 
     & .icon {
-        ${tw`rounded-full bg-neutral-500 p-3`};
+        ${tw`rounded-full bg-neutral-900 p-3`};
     }
 `;
