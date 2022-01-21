4a1aefe14eb575a47ed6d8eb278cc493a059d56b
Sajjaad Farzad <sajjfa@gmail.com>
2022-01-20 20:40:51 -0800

    Finally™, working ESLint

--- resources/scripts/components/server/users/PermissionSelectAll.tsx
+++ resources/scripts/components/server/users/PermissionSelectAll.tsx
@@ -6,30 +6,28 @@ import Input from '@/components/elements/Input';
 import isEqual from 'react-fast-compare';
 
 interface Props {
-	isEditable: boolean;
-	permissions: string[];
-	className?: string;
+  isEditable: boolean;
+  permissions: string[];
+  className?: string;
 }
 
-const PermissionTitleBox: React.FC<Props> = memo(({ isEditable, permissions, className }) => {
-	const [ { value }, , { setValue } ] = useField<string[]>('permissions');
+const PermissionSelectAll: React.FC<Props> = memo(({ isEditable, permissions, className }) => {
+    const [ { value }, , { setValue } ] = useField<string[]>('permissions');
 
-	const onCheckboxClicked = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-		if (e.currentTarget.checked) {
-			setValue([
-				...value,
-				...permissions.filter(p => !value.includes(p)),
-			]);
-		} else {
-			setValue(value.filter(p => !permissions.includes(p)));
-		}
-	}, [ permissions, value ]);
+    const onCheckboxClicked = useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) => {
+            if (e.currentTarget.checked) {
+                setValue([ ...value, ...permissions.filter((p) => !value.includes(p)) ]);
+            } else {
+                setValue(value.filter((p) => !permissions.includes(p)));
+            }
+        },
+        [ permissions, value ]
+    );
 
-	return (
-		<>
-			{isEditable && <Input css={tw`mr-1`} type={'checkbox'} checked={permissions.every((p) => value.includes(p))} onChange={onCheckboxClicked} />}
-		</>
-	);
+    return (
+        <>{isEditable && <Input css={tw`mr-1`} type={'checkbox'} checked={permissions.every((p) => value.includes(p))} onChange={onCheckboxClicked} />}</>
+    );
 }, isEqual);
 
-export default PermissionTitleBox;
+export default PermissionSelectAll;
