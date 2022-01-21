d249d157c9bbea47a2012d489e584dbcb405085d
Sajjaad Farzad <sajjfa@gmail.com>
2022-01-20 20:27:01 -0800

    Match ESLint guidelines

--- resources/scripts/components/server/users/PermissionSelectAll.tsx
+++ resources/scripts/components/server/users/PermissionSelectAll.tsx
@@ -1,33 +1,35 @@
-import React, { memo, useCallback } from "react";
-import { useField } from "formik";
-import TitledGreyBox from "@/components/elements/TitledGreyBox";
-import tw from "twin.macro";
-import Input from "@/components/elements/Input";
-import isEqual from "react-fast-compare";
+import React, { memo, useCallback } from 'react';
+import { useField } from 'formik';
+import TitledGreyBox from '@/components/elements/TitledGreyBox';
+import tw from 'twin.macro';
+import Input from '@/components/elements/Input';
+import isEqual from 'react-fast-compare';
 
 interface Props {
-  isEditable: boolean;
-  permissions: string[];
-  className?: string;
+	isEditable: boolean;
+	permissions: string[];
+	className?: string;
 }
 
-const PermissionSelectAll: React.FC<Props> = memo(({ isEditable, permissions, className }) => {
-  const [{ value }, , { setValue }] = useField<string[]>("permissions");
+const PermissionTitleBox: React.FC<Props> = memo(({ isEditable, permissions, className }) => {
+	const [ { value }, , { setValue } ] = useField<string[]>('permissions');
 
-  const onCheckboxClicked = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      if (e.currentTarget.checked) {
-        setValue([...value, ...permissions.filter((p) => !value.includes(p))]);
-      } else {
-        setValue(value.filter((p) => !permissions.includes(p)));
-      }
-    },
-    [permissions, value]
-  );
+	const onCheckboxClicked = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+		if (e.currentTarget.checked) {
+			setValue([
+				...value,
+				...permissions.filter(p => !value.includes(p)),
+			]);
+		} else {
+			setValue(value.filter(p => !permissions.includes(p)));
+		}
+	}, [ permissions, value ]);
 
-  return (
-    <>{isEditable && <Input css={tw`mr-1`} type={"checkbox"} checked={permissions.every((p) => value.includes(p))} onChange={onCheckboxClicked} />}</>
-  );
+	return (
+		<>
+			{isEditable && <Input css={tw`mr-1`} type={'checkbox'} checked={permissions.every((p) => value.includes(p))} onChange={onCheckboxClicked} />}
+		</>
+	);
 }, isEqual);
 
-export default PermissionSelectAll;
+export default PermissionTitleBox;
