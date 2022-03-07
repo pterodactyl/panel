86c61c08c655591ee13324d222b1d96b0c7cb929
TR <66259485+trgaming@users.noreply.github.com>
2022-03-07 20:19:17 +0100

    Delete resources/lang/en directory

--- resources/lang/en/dashboard/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'search' => 'Search for servers...',
-    'no_matches' => 'There were no servers found matching the search criteria provided.',
-    'cpu_title' => 'CPU',
-    'memory_title' => 'Memory',
-];
