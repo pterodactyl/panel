af4616ccf28fa13fdd05840d9e733ab01c43c42f
Jelco <53396500+Jelcoo@users.noreply.github.com>
2022-03-28 21:43:45 +0200

    Add cron cheatsheet (#3866)

---
+++ resources/scripts/components/server/schedules/ScheduleCheatsheetCards.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import tw from 'twin.macro';
+
+export default () => {
+    return (
+        <>
+            <div css={tw`md:w-1/2 h-full bg-neutral-600`}>
+                <div css={tw`flex flex-col`}>
+                    <h2 css={tw`py-4 px-6 font-bold`}>Examples</h2>
+                    <div css={tw`flex py-4 px-6 bg-neutral-500`}>
+                        <div css={tw`w-1/2`}>*/5 * * * *</div>
+                        <div css={tw`w-1/2`}>every 5 minutes</div>
+                    </div>
+                    <div css={tw`flex py-4 px-6`}>
+                        <div css={tw`w-1/2`}>0 */1 * * *</div>
+                        <div css={tw`w-1/2`}>every hour</div>
+                    </div>
+                    <div css={tw`flex py-4 px-6 bg-neutral-500`}>
+                        <div css={tw`w-1/2`}>0 8-12 * * *</div>
+                        <div css={tw`w-1/2`}>hour range</div>
+                    </div>
+                    <div css={tw`flex py-4 px-6`}>
+                        <div css={tw`w-1/2`}>0 0 * * *</div>
+                        <div css={tw`w-1/2`}>once a day</div>
+                    </div>
+                    <div css={tw`flex py-4 px-6 bg-neutral-500`}>
+                        <div css={tw`w-1/2`}>0 0 * * MON</div>
+                        <div css={tw`w-1/2`}>every Monday</div>
+                    </div>
+                </div>
+            </div>
+            <div css={tw`md:w-1/2 h-full bg-neutral-600`}>
+                <h2 css={tw`py-4 px-6 font-bold`}>Special Characters</h2>
+                <div css={tw`flex flex-col`}>
+                    <div css={tw`flex py-4 px-6 bg-neutral-500`}>
+                        <div css={tw`w-1/2`}>*</div>
+                        <div css={tw`w-1/2`}>any value</div>
+                    </div>
+                    <div css={tw`flex py-4 px-6`}>
+                        <div css={tw`w-1/2`}>,</div>
+                        <div css={tw`w-1/2`}>value list separator</div>
+                    </div>
+                    <div css={tw`flex py-4 px-6 bg-neutral-500`}>
+                        <div css={tw`w-1/2`}>-</div>
+                        <div css={tw`w-1/2`}>range values</div>
+                    </div>
+                    <div css={tw`flex py-4 px-6`}>
+                        <div css={tw`w-1/2`}>/</div>
+                        <div css={tw`w-1/2`}>step values</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
