f88b5b75148d44ae76a354d833e6fa9e6918e2f8
Pterodactyl CI <ci@pterodactyl.io>
2022-05-30 00:53:34 +0000

    bump version for release

--- config/app.php
+++ config/app.php
@@ -9,7 +9,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => 'canary',
+    'version' => '1.8.0',
 
     /*
     |--------------------------------------------------------------------------
