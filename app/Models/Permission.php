955117322382ffc142bc82585c323184844da245
ilworkcn <1061372739@qq.com>
2022-06-12 16:51:30 +0800

    更新子用户权限汉化
    如题

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -111,103 +111,103 @@ class Permission extends Model
      */
     protected static $permissions = [
         'websocket' => [
-            'description' => 'Allows the user to connect to the server websocket, giving them access to view console output and realtime server stats.',
+            'description' => '允许用户连接到服务器 websocket，让他们可以访问查看控制台输出和实时服务器统计信息。',
             'keys' => [
-                'connect' => 'Allows a user to connect to the websocket instance for a server to stream the console.',
+                'connect' => '允许用户连接到服务器的 websocket 实例以流式传输控制台。',
             ],
         ],
 
         'control' => [
-            'description' => 'Permissions that control a user\'s ability to control the power state of a server, or send commands.',
+            'description' => '控制用户控制服务器电源状态或发送命令的能力的权限。',
             'keys' => [
-                'console' => 'Allows a user to send commands to the server instance via the console.',
-                'start' => 'Allows a user to start the server if it is stopped.',
-                'stop' => 'Allows a user to stop a server if it is running.',
-                'restart' => 'Allows a user to perform a server restart. This allows them to start the server if it is offline, but not put the server in a completely stopped state.',
+                'console' => '允许用户通过控制台向服务器实例发送命令。',
+                'start' => '允许用户在服务器停止时启动它。',
+                'stop' => '允许用户停止正在运行的服务器。',
+                'restart' => '允许用户执行服务器重启。 这允许他们在脱机时启动服务器，但不会将服务器置于完全停止状态。',
             ],
         ],
 
         'user' => [
-            'description' => 'Permissions that allow a user to manage other subusers on a server. They will never be able to edit their own account, or assign permissions they do not have themselves.',
+            'description' => '允许用户管理服务器上其他子用户的权限。 他们将永远无法编辑自己的帐户，或分配他们自己没有的权限。',
             'keys' => [
-                'create' => 'Allows a user to create new subusers for the server.',
-                'read' => 'Allows the user to view subusers and their permissions for the server.',
-                'update' => 'Allows a user to modify other subusers.',
-                'delete' => 'Allows a user to delete a subuser from the server.',
+                'create' => '允许用户为服务器创建新的子用户。',
+                'read' => '允许用户查看子用户及其对服务器的权限。',
+                'update' => '允许用户修改其他子用户。',
+                'delete' => '允许用户从服务器中删除子用户。',
             ],
         ],
 
         'file' => [
-            'description' => 'Permissions that control a user\'s ability to modify the filesystem for this server.',
+            'description' => '控制用户修改此服务器文件系统能力的权限。',
             'keys' => [
-                'create' => 'Allows a user to create additional files and folders via the Panel or direct upload.',
-                'read' => 'Allows a user to view the contents of a directory, but not view the contents of or download files.',
-                'read-content' => 'Allows a user to view the contents of a given file. This will also allow the user to download files.',
-                'update' => 'Allows a user to update the contents of an existing file or directory.',
-                'delete' => 'Allows a user to delete files or directories.',
-                'archive' => 'Allows a user to archive the contents of a directory as well as decompress existing archives on the system.',
-                'sftp' => 'Allows a user to connect to SFTP and manage server files using the other assigned file permissions.',
+                'create' => '允许用户通过面板或直接上传创建其他文件和文件夹。',
+                'read' => '允许用户查看目录的内容，但不能查看或下载文件的内容。',
+                'read-content' => '允许用户查看给定文件的内容。 这也将允许用户下载文件。',
+                'update' => '允许用户更新现有文件或目录的内容。',
+                'delete' => '允许用户删除文件或目录。',
+                'archive' => '允许用户压缩系统上的的文件以及解压系统上的现有压缩文件。',
+                'sftp' => '允许用户使用其他分配的文件权限连接到 SFTP 并管理服务器文件。',
             ],
         ],
 
         'backup' => [
-            'description' => 'Permissions that control a user\'s ability to generate and manage server backups.',
+            'description' => '控制用户创建和管理服务器备份的能力的权限。',
             'keys' => [
-                'create' => 'Allows a user to create new backups for this server.',
-                'read' => 'Allows a user to view all backups that exist for this server.',
-                'delete' => 'Allows a user to remove backups from the system.',
-                'download' => 'Allows a user to download a backup for the server. Danger: this allows a user to access all files for the server in the backup.',
-                'restore' => 'Allows a user to restore a backup for the server. Danger: this allows the user to delete all of the server files in the process.',
+                'create' => '允许用户为此服务器创建新备份。',
+                'read' => '允许用户查看此服务器存在的所有备份。',
+                'delete' => '允许用户从系统中删除备份。',
+                'download' => '允许用户下载服务器的备份。 这是个危险权限：这允许用户访问备份中服务器的所有文件。',
+                'restore' => '允许用户恢复服务器的备份。 这是个危险权限：这允许用户删除服务器实例中的所有服务器文件。',
             ],
         ],
 
         // Controls permissions for editing or viewing a server's allocations.
         'allocation' => [
-            'description' => 'Permissions that control a user\'s ability to modify the port allocations for this server.',
+            'description' => '控制用户修改此服务器端口分配能力的权限。',
             'keys' => [
-                'read' => 'Allows a user to view all allocations currently assigned to this server. Users with any level of access to this server can always view the primary allocation.',
-                'create' => 'Allows a user to assign additional allocations to the server.',
-                'update' => 'Allows a user to change the primary server allocation and attach notes to each allocation.',
-                'delete' => 'Allows a user to delete an allocation from the server.',
+                'read' => '允许用户查看当前分配给该服务器的所有分配。 对该服务器具有任何访问级别的用户始终可以查看主要分配。',
+                'create' => '允许用户向服务器创建额外的分配。',
+                'update' => '允许用户更改主服务器分配并将注释附加到每个分配。',
+                'delete' => '允许用户从服务器中删除分配。',
             ],
         ],
 
         // Controls permissions for editing or viewing a server's startup parameters.
         'startup' => [
-            'description' => 'Permissions that control a user\'s ability to view this server\'s startup parameters.',
+            'description' => '控制用户查看此服务器启动参数的能力的权限。',
             'keys' => [
-                'read' => 'Allows a user to view the startup variables for a server.',
-                'update' => 'Allows a user to modify the startup variables for the server.',
-                'docker-image' => 'Allows a user to modify the Docker image used when running the server.',
+                'read' => '允许用户查看服务器的启动变量。',
+                'update' => '允许用户修改服务器的启动变量。',
+                'docker-image' => '允许用户修改运行服务器时使用的 Docker 镜像。',
             ],
         ],
 
         'database' => [
-            'description' => 'Permissions that control a user\'s access to the database management for this server.',
+            'description' => '控制用户访问此服务器的数据库管理的权限。',
             'keys' => [
-                'create' => 'Allows a user to create a new database for this server.',
-                'read' => 'Allows a user to view the database associated with this server.',
-                'update' => 'Allows a user to rotate the password on a database instance. If the user does not have the view_password permission they will not see the updated password.',
-                'delete' => 'Allows a user to remove a database instance from this server.',
-                'view_password' => 'Allows a user to view the password associated with a database instance for this server.',
+                'create' => '允许用户为此服务器创建新数据库。',
+                'read' => '允许用户查看与此服务器关联的数据库。',
+                'update' => '允许用户重置数据库实例的密码。 如果用户没有 view_password 权限，他们将看不到更新的密码。',
+                'delete' => '允许用户从此服务器中删除数据库实例。',
+                'view_password' => '允许用户查看与此服务器的数据库实例关联的密码。',
             ],
         ],
 
         'schedule' => [
-            'description' => 'Permissions that control a user\'s access to the schedule management for this server.',
+            'description' => '控制用户访问此服务器计划系统的权限。',
             'keys' => [
-                'create' => 'Allows a user to create new schedules for this server.', // task.create-schedule
-                'read' => 'Allows a user to view schedules and the tasks associated with them for this server.', // task.view-schedule, task.list-schedules
-                'update' => 'Allows a user to update schedules and schedule tasks for this server.', // task.edit-schedule, task.queue-schedule, task.toggle-schedule
-                'delete' => 'Allows a user to delete schedules for this server.', // task.delete-schedule
+                'create' => '允许用户为此服务器创建新计划。', // task.create-schedule
+                'read' => '允许用户查看此服务器的计划和与其关联的任务。', // task.view-schedule, task.list-schedules
+                'update' => '允许用户更新此服务器的计划和计划中的任务。', // task.edit-schedule, task.queue-schedule, task.toggle-schedule
+                'delete' => '允许用户删除此服务器的计划。', // task.delete-schedule
             ],
         ],
 
         'settings' => [
-            'description' => 'Permissions that control a user\'s access to the settings for this server.',
+            'description' => '控制用户访问此服务器设置的权限。',
             'keys' => [
-                'rename' => 'Allows a user to rename this server.',
-                'reinstall' => 'Allows a user to trigger a reinstall of this server.',
+                'rename' => '允许用户重命名此服务器。',
+                'reinstall' => '允许用户执行此服务器的重新安装程序。',
             ],
         ],
     ];
