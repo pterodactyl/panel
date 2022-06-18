16ea2f668ff75301abefcaddd09d2f58d0dffae7
OlekAleksander <66617078+OlekAleksander@users.noreply.github.com>
2022-06-18 20:12:02 +0200

    Update ActivityLogContainer.tsx

--- resources/scripts/components/dashboard/activity/ActivityLogContainer.tsx
+++ resources/scripts/components/dashboard/activity/ActivityLogContainer.tsx
@@ -35,6 +35,8 @@ export default () => {
 
     return (
         <PageContentBlock title={'Account Activity Log'}>
+            <h1 css={tw`text-5xl`}>Account activity log</h1>
+            <h3 css={tw`text-2xl text-neutral-500`}>View your recent Activity</h3>
             <FlashMessageRender byKey={'account'}/>
             {(filters.filters?.event || filters.filters?.ip) &&
                 <div className={'flex justify-end mb-2'}>
