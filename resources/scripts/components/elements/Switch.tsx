13bdb66ff04e3f7b81de76de455c83ebc01fbd7f
DaneEveritt <dane@daneeveritt.com>
2022-06-26 17:19:12 -0400

    Revert "Package updates prior to release"
    This reverts commit af2a84d37930e4481c72c0acb899170da18335fb.

--- resources/scripts/components/elements/Switch.tsx
+++ resources/scripts/components/elements/Switch.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 import styled from 'styled-components/macro';
-import { v4 } from 'uuid';
+import v4 from 'uuid/v4';
 import tw from 'twin.macro';
 import Label from '@/components/elements/Label';
 import Input from '@/components/elements/Input';
