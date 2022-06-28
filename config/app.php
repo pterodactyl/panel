dcf729b3bf7dfd517823cf1b1b9085456fa8332e
VinGal <45609819+VinGal0@users.noreply.github.com>
2022-06-28 20:59:07 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -9,7 +9,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => '1.9.1',
+    'version' => 'canary',
 
     /*
     |--------------------------------------------------------------------------
