bba505dd4aeec23afdf75c61daf9c4eb295a72d6
Boy132 <mail@boy132.de>
2022-07-03 23:02:55 +0200

    fix merge

--- resources/scripts/components/server/files/PullFileModal.tsx
+++ resources/scripts/components/server/files/PullFileModal.tsx
@@ -110,7 +110,7 @@ export default ({ className }: WithClassname) => {
                                     </Code>
                                 </p>
                             </Form>
-                            <Dialog.Buttons>
+                            <Dialog.Footer>
                                 <Button.Text
                                     className={'w-full sm:w-auto'}
                                     onClick={() => {
@@ -123,7 +123,7 @@ export default ({ className }: WithClassname) => {
                                 <Button className={'w-full sm:w-auto'} onClick={submitForm}>
                                     Pull File
                                 </Button>
-                            </Dialog.Buttons>
+                            </Dialog.Footer>
                         </Dialog>
                     )}
                 </Formik>
