1aa308b91419743a68c66ab2d44da57d9e8365b3
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2022-08-19 21:56:50 +0200

    Support language in admin nav bar
    Add @lang in process to supportl anguage

--- resources/views/partials/admin/settings/nav.blade.php
+++ resources/views/partials/admin/settings/nav.blade.php
@@ -6,9 +6,9 @@
         <div class="col-xs-12">
             <div class="nav-tabs-custom nav-tabs-floating">
                 <ul class="nav nav-tabs">
-                    <li @if($activeTab === 'basic')class="active"@endif><a href="{{ route('admin.settings') }}">General</a></li>
-                    <li @if($activeTab === 'mail')class="active"@endif><a href="{{ route('admin.settings.mail') }}">Mail</a></li>
-                    <li @if($activeTab === 'advanced')class="active"@endif><a href="{{ route('admin.settings.advanced') }}">Advanced</a></li>
+                    <li @if($activeTab === 'basic')class="active"@endif><a href="{{ route('admin.settings') }}">@lang('admin.nav.header.general')</a></li>
+                    <li @if($activeTab === 'mail')class="active"@endif><a href="{{ route('admin.settings.mail') }}">@lang('admin.nav.header.mail')</a></li>
+                    <li @if($activeTab === 'advanced')class="active"@endif><a href="{{ route('admin.settings.advanced') }}">@lang('admin.nav.header.advanced')</a></li>
                 </ul>
             </div>
         </div>
