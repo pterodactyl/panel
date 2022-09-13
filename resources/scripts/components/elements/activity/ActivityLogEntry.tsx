e6979885fd1b9ba115906126f7c98a8186f88c77
cmrxnn <cmrxn2005@gmail.com>
2022-09-13 22:30:18 +0100

    Add actor username to activity log UI; closes #133

--- resources/scripts/components/elements/activity/ActivityLogEntry.tsx
+++ resources/scripts/components/elements/activity/ActivityLogEntry.tsx
@@ -53,6 +53,10 @@ export default ({ activity, children }: Props) => {
             <div className={'col-span-10 sm:col-span-9 flex'}>
                 <div className={'flex-1 px-4 sm:px-0'}>
                     <div className={'flex items-center text-gray-50'}>
+                        <Tooltip placement={'top'} content={actor?.email || 'System User'}>
+                            <span>{actor?.username || 'System'}</span>
+                        </Tooltip>
+                        <span className={'text-gray-400'}>&nbsp;&mdash;&nbsp;</span>
                         <Link
                             to={`#${pathTo({ event: activity.event })}`}
                             className={'transition-colors duration-75 active:text-cyan-400 hover:text-cyan-400'}
