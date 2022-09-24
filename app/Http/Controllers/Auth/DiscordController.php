8a9fc1cc41e13e9f350d10ccc0eece02a6b9a233
cmrxnn <cmrxn2005@gmail.com>
2022-09-24 22:53:05 +0100

    Revert to previous DiscordController; closes #137

--- app/Http/Controllers/Auth/DiscordController.php
+++ app/Http/Controllers/Auth/DiscordController.php
@@ -16,26 +16,28 @@ use Pterodactyl\Contracts\Repository\SettingsRepositoryInterface;
 
 class DiscordController extends Controller
 {
-    private UserCreationService $creationService;
     private SettingsRepositoryInterface $settings;
+    private UserCreationService $creationService;
 
-    public function __construct(UserCreationService $creationService, SettingsRepositoryInterface $settings)
-    {
-        $this->settings = $settings;
+    public function __construct(
+        UserCreationService $creationService,
+        SettingsRepositoryInterface $settings,
+    ) {
         $this->creationService = $creationService;
+        $this->settings = $settings;
     }
 
     /**
      * Uses the Discord API to return a user objext.
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        return redirect(
+        return new JsonResponse([
             'https://discord.com/api/oauth2/authorize?'
             . 'client_id=' . $this->settings->get('jexactyl::discord:id')
             . '&redirect_uri=' . route('auth.discord.callback')
-            . '&response_type=code&scope=identify%20email&prompt=none',
-        );
+            . '&response_type=code&scope=identify%20email%20guilds%20guilds.join&prompt=none',
+        ], 200, [], null, false);
     }
 
     /**
@@ -52,60 +54,67 @@ class DiscordController extends Controller
             'grant_type' => 'authorization_code',
             'code' => $request->input('code'),
             'redirect_uri' => route('auth.discord.callback'),
-        ])->body();
+        ]);
 
-        $discord = json_decode(Http::withHeaders(['Authorization' => 'Bearer ' . $code->access_token])->asForm()->get('https://discord.com/api/users/@me')->body());
+        if (!$code->ok()) {
+            return;
+        }
 
-        if (!$code->ok() || !$discord->ok()) return;
-        if (preg_match('(email|guilds|identify)', $code->scope) !== 1) return;
+        $req = json_decode($code->body());
+        if (preg_match('(email|guilds|identify|guilds.join)', $req->scope) !== 1) return;
+
+        $discord = json_decode(Http::withHeaders(['Authorization' => 'Bearer ' . $req->access_token])->asForm()->get('https://discord.com/api/users/@me')->body());
 
         if (User::where('discord_id', $discord->id)->exists()) {
             $user = User::where('discord_id', $discord->id)->first();
             Auth::loginUsingId($user->id, true);
 
-            return redirect()->route('index');
+            return redirect('/');
         } else {
-            if ($this->settings->get('jexactyl::discord:enabled') == 'false') return;
-
             $approved = true;
+
+            if ($this->settings->get('jexactyl::discord:enabled') != 'true') return;
             if ($this->settings->get('jexactyl::approvals:enabled') == 'true') {
                 $approved = false;
             };
 
-            if (User::where('email', $discord->email)->exists()) {
-                redirect()->route('auth.login');
-                throw new DisplayException('An account with this email already exists.');
-            }
-
+            $username = $this->genString();
             $data = [
                 'approved' => $approved,
                 'email' => $discord->email,
-                'username' => $discord->id,
+                'username' => $username,
                 'discord_id' => $discord->id,
                 'name_first' => $discord->username,
-                'name_last' => '#' . $discord->discriminator,
-                'password' => $this->generatePassword(),
+                'name_last' => $discord->discriminator,
+                'password' => $this->genString(),
                 'ip' => $request->getClientIp(),
-                'store_cpu' => $this->settings->get('jexactyl::registration:cpu'),
-                'store_memory' => $this->settings->get('jexactyl::registration:memory'),
-                'store_disk' => $this->settings->get('jexactyl::registration:disk'),
-                'store_slots' => $this->settings->get('jexactyl::registration:slot'),
-                'store_ports' => $this->settings->get('jexactyl::registration:port'),
-                'store_backups' => $this->settings->get('jexactyl::registration:backup'),
-                'store_databases' => $this->settings->get('jexactyl::registration:database'),
+                'store_cpu' => $this->settings->get('jexactyl::registration:cpu', 0),
+                'store_memory' => $this->settings->get('jexactyl::registration:memory', 0),
+                'store_disk' => $this->settings->get('jexactyl::registration:disk', 0),
+                'store_slots' => $this->settings->get('jexactyl::registration:slot', 0),
+                'store_ports' => $this->settings->get('jexactyl::registration:port', 0),
+                'store_backups' => $this->settings->get('jexactyl::registration:backup', 0),
+                'store_databases' => $this->settings->get('jexactyl::registration:database', 0),
             ];
 
-            $this->creationService->handle($data);
-            Auth::loginUsingId(User::where('discord_id', $discord->id)->first()->discord_id, true);
+            try {
+                $this->creationService->handle($data);
+            } catch (Exception $e) {
+                return;
+            }
 
-            return redirect()->route('index');
+            $user = User::where('username', $username)->first();
+            Auth::loginUsingId($user->id, true);
+
+            return redirect('/');
         }
     }
 
     /**
-     * Returns a string used for creating a user's password on the Panel.
+     * Returns a string used for creating a users
+     * username and password on the Panel.
      */
-    private function generatePassword(): string
+    public function genString(): string
     {
         $chars = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
