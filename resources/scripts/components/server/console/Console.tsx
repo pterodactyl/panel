aff1819abd3e9e0a42d0e4b10e3e93a50f85d98e
Charles Morgan <sir3lit@gmail.com>
2022-10-25 01:25:57 -0400

    allowProposedApi->ture

--- resources/scripts/components/server/console/Console.tsx
+++ resources/scripts/components/server/console/Console.tsx
@@ -48,6 +48,7 @@ const terminalProps: ITerminalOptions = {
     fontSize: 12,
     fontFamily: th('fontFamily.mono'),
     theme: theme,
+    allowProposedApi: true,
 };
 
 const terminalInitOnlyProps: ITerminalInitOnlyOptions = {
