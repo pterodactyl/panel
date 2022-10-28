7db66658175f7de852a86979ff83b3a36bc6dada
Lance Pioch <git@lance.sh>
2022-10-27 21:46:14 -0400

    Make sure migrations aren't rolled back

--- tests/Traits/DatabaseMigrations.php
+++ tests/Traits/DatabaseMigrations.php
@@ -4,16 +4,19 @@ namespace Pterodactyl\Tests\Traits;
 
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Testing\Traits\CanConfigureMigrationCommands;
+use Illuminate\Foundation\Testing\DatabaseMigrations as DM;
 
 trait DatabaseMigrations
 {
     use CanConfigureMigrationCommands;
+    use DM;
 
     /**
      * Define hooks to migrate the database before and after each test.
      *
+     * @return void
      */
-    public function runDatabaseMigrations(): void
+    public function runDatabaseMigrations()
     {
         $this->artisan('migrate:fresh', $this->migrateFreshUsing());
 
