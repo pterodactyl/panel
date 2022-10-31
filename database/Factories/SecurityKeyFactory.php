aa380d4c0d52d0456bee0446b863eaf67e683cb4
Matthew Penner <me@matthewp.io>
2022-10-31 13:42:49 -0600

    fix SecurityKeyFactory

--- database/Factories/SecurityKeyFactory.php
+++ database/Factories/SecurityKeyFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Ramsey\Uuid\Uuid;
-use Pterodactyl\Models\User;
 use Pterodactyl\Models\SecurityKey;
 use Webauthn\TrustPath\EmptyTrustPath;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -24,15 +23,11 @@ class SecurityKeyFactory extends Factory
     {
         return [
             'uuid' => Uuid::uuid4()->toString(),
-            'user_id' => User::factory(),
             'name' => $this->faker->word,
             'type' => 'public-key',
             'transports' => [],
             'attestation_type' => 'none',
             'trust_path' => new EmptyTrustPath(),
-            'user_handle' => function (array $attributes) {
-                return User::find($attributes['user_id'])->uuid;
-            },
             'counter' => 0,
         ];
     }
