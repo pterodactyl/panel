a4e547dc676529018ef6404c597b3bd84dddd099
Charles Morgan <sir3lit@gmail.com>
2022-11-27 21:38:28 -0500

    ui(server): fix console searchbar z-index (#4587)

--- resources/scripts/components/server/console/Console.tsx
+++ resources/scripts/components/server/console/Console.tsx
@@ -66,6 +66,11 @@ export default () => {
     const isTransferring = ServerContext.useStoreState((state) => state.server.data!.isTransferring);
     const [history, setHistory] = usePersistedState<string[]>(`${serverId}:command_history`, []);
     const [historyIndex, setHistoryIndex] = useState(-1);
+    // SearchBarAddon has hardcoded z-index: 999 :(
+    const zIndex = `
+    .xterm-search-bar__addon {
+        z-index: 10;
+    }`;
 
     const handleConsoleOutput = (line: string, prelude = false) =>
         terminal.writeln((prelude ? TERMINAL_PRELUDE : '') + line.replace(/(?:\r\n|\r|\n)$/im, '') + '\u001b[0m');
@@ -126,6 +131,7 @@ export default () => {
 
             terminal.open(ref.current);
             fitAddon.fit();
+            searchBar.addNewStyle(zIndex);
 
             // Add support for capturing keys
             terminal.attachCustomKeyEventHandler((e: KeyboardEvent) => {
