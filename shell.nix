16e34af7736e5fcba1018a31be711bbca847e6ed
Matthew Penner <me@matthewp.io>
2022-11-29 10:48:14 -0700

    nix: add panel and container packages

--- shell.nix
+++ shell.nix
@@ -1,14 +1,15 @@
 {
+  composer ? null,
+  php81WithExtensions ? null,
   pkgs ? import <nixpkgs> {},
-  php81WithExtensions,
 }:
 with pkgs;
   mkShell rec {
     buildInputs = [
       alejandra
+      composer
       nodejs-18_x
       nodePackages.yarn
       php81WithExtensions
-      (php81Packages.composer.override {php = php81WithExtensions;})
     ];
   }
