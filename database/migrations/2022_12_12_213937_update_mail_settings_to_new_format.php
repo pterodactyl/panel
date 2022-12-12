c8c648f0e5a0a7007461bc11f519dc3c140ddeec
Matthew Penner <me@matthewp.io>
2022-12-12 15:14:37 -0700

    php-cs-fixer

--- database/migrations/2022_12_12_213937_update_mail_settings_to_new_format.php
+++ database/migrations/2022_12_12_213937_update_mail_settings_to_new_format.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
