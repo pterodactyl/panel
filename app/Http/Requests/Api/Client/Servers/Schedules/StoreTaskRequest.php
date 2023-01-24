2b14e46eec4a71d3c67380f15587f2e5dc8f2f41
Matthew Penner <me@matthewp.io>
2023-01-24 15:57:24 -0700

    api: fix `sequence_id` being ignored in server task API
    Closes #4434

--- app/Http/Requests/Api/Client/Servers/Schedules/StoreTaskRequest.php
+++ app/Http/Requests/Api/Client/Servers/Schedules/StoreTaskRequest.php
@@ -23,6 +23,7 @@ class StoreTaskRequest extends ViewScheduleRequest
             'payload' => 'required_unless:action,backup|string|nullable',
             'time_offset' => 'required|numeric|min:0|max:900',
             'sequence_id' => 'sometimes|required|numeric|min:1',
+            'continue_on_failure' => 'sometimes|required|boolean',
         ];
     }
 }
