31ff87805a491e928175b5ac7fb356df9b0d3813
Xavi <xavi@Xavis-Air.webpass.net>
2023-02-13 01:28:16 -0600

    Remove csrf token verification from sanctum middleware

--- config/sanctum.php
+++ config/sanctum.php
@@ -57,7 +57,7 @@ return [
     */
 
     'middleware' => [
-        'verify_csrf_token' => Pterodactyl\Http\Middleware\VerifyCsrfToken::class,
+        // 'verify_csrf_token' => Pterodactyl\Http\Middleware\VerifyCsrfToken::class,
         'encrypt_cookies' => Pterodactyl\Http\Middleware\EncryptCookies::class,
     ],
 ];
