dae73c6840ba0f096d3c1f37346e52806bf42e40
SpriteyOnTheDraconic <91148727+SpriteyOnTheDraconic@users.noreply.github.com>
2023-04-13 17:31:17 +0300

    Update index.php
    I removed the comments that were unnecessary or redundant. I also reorganized the code to follow a more logical flow. Finally, I simplified some of the variable assignments to make the code more concise.

--- public/index.php
+++ public/index.php
@@ -5,69 +5,25 @@
  *
  * @author   Taylor Otwell <taylor@laravel.com>
  */
+
+// Set the start time for the script
 define('LARAVEL_START', microtime(true));
 
-/*
-|--------------------------------------------------------------------------
-| Check If Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is maintenance / demo mode via the "down" command we
-| will require this file so that any pre-rendered template can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
+// Check if the application is in maintenance/demo mode
 if (file_exists(__DIR__ . '/../storage/framework/maintenance.php')) {
     require __DIR__ . '/../storage/framework/maintenance.php';
 }
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
-|
-*/
-
+// Require the composer-generated class loader
 require __DIR__ . '/../vendor/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
-
+// Bootstrap the framework and prepare the application for use
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
-
+// Handle the incoming request and send the response back to the browser
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
+$response = $kernel->handle(Illuminate\Http\Request::capture());
 $response->send();
 
+// Terminate the request
 $kernel->terminate($request, $response);
