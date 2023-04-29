42c670c7c445cffaef56859da4461a456b4f645b
Alexander Dębowski <alexander.debowski.05@gmail.com>
2023-04-29 23:56:03 +0200

    aaa

--- routes/admin.php
+++ routes/admin.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Pterodactyl\Http\Controllers\Admin;
 
-Route::get('/', [Admin\BaseController::class, 'index'])->name('admin.index')->fallback();
+Route::get('/{react}', [Admin\BaseController::class, 'index'])->where('react', '.+');
