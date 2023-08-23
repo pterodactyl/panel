67b2d944a6f3cae4892f700ef9d21d304576bee8
Boy132 <Boy132@users.noreply.github.com>
2023-08-23 03:12:57 +0200

    ui(client): allow MassActionBar to be clicked through (#4753)

--- resources/scripts/components/server/files/MassActionsBar.tsx
+++ resources/scripts/components/server/files/MassActionsBar.tsx
@@ -93,7 +93,7 @@ const MassActionsBar = () => {
                     />
                 )}
                 <Portal>
-                    <div className={'fixed bottom-0 mb-6 flex justify-center w-full z-50'}>
+                    <div className={'pointer-events-none fixed bottom-0 mb-6 flex justify-center w-full z-50'}>
                         <Fade timeout={75} in={selectedFiles.length > 0} unmountOnExit>
                             <div css={tw`flex items-center space-x-4 pointer-events-auto rounded p-4 bg-black/50`}>
                                 <Button onClick={() => setShowMove(true)}>Move</Button>
