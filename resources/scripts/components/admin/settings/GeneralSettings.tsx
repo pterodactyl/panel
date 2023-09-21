c4559a6f0326b66c49704ecad3f6f5eed02562eb
Alexe Vlad <alexevladgabriel@gmail.com>
2023-09-21 06:44:02 +0000

    fix: solved routing handler for admin settings

--- resources/scripts/components/admin/settings/GeneralSettings.tsx
+++ resources/scripts/components/admin/settings/GeneralSettings.tsx
@@ -3,14 +3,24 @@ import tw from 'twin.macro';
 
 import AdminBox from '@/components/admin/AdminBox';
 import Field, { FieldRow } from '@/components/elements/Field';
+import { useStoreState } from 'easy-peasy';
+import { ApplicationStore } from '@/state';
 
 export default () => {
+    const { name: appName } = useStoreState((state: ApplicationStore) => state.settings.data!);
+
     const submit = () => {
         //
     };
 
     return (
-        <Formik onSubmit={submit} initialValues={{}}>
+        <Formik
+            onSubmit={submit}
+            initialValues={{
+                appName,
+                googleAnalytics: '',
+            }}
+        >
             <Form>
                 <div css={tw`grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-6`}>
                     <AdminBox title="Branding">
@@ -18,7 +28,6 @@ export default () => {
                             <Field id={'appName'} name={'appName'} type={'text'} label={'App Name'} description={''} />
                         </FieldRow>
                     </AdminBox>
-
                     <AdminBox title="Analytics">
                         <FieldRow>
                             <Field
