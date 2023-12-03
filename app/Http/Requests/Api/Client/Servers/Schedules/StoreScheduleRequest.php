c284ff1714e19e29405f90b4d9647ec635c6d902
Boy132 <mail@boy132.de>
2023-12-03 23:39:36 +0100

    add missing fields to StoreScheduleRequest

--- app/Http/Requests/Api/Client/Servers/Schedules/StoreScheduleRequest.php
+++ app/Http/Requests/Api/Client/Servers/Schedules/StoreScheduleRequest.php
@@ -19,9 +19,11 @@ class StoreScheduleRequest extends ViewScheduleRequest
         return [
             'name' => $rules['name'],
             'is_active' => array_merge(['filled'], $rules['is_active']),
+            'only_when_online' => $rules['only_when_online'],
             'minute' => $rules['cron_minute'],
             'hour' => $rules['cron_hour'],
             'day_of_month' => $rules['cron_day_of_month'],
+            'month' => $rules['cron_month'],
             'day_of_week' => $rules['cron_day_of_week'],
         ];
     }
