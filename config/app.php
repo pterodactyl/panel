3020c740c260413bb21984f988a07ab6ca284727
UndercoverNL <42718013+UndercoverNL@users.noreply.github.com>
2023-12-12 18:27:20 +0100

    Revert canary version

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => '1.11.4',
+    'version' => 'canary',
 
     /*
     |--------------------------------------------------------------------------
