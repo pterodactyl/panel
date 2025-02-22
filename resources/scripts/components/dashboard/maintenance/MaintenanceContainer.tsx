e53e36f62c5bfc6ea7c706f8a9e77d0c8448736f
Arion2000 <github@arion2000.xyz>
2025-02-22 14:35:52 +0100

    Sync gh with prod

--- resources/scripts/components/dashboard/maintenance/MaintenanceContainer.tsx
+++ resources/scripts/components/dashboard/maintenance/MaintenanceContainer.tsx
@@ -9,11 +9,11 @@ export default () => {
         return new Date(inputDate.toDateString()) >= new Date(new Date().toDateString());
     }
 
-    if (validateDate(2024, 10, 25)) {
+    if (validateDate(2025, 2, 9)) {
         return (
             <>
                 <Tooltip placement={'bottom'} content={'Geplante Wartungsarbeiten'}>
-                    <div className={'navigation-link'} onClick={() => window.open('https://status.arion2000.xyz/incident/965092', '_blank')}>
+                    <div className={'navigation-link'} onClick={() => window.open('https://status.arion2000.xyz/incident/1103639', '_blank')}>
                         <FontAwesomeIcon icon={faBell} />
                     </div>
                 </Tooltip>
