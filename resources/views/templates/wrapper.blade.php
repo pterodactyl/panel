29772d3b7adb551ad362a00074a258a69daadccb
Jenni <github@yeahjenni.xyz>
2025-05-23 12:25:28 +1000

    Changes for GDPR compliance

--- resources/views/templates/wrapper.blade.php
+++ resources/views/templates/wrapper.blade.php
@@ -31,10 +31,7 @@
                 </script>
             @endif
         @show
-        <style>
-            @import url('//fonts.googleapis.com/css?family=Rubik:300,400,500&display=swap');
-            @import url('//fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans:500&display=swap');
-        </style>
+        <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
 
         @yield('assets')
 
