6339ddf125ab764e421ffda0a5f8b676e7446ba9
Lukas Leis <lukasleis@icloud.com>
2025-06-10 20:57:11 +0000

    Update trigger regex validation to use clearer error messages.

--- app/Repositories/Eloquent/HookRepository.php
+++ app/Repositories/Eloquent/HookRepository.php
@@ -7,7 +7,6 @@ use Pterodactyl\Models\Schedule;
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Pterodactyl\Exceptions\Repository\RecordNotFoundException;
-use Pterodactyl\Contracts\Repository\ScheduleRepositoryInterface;
 
 class HookRepository extends EloquentRepository
 {
@@ -20,22 +19,27 @@ class HookRepository extends EloquentRepository
     }
 
     /**
-     * Return all the schedules for a given server.
+     * Return all hooks for a given server.
      */
     public function findServerHooks(int $server): Collection
     {
-        return $this->getBuilder()->withCount('triggers')->where('server_id', '=', $server)->get($this->getColumns());
+        return $this->getBuilder()
+            ->with(['trigger', 'action'])
+            ->where('server_id', '=', $server)
+            ->get($this->getColumns());
     }
 
     /**
-     * Return a schedule model with all the associated tasks as a relationship.
+     * Return a hook model with the associated  trigger and action relationships.
      *
      * @throws \Pterodactyl\Exceptions\Repository\RecordNotFoundException
      */
     public function getHookWithTriggerAndAction(int $hook): Hook
     {
         try {
-            return $this->getBuilder()->with('tasks')->findOrFail($schedule, $this->getColumns());
+            return $this->getBuilder()
+                ->with(['trigger', 'action'])
+                ->findOrFail($hook, $this->getColumns());
         } catch (ModelNotFoundException) {
             throw new RecordNotFoundException();
         }
