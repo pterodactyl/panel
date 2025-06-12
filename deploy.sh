5f4d6045a250317c912459b30a2dbed010226583
thelostgenx@gmail.com <thelostgenx@gmail.com>
2025-06-12 08:39:16 -0600

    More deploy command things...

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-cd /var/www/pterodactyl && rm -rf panel && git clone https://github.com/Frostbyt3/panel.git && cd panel && cp -r * /var/www/pterodactyl && cd /var/www/pterodactyl && rm -rf panel && yarn build:production
+cd /var/www/pterodactyl && rm -rf panel && git clone https://github.com/Frostbyt3/panel.git && cd panel && cp -r * /var/www/pterodactyl && cd /var/www/pterodactyl && rm -rf panel && php artisan down && yarn build:production && php artisan view:clear && php artisan up
