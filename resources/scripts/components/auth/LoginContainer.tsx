3b9aa228c347a1dba2d38af41ddb4ba6a032d9f2
thelostgenx@gmail.com <thelostgenx@gmail.com>
2025-06-12 09:33:41 -0600

    A wee bit more styling.
    Now it's off to bed with me!

--- resources/scripts/components/auth/LoginContainer.tsx
+++ resources/scripts/components/auth/LoginContainer.tsx
@@ -102,7 +102,7 @@ const LoginContainer = ({ history }: RouteComponentProps) => {
                     <div css={tw`mt-6 text-center`}>
                         <Link
                             to={'/auth/password'}
-                            css={tw`text-xs text-neutral-500 tracking-wide no-underline uppercase hover:text-neutral-600`}
+                            css={tw`text-xs text-neutral-100 tracking-wide no-underline uppercase hover:text-neutral-600`}
                         >
                             Forgot password?
                         </Link>
