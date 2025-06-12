fac601a6ad990f6299e4192734de73eedcd025b7
thelostgenx@gmail.com <thelostgenx@gmail.com>
2025-06-12 08:08:01 -0600

    Getting ready to push changes to live panel.

--- resources/scripts/routers/routes.ts
+++ resources/scripts/routers/routes.ts
@@ -111,8 +111,8 @@ export default {
             permission: 'file.*',
             name: 'Modpacks',
             component: ModpacksContainer,
-            // eggIds: [1, 2, 18],
-            eggIds: [4, 5, 16, 19],
+            eggIds: [1, 2, 5, 18],
+            // eggIds: [4, 5, 16, 19],
             iconProp: faGamepad,
         },
         {
