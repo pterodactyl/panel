a07f6d44f238550d16eaa9eff1382d524f5cc630
thelostgenx@gmail.com <thelostgenx@gmail.com>
2025-06-14 05:00:10 -0600

    Oh, thank god that worked. Now to get fancy.

--- resources/scripts/components/shop/GamesContainer.tsx
+++ resources/scripts/components/shop/GamesContainer.tsx
@@ -54,11 +54,15 @@ export default () => {
                                         </div>
                                         <div css={tw`flex items-center justify-between mt-2 text-sm`}>
                                             <p>Memory</p>
-                                            <code css={tw`font-mono bg-neutral-900 rounded py-1 px-2`}>{item.memory} MB</code>
+                                            <code css={tw`font-mono bg-neutral-900 rounded py-1 px-2`} title="Unlimited">
+                                                {item.memory === 0 ? '∞' : `${item.memory} MB`}
+                                            </code>
                                         </div>
                                         <div css={tw`flex items-center justify-between mt-2 text-sm`}>
                                             <p>Swap</p>
-                                            <code css={tw`font-mono bg-neutral-900 rounded py-1 px-2`}>{item.swap} MB</code>
+                                            <code css={tw`font-mono bg-neutral-900 rounded py-1 px-2`} title="Unlimited">
+                                                {item.swap === 0 ? '∞' : `${item.memory} MB`}
+                                            </code>
                                         </div>
                                         <div css={tw`flex items-center justify-between mt-2 text-sm`}>
                                             <p>Disk</p>
