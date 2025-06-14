eb483d1b3e3835ac0099330e49406aeb14d5de15
thelostgenx@gmail.com <thelostgenx@gmail.com>
2025-06-14 04:49:00 -0600

    Okay. Let's keep trying this with Tailwind...

--- resources/scripts/components/shop/CategoriesContainer.tsx
+++ resources/scripts/components/shop/CategoriesContainer.tsx
@@ -40,8 +40,8 @@ export default () => {
                                 <TitledGreyBox title={item.title}>
                                     <div css={tw`px-1 py-2`}>
                                         <div css={tw`flex flex-wrap`}>
-                                            <div css={tw`w-auto`}>
-                                                <center><img css={'width: 100%;'} src={item.image_url} /></center>
+                                            <div css={tw`w-full flex justify-center`}>
+                                                <img css={tw`max-w-full h-auto`} src={item.image_url} />
                                             </div>
                                         </div>
                                         <div css={tw`text-center`}>
--- resources/scripts/components/shop/GamesContainer.tsx
+++ resources/scripts/components/shop/GamesContainer.tsx
@@ -48,8 +48,8 @@ export default () => {
                                 <TitledGreyBox title={item.name}>
                                     <div css={tw`px-1 py-2`}>
                                         <div css={tw`flex flex-wrap`}>
-                                            <div css={tw`w-auto`}>
-                                                <center><img css={'width: 100%;'} src={item.image_url} /></center>
+                                            <div css={tw`w-full flex justify-center`}>
+                                                <img css={tw`max-w-full h-auto`} src={item.image_url} />
                                             </div>
                                         </div>
                                         <div css={tw`flex items-center justify-between mt-2 text-sm`}>
