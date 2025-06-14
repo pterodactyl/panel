d0e67c2163fd7582888e28be6fc39807daa98298
thelostgenx@gmail.com <thelostgenx@gmail.com>
2025-06-14 07:28:28 -0600

    Update to the admin side of the shop.

--- resources/views/admin/shop/settings/partials/navigation.blade.php
+++ resources/views/admin/shop/settings/partials/navigation.blade.php
@@ -6,11 +6,11 @@
         <div class="nav-tabs-custom nav-tabs-floating">
             <ul class="nav nav-tabs">
                 <li class="{{ $router->currentRouteNamed('admin.shop.settings.payments') ? 'active' : '' }}">
-                    <a href="{{ route('admin.shop.settings.payments') }}">Payments</a></li>
+                    <a href="{{ route('admin.shop.settings.payments') }}">Payments Settings</a></li>
                 <li class="{{ $router->currentRouteNamed('admin.shop.settings.servers') ? 'active' : '' }}">
                     <a href="{{ route('admin.shop.settings.servers') }}">Server Settings</a></li>
                 <li class="{{ $router->currentRouteNamed('admin.shop.settings.tos') ? 'active' : '' }}">
-                    <a href="{{ route('admin.shop.settings.tos') }}">Terms Of Services</a></li>
+                    <a href="{{ route('admin.shop.settings.tos') }}">Terms Of Service</a></li>
                 <li class="{{ $router->currentRouteNamed('admin.shop.settings.invoice') ? 'active' : '' }}">
                     <a href="{{ route('admin.shop.settings.invoice') }}">Invoice Settings</a></li>
             </ul>
--- resources/views/admin/shop/settings/tos.blade.php
+++ resources/views/admin/shop/settings/tos.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.admin')
 
 @section('title')
-    Settings | Terms of Services
+    Settings | Terms of Service
 @endsection
 
 @section('content-header')
-    <h1>Terms of Services Settings <small>Setup your terms of services.</small></h1>
+    <h1>Terms of Service Settings <small>Configure your Terms of Service settings</small></h1>
     <ol class="breadcrumb">
         <li><a href="{{ route('admin.index') }}">Admin</a></li>
-        <li class="active">Terms of Services</li>
+        <li class="active">Terms of Service</li>
     </ol>
 @endsection
 
@@ -19,16 +19,16 @@
         <div class="col-xs-12">
             <div class="box box-primary">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Terms of Services</h3>
+                    <h3 class="box-title">Terms of Service</h3>
                 </div>
                 <form method="post" action="{{ route('admin.shop.settings.tos') }}">
                     <div class="box-body">
                         <div class="form-group">
-                            <label for="tos_url">TOS Url</label>
+                            <label for="tos_url">Terms of Service URL</label>
                             <input type="text" class="form-control" name="tos_url" id="tos_url" value="{{ old('tos_url', $tos_url) }}" placeholder="https://myhost.tld/mytos.pdf">
                         </div>
                         <hr>
-                        <h3>Or: <small>If you set TOS Url, the message won't be shown.</small></h3>
+                        <h3><small>If you set a Terms of Service URL, the below text will not be displayed.</small></h3>
                         <hr>
                         <div class="form-group">
                             <textarea name="tos" id="tos">{!! $tos !!}</textarea>
