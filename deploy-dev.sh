7ee67d78101f8ba40bfecf7cdaaf6e3a42d3419c
thelostgenx@gmail.com <thelostgenx@gmail.com>
2025-06-14 04:20:29 -0600

    ugh

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,1 +1,12 @@
-cd /var/www/pterodactyl && rm -rf panel && git clone https://github.com/Frostbyt3/panel.git && cd panel && cp -r * /var/www/pterodactyl && cd /var/www/pterodactyl && rm -rf panel && export NODE_OPTIONS=--openssl-legacy-provider && php artisan down && yarn build:production && php artisan view:clear && php artisan up
+cd /var/www/pterodactyl &&
+rm -rf panel &&
+git clone --branch 1.0-shop https://github.com/Frostbyt3/panel.git --single-branch &&
+cd panel &&
+cp -r * /var/www/pterodactyl &&
+cd /var/www/pterodactyl &&
+rm -rf panel &&
+export NODE_OPTIONS=--openssl-legacy-provider &&
+php artisan down &&
+yarn build:production &&
+php artisan view:clear &&
+php artisan up
