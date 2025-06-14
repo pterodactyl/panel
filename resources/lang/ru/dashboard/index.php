74cad786d25a6fb7b19bf536430c5783e7138cc6
RimMirK <me@rimmirk.pp.ua>
2025-06-14 15:12:04 +0200

    Update index.php

--- resources/lang/ru/dashboard/index.php
+++ resources/lang/ru/dashboard/index.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'search' => 'Search for servers...',
-    'no_matches' => 'There were no servers found matching the search criteria provided.',
-    'cpu_title' => 'CPU',
-    'memory_title' => 'Memory',
+    'search' => 'Поиск серверов...',
+    'no_matches' => 'Серверы, соответствующие критериям поиска, не найдены.',
+    'cpu_title' => 'ЦП (CPU)',
+    'memory_title' => 'Память (Memory)',
 ];
