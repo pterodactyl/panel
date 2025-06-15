bbf0f916ddb1df0b4c07eb86577c2d4e6dc04dc8
thelostgenx@gmail.com <thelostgenx@gmail.com>
2025-06-15 00:26:33 -0600

    Glaive is apparently right there.

--- resources/scripts/components/elements/PageContentBlock.tsx
+++ resources/scripts/components/elements/PageContentBlock.tsx
@@ -42,7 +42,7 @@ const PageContentBlock: React.FC<PageContentBlockProps> = ({ title, showFlashKey
                             rel={'noopener nofollow noreferrer'}
                             href='https://streamlabs.com/relykstv/tip'
                             target='_blank'
-                            css={tw`no-underline text-neutral-500 hover:text-neutral-300`}>
+                            css={tw`no-underline text-neutral-100 hover:text-neutral-300`}>
                             Donate
                         </a>
                         &nbsp;|&nbsp;
@@ -50,7 +50,7 @@ const PageContentBlock: React.FC<PageContentBlockProps> = ({ title, showFlashKey
                             rel={'noopener nofollow noreferrer'}
                             href='https://discord.gg/xAfHGhvwwA'
                             target='_blank'
-                            css={tw`no-underline text-neutral-500 hover:text-neutral-300`}>
+                            css={tw`no-underline text-neutral-100 hover:text-neutral-300`}>
                             Discord
                         </a>
                         &nbsp;|&nbsp;
@@ -58,7 +58,7 @@ const PageContentBlock: React.FC<PageContentBlockProps> = ({ title, showFlashKey
                             rel={'noopener nofollow noreferrer'}
                             href='https://www.twitch.tv/relykstv'
                             target='_blank'
-                            css={tw`no-underline text-neutral-500 hover:text-neutral-300`}>
+                            css={tw`no-underline text-neutral-100 hover:text-neutral-300`}>
                             Twitch
                         </a>
                     </p>
