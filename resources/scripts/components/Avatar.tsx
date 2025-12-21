d5477ffc5545d2ce2cf6c4b15a5520a56afa4763
lajczi <lajczi@disroot.org>
2025-12-21 10:58:09 +0100

    chore: format code using Prettier

--- resources/scripts/components/Avatar.tsx
+++ resources/scripts/components/Avatar.tsx
@@ -12,14 +12,34 @@ interface Props extends React.ImgHTMLAttributes<HTMLImageElement> {
 
 const _Avatar = ({ email, name, size = 48, alt, className, style, ...props }: Props) => {
     const src = gravatarUrl(email ?? name ?? '', size);
-    return <img src={src} width={size} height={size} className={className} style={style} alt={alt ?? 'Avatar'} {...props} />;
+    return (
+        <img
+            src={src}
+            width={size}
+            height={size}
+            className={className}
+            style={style}
+            alt={alt ?? 'Avatar'}
+            {...props}
+        />
+    );
 };
 
 const _UserAvatar = ({ size = 48, alt, className, style, ...props }: Omit<Props, 'name'>) => {
     const email = useStoreState((state) => state.user.data?.email);
     const src = gravatarUrl(email ?? '', size);
 
-    return <img src={src} width={size} height={size} className={className} style={style} alt={alt ?? 'User avatar'} {...props} />;
+    return (
+        <img
+            src={src}
+            width={size}
+            height={size}
+            className={className}
+            style={style}
+            alt={alt ?? 'User avatar'}
+            {...props}
+        />
+    );
 };
 
 _Avatar.displayName = 'Avatar';
