687c62236721c32f274d0eaf44410c3257e8848e
cesarmr-github <167689141+cesarmr-github@users.noreply.github.com>
2025-12-27 20:34:30 +0100

    Enhance version display and update instructions

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -27,9 +27,29 @@
             </div>
             <div class="box-body">
                 @if ($version->isLatestPanel())
-                    You are running Pterodactyl Panel version <code>{{ config('app.version') }}</code>. Your panel is up-to-date!
+                    You are running Pterodactyl Panel version 
+                    <a href="https://github.com/Pterodactyl/Panel/releases/v{{ config('app.version') }}" target="_blank">
+                        <code>{{ config('app.version') }}</code>
+                    </a>
+                    . Your panel is up-to-date!
                 @else
-                    Your panel is <strong>not up-to-date!</strong> The latest version is <a href="https://github.com/Pterodactyl/Panel/releases/v{{ $version->getPanel() }}" target="_blank"><code>{{ $version->getPanel() }}</code></a> and you are currently running version <code>{{ config('app.version') }}</code>.
+                    <p>
+                        Your panel is <strong>not up-to-date!</strong><br>
+                        The latest version is
+                        <a href="https://github.com/Pterodactyl/Panel/releases/v{{ $version->getPanel() }}" target="_blank">
+                            <code>{{ $version->getPanel() }}</code>
+                        </a>
+                        and you are currently running version
+                        <a href="https://github.com/Pterodactyl/Panel/releases/v{{ config('app.version') }}" target="_blank">
+                            <code>{{ config('app.version') }}</code>
+                        </a>.
+                    </p>
+
+                    <a href="https://pterodactyl.io/panel/1.0/updating.html"
+                    target="_blank"
+                    class="btn btn-danger">
+                        <i class="fa fa-refresh"></i> Update Instructions
+                    </a>    
                 @endif
             </div>
         </div>
