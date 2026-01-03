4131b26b9a81aa8088e0542642bcc37858a71365
cesarmr-github <167689141+cesarmr-github@users.noreply.github.com>
2026-01-03 17:06:21 +0100

    Update ServerRouter.tsx

--- resources/scripts/routers/ServerRouter.tsx
+++ resources/scripts/routers/ServerRouter.tsx
@@ -82,12 +82,7 @@ export default () => {
                                         route.permission ? (
                                             <Can key={route.path} action={route.permission} matchAny>
                                                 <NavLink to={to(route.path, true)} exact={route.exact}>
-                                                    {route.icon && (
-                                                        <FontAwesomeIcon 
-                                                            icon={route.icon} 
-                                                            style={{ marginRight: '8px' }} 
-                                                        />
-                                                    )}
+                                                    {icon && <FontAwesomeIcon icon={icon} style={{ marginRight: '8px' }} />}
                                                     {route.name}
                                                 </NavLink>
                                             </Can>
