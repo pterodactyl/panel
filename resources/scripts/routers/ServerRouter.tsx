1f6722b6b7405b6e600e4f5b46374cbdf7f178f3
cesarmr-github <167689141+cesarmr-github@users.noreply.github.com>
2026-01-03 17:12:08 +0100

    Update ServerRouter.tsx

--- resources/scripts/routers/ServerRouter.tsx
+++ resources/scripts/routers/ServerRouter.tsx
@@ -82,16 +82,20 @@ export default () => {
                                         route.permission ? (
                                             <Can key={route.path} action={route.permission} matchAny>
                                                 <NavLink to={to(route.path, true)} exact={route.exact}>
-                                                    {route.icon && <FontAwesomeIcon icon={route.icon} style={{ marginRight: '8px' }} />}
+                                                    {route.icon && (
+                                                        <FontAwesomeIcon
+                                                            icon={route.icon}
+                                                            style={{ marginRight: '8px' }}
+                                                        />
+                                                    )}
                                                     {route.name}
                                                 </NavLink>
                                             </Can>
                                         ) : (
                                             <NavLink key={route.path} to={to(route.path, true)} exact={route.exact}>
                                                 {route.icon && (
-                                                    <FontAwesomeIcon 
-                                                        icon={route.icon} 
-                                                        style={{ marginRight: '8px' }} 
+                                                    <FontAwesomeIcon
+                                                        icon={route.icon} style={{ marginRight: '8px' }}
                                                     />
                                                 )}
                                                 {route.name}
