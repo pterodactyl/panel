a691b068a49087e490aaf6df8ad50a1f97df2752
Mackenzie Molloy <mackenzie.j.molloy@gmail.com>
2026-01-04 20:51:57 +0000

    Removed hidden space

--- app/Http/Controllers/Admin/NodeAutoDeployController.php
+++ app/Http/Controllers/Admin/NodeAutoDeployController.php
@@ -34,7 +34,7 @@ class NodeAutoDeployController extends Controller
         /** @var ApiKey|null $key */
         $key = $this->repository->getApplicationKeys($request->user())
             ->filter(function (ApiKey $key) {
-                if ($key->user->id != $request->user()->id) { 
+                if ($key->user->id != $request->user()->id) {
                     return false;
                 }
                 foreach ($key->getAttributes() as $permission => $value) {
