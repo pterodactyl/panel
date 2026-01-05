5c98b92aea7d9232f8708ba315533bf8de5eebd7
DaneEveritt <dane@daneeveritt.com>
2026-01-04 16:36:16 -0800

    format files

--- database/migrations/2024_07_13_091852_clear_unused_allocation_notes.php
+++ database/migrations/2024_07_13_091852_clear_unused_allocation_notes.php
@@ -1,16 +1,15 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        
+
         DB::table('allocations')
             ->whereNull('server_id')
             ->update(['notes' => null]);
