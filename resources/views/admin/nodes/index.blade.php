ae2647d4cc1fda652ec62a8f047c643408c1d3cc
_KroZen_ <109613115+KroZen-Dev@users.noreply.github.com>
2026-02-08 20:17:00 +0200

    Add clone functionality for nodes
    Added a modal for cloning nodes with form inputs for name, location, FQDN, and description. Updated the node table to include a clone button for each node.

--- resources/views/admin/nodes/index.blade.php
+++ resources/views/admin/nodes/index.blade.php
@@ -51,7 +51,7 @@
                         @foreach ($nodes as $node)
                             <tr>
                                 <td class="text-center text-muted left-icon" data-action="ping" data-secret="{{ $node->getDecryptedKey() }}" data-location="{{ $node->scheme }}://{{ $node->fqdn }}:{{ $node->daemonListen }}/api/system"><i class="fa fa-fw fa-refresh fa-spin"></i></td>
-                                <td>{!! $node->maintenance_mode ? '<span class="label label-warning"><i class="fa fa-wrench"></i></span> ' : '' !!}<a href="{{ route('admin.nodes.view', $node->id) }}">{{ $node->name }}</a></td>
+                                <td>{!! $node->maintenance_mode ? '<span class="label label-warning"><i class="fa fa-wrench"></i></span> ' : '' !!}<a href="{{ route('admin.nodes.view', $node->id) }}">{{ $node->name }}</a> <a href="#" class="btn btn-xs btn-info clone-btn" data-toggle="modal" data-target="#cloneModal" data-node-id="{{ $node->id }}" data-node-name="{{ $node->name }}" title="Clone Node"><i class="fa fa-copy"></i></a></td>
                                 <td>{{ $node->location->short }}</td>
                                 <td>{{ $node->memory }} MiB</td>
                                 <td>{{ $node->disk }} MiB</td>
@@ -71,11 +71,59 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="cloneModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Clone Node</h4>
+            </div>
+            <form action="" method="POST" id="cloneForm">
+                {!! csrf_field() !!}
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="cloneName">New Node Name</label>
+                        <input type="text" class="form-control" id="cloneName" name="name" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="cloneLocation">Location</label>
+                        <select class="form-control" id="cloneLocation" name="location_id" required>
+                            @foreach($locations ?? [] as $location)
+                                <option value="{{ $location->id }}">{{ $location->short }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="cloneFqdn">FQDN</label>
+                        <input type="text" class="form-control" id="cloneFqdn" name="fqdn" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="cloneDescription">Description</label>
+                        <textarea class="form-control" id="cloneDescription" name="description"></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Clone</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
 
 @section('footer-scripts')
     @parent
     <script>
+    $('#cloneModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var nodeId = button.data('node-id');
+        var nodeName = button.data('node-name');
+        var modal = $(this);
+        modal.find('#cloneName').val(nodeName + ' (Clone)');
+        modal.find('#cloneForm').attr('action', '/admin/nodes/' + nodeId + '/clone');
+    });
     (function pingNodes() {
         $('td[data-action="ping"]').each(function(i, element) {
             $.ajax({
@@ -95,12 +143,11 @@
                 try {
                     errorText = error.responseJSON.errors[0].detail || errorText;
                 } catch (ex) {}
-
-                $(element).removeClass('text-muted').find('i').removeClass().addClass('fa fa-fw fa-heart-o').css('color', '#d9534f');
-                $(element).find('i').tooltip({ title: errorText });
+                $(element).find('i').tooltip({
+                    title: errorText,
+                });
+                $(element).removeClass('text-muted').find('i').removeClass().addClass('fa fa-fw fa-times').css('color', '#d9534f');
             });
-        }).promise().done(function () {
-            setTimeout(pingNodes, 10000);
         });
     })();
     </script>
