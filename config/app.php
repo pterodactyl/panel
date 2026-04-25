bf5e4be31e9fba03bf3f62464e58c76fb9c6230f
Arion2000 <github@arion2000.xyz>
2026-04-25 11:52:32 +0200

    Update app version

--- config/app.php
+++ config/app.php
@@ -10,7 +10,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => '1.12.1',
+    'version' => '1.12.2',
 
     /*
     |--------------------------------------------------------------------------
