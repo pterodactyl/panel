deee161c6c5642d2997815660ca4218ac697513c
cesarmr-github <c3s1r@proton.me>
2026-06-16 00:10:48 +0200

    Add copy configuration button in admin node configurations

--- resources/views/admin/nodes/view/configuration.blade.php
+++ resources/views/admin/nodes/view/configuration.blade.php
@@ -34,7 +34,15 @@
             <div class="box-header with-border">
                 <h3 class="box-title">Configuration File</h3>
             </div>
-            <div class="box-body">
+            <div class="box-body" style="position: relative;">
+                <button
+                    id="copyConfigBtn"
+                    class="btn btn-xs btn-default"
+                    style="position:absolute; top:14px; right:14px; z-index:10;"
+                    title="Copy configuration"
+                >
+                    <i class="fa fa-copy"></i> Copy
+                </button>
                 <pre class="no-margin">{{ $node->getYamlConfiguration() }}</pre>
             </div>
             <div class="box-footer">
@@ -82,6 +90,20 @@
                 text: 'Something went wrong creating your token.',
                 type: 'error'
             });
+        });
+    });
+
+    $('#copyConfigBtn').on('click', function () {
+        const textArea = document.createElement('textarea');
+        textArea.value = $('pre.no-margin').text();
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+        swal({
+            type: 'success',
+            title: 'Copied!',
+            text: 'The configuration has been copied to your clipboard.'
         });
     });
     </script>
