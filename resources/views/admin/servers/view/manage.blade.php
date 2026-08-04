ff3788c315a59cecfd2ef2d78614f8fe3606df73
Sky Mulley <daniel.mulley@outlook.com>
2026-08-04 16:04:42 +0100

    refactor: styling amendments to admin reinstall box

--- resources/views/admin/servers/view/manage.blade.php
+++ resources/views/admin/servers/view/manage.blade.php
@@ -27,8 +27,8 @@
                 </div>
                 <div class="box-footer">
                     @if(! $server->canBeReinstalled())
-                        <button class="btn btn-danger disabled">Server Is Set to Skip Its Install Script</button>
-                        <p style="padding-top: 1rem;">Disable "Skip Egg Install Script" on the startup page to reinstall it.</p>
+                        <button class="btn btn-danger disabled">Reinstall Server</button>
+                        <p style="padding-top: 1rem;">This server is set to skip its install script. Disable "Skip Egg Install Script" on the startup page to reinstall it.</p>
                     @elseif($server->isInstalled())
                         <form action="{{ route('admin.servers.view.manage.reinstall', $server->id) }}" method="POST">
                             {!! csrf_field() !!}
